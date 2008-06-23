78a0fde55fc1c24e69e2621355040698a85fa52b
Krzysztof Krzyżaniak <k.krzyzaniak@gmail.com>
2008-06-23 12:57:49 +0000

    cleanup, add stubs for for read/write functions

--- wikia/releases/200806.4/includes/wikia/ExternalStoreMerged.php
+++ wikia/releases/200806.4/includes/wikia/ExternalStoreMerged.php
@@ -1,15 +1,14 @@
 <?php
 /**
- *
- *
  * DB accessable external objects, all revisions from all databases are merged
  * in one table. Class is singleton
  *
+ * Small ad: use openkomodo
  */
 
 $wgDefaultExternalStore = "MERGED://archive";
 
-#CREATE TABLE `revision` (
+#CREATE TABLE `revisions` (
 #  `id` int(10) NOT NULL auto_increment,
 #  `rev_wikia_id` int(8) unsigned NOT NULL,
 #  `rev_id` int(10) unsigned NOT NULL,
@@ -28,6 +27,17 @@ $wgDefaultExternalStore = "MERGED://archive";
 #) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
 
+#CREATE TABLE `pages` (
+#  `page_wikia_id` int(8) unsigned NOT NULL,
+#  `page_id` int(10) unsigned NOT NULL,
+#  `page_namespace` int(10) unsigned NOT NULL default '0',
+#  `page_title` varchar(255) NOT NULL,
+#  `page_counter` int(8) unsigned NOT NULL default '0',
+#  `page_edits` int(10) unsigned NOT NULL default '0',
+#  PRIMARY KEY (`page_wikia_id`,`page_id`),
+#  KEY `page_namespace` (`page_wikia_id`,`page_namespace`,`page_title`)
+#) ENGINE=InnoDB DEFAULT CHARSET=utf8
+
 /**
  * One-step cache variable to hold base blobs; operations that
  * pull multiple revisions may often pull multiple times from
@@ -39,7 +49,8 @@ $wgExternalBlobCache = array();
 
 class ExternalStoreMerged {
 
-	static $balancers = array();
+	static private $mBalancers = array();
+	private $mDBbname	= "dataware";
 
 	/**
 	 *  __construct
@@ -57,30 +68,30 @@ class ExternalStoreMerged {
 	/** @todo Document.*/
 	function &getLoadBalancer( $cluster ) {
 		global $wgExternalServers;
-		if( !array_key_exists( $cluster, $balancers ) ) {
-			$balancers[ $cluster ] = LoadBalancer::newFromParams( $wgExternalServers[ $cluster ] );
+		if( !array_key_exists( $cluster, $this->mBalancers ) ) {
+			$this->mBalancers[ $cluster ] = LoadBalancer::newFromParams( $wgExternalServers[ $cluster ] );
 		}
-		$balancers[ $cluster ]->allowLagged(true);
-		return $balancers[ $cluster ];
+		$this->mBalancers[ $cluster ]->allowLagged(true);
+		return $this->mBalancers[ $cluster ];
 	}
 
 	/** @todo Document.*/
 	function &getSlave( $cluster ) {
-		$lb =& $this->getLoadBalancer( $cluster );
+		$lb = $this->getLoadBalancer( $cluster );
 		return $lb->getConnection( DB_SLAVE );
 	}
 
 	/** @todo Document.*/
 	function &getMaster( $cluster ) {
-		$lb =& $this->getLoadBalancer( $cluster );
+		$lb = $this->getLoadBalancer( $cluster );
 		return $lb->getConnection( DB_MASTER );
 	}
 
 	/** @todo Document.*/
-	function getTable( &$db ) {
-		$table = $db->getLBInfo( 'blobs table' );
+	function getTable( &$db, $name ) {
+		$table = $db->getLBInfo( "{$this->mDBbname} {$name}" );
 		if ( is_null( $table ) ) {
-			$table = 'blobs';
+			$table = $name;
 		}
 		return $table;
 	}
@@ -99,7 +110,7 @@ class ExternalStoreMerged {
 			$itemID = false;
 		}
 
-		$ret =& $this->fetchBlob( $cluster, $id, $itemID );
+		$ret = $this->fetchBlob( $cluster, $id, $itemID );
 
 		if ( $itemID !== false && $ret !== false ) {
 			return $ret->getItem( $itemID );
@@ -115,28 +126,24 @@ class ExternalStoreMerged {
 	 * @param $id
 	 * @param $itemID
 	 * @return mixed
-	 * @private
+	 *
+	 * @access private
 	 */
 	function &fetchBlob( $cluster, $id, $itemID ) {
 		global $wgExternalBlobCache;
 		$cacheID = ( $itemID === false ) ? "$cluster/$id" : "$cluster/$id/";
 		if( isset( $wgExternalBlobCache[$cacheID] ) ) {
-			wfDebug( "ExternalStoreDB::fetchBlob cache hit on $cacheID\n" );
+			wfDebug( __METHOD__.": cache hit on $cacheID\n" );
 			return $wgExternalBlobCache[$cacheID];
 		}
 
-		wfDebug( "ExternalStoreDB::fetchBlob cache miss on $cacheID\n" );
+		wfDebug( __METHOD__.": cache miss on $cacheID\n" );
 
-		$dbr =& $this->getSlave( $cluster );
-		$ret = $dbr->selectField( $this->getTable( $dbr ), 'blob_text', array( 'blob_id' => $id ) );
+		$dbr = $this->getSlave( $cluster );
+		#--- get revision from slave
 		if ( $ret === false ) {
-			wfDebugLog( 'ExternalStoreDB', "ExternalStoreDB::fetchBlob master fallback on $cacheID\n" );
-			// Try the master
-			$dbw =& $this->getMaster( $cluster );
-			$ret = $dbw->selectField( $this->getTable( $dbw ), 'blob_text', array( 'blob_id' => $id ) );
-			if( $ret === false) {
-				wfDebugLog( 'ExternalStoreDB', "ExternalStoreDB::fetchBlob master failed to find $cacheID\n" );
-			}
+			#--- get revision from master
+			$dbw = $this->getMaster( $cluster );
 		}
 		if( $itemID !== false && $ret !== false ) {
 			// Unserialise object; caller extracts item
@@ -153,20 +160,16 @@ class ExternalStoreMerged {
 	 * @param $cluster String: the cluster name
 	 * @param $data String: the data item
 	 * @param Revision	$revision	Revision object
-	 * 
+	 *
 	 * @return string URL
 	 */
 	function store( $cluster, $data, &$revision ) {
-		$fname = __METHOD__;
-
 		$dbw = $this->getMaster( $cluster );
 
-		$id = $dbw->nextSequenceValue( 'blob_blob_id_seq' );
-		$dbw->insert( $this->getTable( $dbw ), array( 'blob_id' => $id, 'blob_text' => $data ), $fname );
-		$id = $dbw->insertId();
-		if ( $dbw->getFlag( DBO_TRX ) ) {
-			$dbw->immediateCommit();
-		}
+		$dbw->begin();
+		#--- fill revision table
+		#--- fill page table
+		$dbw->commit();
 		return "MERGED://$cluster/$id";
 	}
 }
