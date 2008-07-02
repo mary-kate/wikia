d51c3ed09e1091056a3b55f31a5d5c6003df7737
inez <inez@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-07-02 16:11:10 +0000

    * Inc

--- wikia/releases/200807.1/includes/wikia/DefaultSettings.php
+++ wikia/releases/200807.1/includes/wikia/DefaultSettings.php
@@ -27,9 +27,9 @@ if( !defined( 'MEDIAWIKI' ) ) {
  * Use revision number
  */
 
-$wgStyleVersion         = '127';
-$wgMergeStyleVersionJS  = '127';
-$wgMergeStyleVersionCSS = '127';
+$wgStyleVersion         = '128';
+$wgMergeStyleVersionJS  = '128';
+$wgMergeStyleVersionCSS = '128';
 
 /**
  * @name $wgCityId
