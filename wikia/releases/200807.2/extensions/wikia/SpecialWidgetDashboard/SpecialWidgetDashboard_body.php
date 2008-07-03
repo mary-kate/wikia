9624f6bdf01a70edaed54586f5e4e819847666e5
macbre <macbre@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-07-03 16:33:22 +0000

    Widgets: fixes #2397

--- wikia/releases/200807.2/extensions/wikia/SpecialWidgetDashboard/SpecialWidgetDashboard_body.php
+++ wikia/releases/200807.2/extensions/wikia/SpecialWidgetDashboard/SpecialWidgetDashboard_body.php
@@ -20,14 +20,14 @@ class SpecialWidgetDashboard extends SpecialPage {
 		$this->setHeaders();
 
 		if(WidgetFramework::getInstance()->GetSkin() == 'monaco') {
-			$wgOut->addHTML('<div class="reset sidebar widget_sidebar">');
-			$wgOut->addHTML('<div id="sidebar_3" class="reset sidebar" style="display:inline; float: left; width: 15.45em; margin-right: 8px; padding-bottom: 75px">');
+			$wgOut->addHTML('<div class="sidebar widget_sidebar widget_dashboard">');
+			$wgOut->addHTML('<div id="sidebar_3" class="sidebar" style="display:inline; float: left; width: 15.45em; margin-right: 8px; padding-bottom: 75px">');
 			$wgOut->addHTML(WidgetFramework::getInstance()->Draw(3));
 			$wgOut->addHTML('</div>');
-			$wgOut->addHTML('<div id="sidebar_4" class="reset sidebar" style="display:inline; float: left; width: 15.45em; padding-bottom: 75px">');
+			$wgOut->addHTML('<div id="sidebar_4" class="sidebar" style="display:inline; float: left; width: 15.45em; padding-bottom: 75px">');
 			$wgOut->addHTML(WidgetFramework::getInstance()->Draw(4));
 			$wgOut->addHTML('</div>');
-			$wgOut->addHTML('<div id="sidebar_5" class="reset sidebar" style="display:inline; float: left; width: 15.45em; margin-left: 8px; padding-bottom: 75px">');
+			$wgOut->addHTML('<div id="sidebar_5" class="sidebar" style="display:inline; float: left; width: 15.45em; margin-left: 8px; padding-bottom: 75px">');
 			$wgOut->addHTML(WidgetFramework::getInstance()->Draw(5));
 			$wgOut->addHTML('</div>');
 			$wgOut->addHTML('</div>');
