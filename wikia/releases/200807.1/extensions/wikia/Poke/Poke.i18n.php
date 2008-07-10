9ff2a0f53cf8bc8d9d659da13fb4f3df5fb5789f
pean <pean@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-07-10 19:14:30 +0000

    added real_name to email params

--- wikia/releases/200807.1/extensions/wikia/Poke/Poke.i18n.php
+++ wikia/releases/200807.1/extensions/wikia/Poke/Poke.i18n.php
@@ -4,13 +4,18 @@ function efWikiaPoke() {
 	return array(
 	
 	'en' => array(
-		'poke_subject' => '$1 has poked you on {{SITENAME}}!',
+		'poke_subject' => '$2 has poked you on {{SITENAME}}!',
 		'poke_body' => 'Hi $1:
 
-$2 has poked you on {{SITENAME}}!
+You have been nudged by $5 on Wikia Search.
 
-Please click this link to view your profile and poke $2 back:
-$3
+To nudge $5 back or to see all of your notifications, click the link below:
+
+{{PROFILEJSONPATH}}notifications.html
+
+To view $5\'s profile, click the link below:
+
+{{PROFILEJSONPATH}}profile.html?user={{#BULLETINNAME:$2}}
 
 Thanks
 
@@ -20,13 +25,19 @@ Hey, want to stop getting emails from us?
 
 Click $4 
 and change your settings to disable email notifications.',
-		'poke_back_subject' => '$1 has poked you back on {{SITENAME}}!',
+		'poke_back_subject' => '$2 has poked you back on {{SITENAME}}!',
 		'poke_back_body' => 'Hi $1:
 
-$2 has poked you back on {{SITENAME}}!
+$5 nudged you back on Wikia Search.
 
-Please click this link to view your profile and poke $2 back:
-$3
+To nudge $5 back again or to see all of your notifications, click the link below:
+
+{{PROFILEJSONPATH}}notifications.html
+
+
+To view $5\'s profile, click the link below:
+
+{{PROFILEJSONPATH}}profile.html?user={{#BULLETINNAME:$2}}
 
 Thanks
 
