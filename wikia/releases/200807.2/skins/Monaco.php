9456f80947494967fc449e1d9bc843af1569b650
inez <inez@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-07-10 08:17:13 +0000

    * Move ads injection JavaScript from Monaco.php to main.js. That will have big positive impact on every page size.

--- wikia/releases/200807.2/skins/Monaco.php
+++ wikia/releases/200807.2/skins/Monaco.php
@@ -1624,50 +1624,7 @@ if($wgAdServingType === 1) {
 	}
 	uasort($adsDisplayed, "cmpAds");
 	foreach($adsDisplayed as $adSpace => $ad) {
-?>
-<script type="text/javascript">
-<!--//<![CDATA[
-if(document.getElementById('adSpace<?=$adSpace?>')<?=(substr($ad[1], 0, 4) == 'FAST' ? ' && FASTisValid("'.$ad[1].'")' : '') ?>) {
-	var adSpace<?=$adSpace?> = true;
-	document.write('<scr'+'ipt type="text/javascript">enableWikiaWriter(<?=$adSpace?>);</scr'+'ipt>');
-	document.write('<scr'+'ipt type="text/javascript">');
-	document.write('var base_url = "http://wikia-ads.wikia.com/www/delivery/ajs.php";');
-	document.write('base_url += "?loc=" + escape(window.location);');
-	document.write('if(typeof document.referrer != "undefined") base_url += "&referer=" + escape(document.referrer);');
-	document.write('if(typeof document.context != "undefined") base_url += "&context=" + escape(document.context);');
-	document.write('if(typeof document.mmm_fo != "undefined") base_url += "&mmm_fo=1";');
-	document.write('base_url += "&zoneid=<?=$ad[0]?>";');
-	document.write('base_url += "&cb=" + Math.floor(Math.random()*99999999999);');
-	document.write('if(typeof document.MAX_used != "undefined" && document.MAX_used != ",") base_url += "&exclude=" + document.MAX_used;');
-<?php
-/**
- * Parameters description
- * 1 - collision
- * 2 - no-collision
- * 3 - logged in
- * 4 - not logged in
- */
-if($ad[1] == 'FAST_BOTTOM') {
-?>
-	document.write('if(FASTisCollisionBottom()) base_url += "&source=1";');
-<?php
-} else if($ad[1] == 'FAST_TOP') {
-?>
-	document.write('if(FASTisCollisionTop()) base_url += "&source=14"; else base_url += "&source=24";');
-<?php
-}
-?>
-	document.write('</scr'+'ipt>');
-	document.write('<scr'+'ipt type="text/javascript" src="'+base_url+'"></scr'+'ipt>');
-}
-//]]>--></script>
-<script type="text/javascript">
-<!--//<![CDATA[
-if(typeof adSpace<?=$adSpace?> != 'undefined') {
-	document.write('<scr'+'ipt type="text/javascript">disableWikiaWriter();</scr'+'ipt>');
-}
-//]]>--></script>
-<?php
+?><script type="text/javascript">ad_call(<?= $adSpace ?>,'<?= $ad[0] ?>','<?= $ad[1] ?>');</script><script type="text/javascript">if(curAdSpaceId != -1) document.write('<scr'+'ipt type="text/javascript">disableWikiaWriter();</scr'+'ipt>');</script><?php
 	}
 }
 echo AdServer::getInstance()->getAd('js_bot2');
