eb49c4e345d772325528567a64d4403797d63353
Inez Korczyński <inez@users.noreply.github.com>
2008-07-11 10:44:59 +0000

    * #3248

--- wikia/releases/200807.2/languages/messages/wikia/MessagesEn.php
+++ wikia/releases/200807.2/languages/messages/wikia/MessagesEn.php
@@ -943,5 +943,5 @@ If you think this was sent in error, please let us know at support@wikia.com',
 'var_logtext' => 'Settings log',
 'var_logheader' => '123',
 'tog-showAds' => '<b>Show all advertisements</b><br/>Select this option to see article pages as logged-out users see them.<br/><br/>',
-
+'fast-adv' => 'Advertisement',
 ) );
