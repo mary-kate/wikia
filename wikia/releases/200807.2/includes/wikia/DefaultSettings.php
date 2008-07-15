dff67ef3909f5e4a153773dc26108f2e94c5071f
inez <inez@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-07-15 09:14:42 +0000

    * Increment $wgStyleVersion

--- wikia/releases/200807.2/includes/wikia/DefaultSettings.php
+++ wikia/releases/200807.2/includes/wikia/DefaultSettings.php
@@ -27,7 +27,7 @@ if( !defined( 'MEDIAWIKI' ) ) {
  * Use revision number
  */
 
-$wgStyleVersion         = '260';
+$wgStyleVersion         = '261';
 $wgMergeStyleVersionJS  = '260';
 $wgMergeStyleVersionCSS = '257';
 
