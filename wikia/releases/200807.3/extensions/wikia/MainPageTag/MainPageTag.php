2a374b0604a13cf4fff83a7d471057e958b316ad
inez <inez@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-07-17 09:40:58 +0000

    * Fix Christian's problem with MainPageTag

--- wikia/releases/200807.3/extensions/wikia/MainPageTag/MainPageTag.php
+++ wikia/releases/200807.3/extensions/wikia/MainPageTag/MainPageTag.php
@@ -8,7 +8,8 @@ if( !defined( 'MEDIAWIKI' ) ) {
 }
 
 $wgExtensionFunctions[] = 'wfMainPageTag';
-$MainPageTagState = 0;
+$wfMainPageTag_rcs_called = false;
+$wfMainPageTag_lcs_called = false;
 
 function wfMainPageTag() {
 	global $wgParser;
@@ -19,21 +20,26 @@ function wfMainPageTag() {
 }
 
 function wfMainPageTag_rcs( $input, $args, $parser ) {
-	global $MainPageTagState;
+	global $wfMainPageTag_rcs_called, $wfMainPageTag_lcs_called;
+	if(!$wfMainPageTag_lcs_called) {
+		$wfMainPageTag_rcs_called = true;
+	}
 	$html = '<div style="position: relative; width: 300px; float: right; clear: right;"><div>';
-	$MainPageTagState = 1;
 	return $html;
 }
 
 function wfMainPageTag_lcs( $input, $args, $parser ) {
-	global $MainPageTagState;
+	global $wfMainPageTag_rcs_called, $wfMainPageTag_lcs_called;
+	$wfMainPageTag_lcs_called = true;
+
 	if(!isset($args['gutter'])) {
 		$args['gutter'] = '10';
 	}
+
 	$args['gutter'] = str_replace('px', '', $args['gutter']);
-	if($MainPageTagState === 1) {
+
+	if($wfMainPageTag_rcs_called) {
 		$html = '<div style="overflow: hidden; height: 1%; padding-right: '. $args['gutter'] .'px"><div>';
-		$MainPageTagState = 0;
 	} else {
 		$gutter = 300 + $args['gutter'];
 		$html = '<div style="float: left; margin-right: -'. $gutter .'px; width: 100%; position: relative;"><div style="margin-right: '. $gutter .'px;">';
