3780a312b2260e64c3c0728b0e94ee35a9872d0b
marooned <marooned@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-07-22 12:11:18 +0000

    ComplexAnalysis project

---
+++ wikia/branches/Marooned/ComplexAnalysis/extensions/wikia/ProblemReports/js/ProblemReports-loader.js
@@ -0,0 +1,24 @@
+var wikiaProblemReportsDialog = false;
+
+// initialize "report a problem" callbacks
+// perform lazy loading of JS when user clicks "report a problem"
+YAHOO.util.Event.onDOMReady( function () {
+
+	var links = ["fe_report_link", "ca-report-problem"];
+
+	YAHOO.util.Event.addListener(links, "click", function() {
+		if (wikiaProblemReportsDialog == false) {
+			YAHOO.log('ProblemReports: loading JS');
+			YAHOO.util.Get.script(wgExtensionsPath + '/wikia/ProblemReports/js/ProblemReports.js?' + wgStyleVersion, {
+				onSuccess: function() {
+					YAHOO.log('ProblemReports: JS loaded');
+					wikiaProblemReportsDialog = new YAHOO.wikia.ProblemReportsDialog();
+					wikiaProblemReportsDialog.fire();
+				}
+			});
+		}
+		else {
+			wikiaProblemReportsDialog.fire();
+		}
+	});
+});
