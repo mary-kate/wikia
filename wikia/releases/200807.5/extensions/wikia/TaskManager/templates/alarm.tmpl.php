a8729212c68b7773e46d5c92bdca6d985939e174
eloy <eloy@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-07-23 07:57:43 +0000

    change link to main wikia

--- wikia/releases/200807.5/extensions/wikia/TaskManager/templates/alarm.tmpl.php
+++ wikia/releases/200807.5/extensions/wikia/TaskManager/templates/alarm.tmpl.php
@@ -1,6 +1,6 @@
 Task no <?php echo $task->getId() ?> (<?php echo $task->getType()?>) stucked
 and was killed by Task manager.
 
-Go to <https://staff.wikia.com/wiki/Special:TaskManager> and check situation.
+Go to <https://www.wikia.com/wiki/Special:TaskManager> and check situation.
 
 Yours Task Manager
