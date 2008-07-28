29f8ccd1794faacc4f8552463b5e506edf15116f
marooned <marooned@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-07-28 15:18:03 +0000

    wrong place for 2 lines..

--- wikia/branches/Marooned/ComplexAnalysis/maintenance/wikia/complexity_analysis.php
+++ wikia/branches/Marooned/ComplexAnalysis/maintenance/wikia/complexity_analysis.php
@@ -27,21 +27,18 @@ $db = wfGetDB(DB_SLAVE);
 $sql = 'SELECT page_id, page_title FROM page WHERE page_namespace = 0;';
 $res = $db->query($sql);
 $countAll = 0;
-wfElementCount("*special:start:{$wgCityId}|{$row->page_id}|{$row->page_title}");
 while ($row = $db->fetchObject($res)) {
 	$title = Title::newFromText($row->page_title);
 	if (is_object($title)) {
 		$revision = Revision::newFromTitle($title);
 		if(is_object($revision)) {
+			wfElementCount("*special:start:{$wgCityId}|{$row->page_id}|{$row->page_title}");
 			$wgOut->parse($revision->getText());
+			wfElementCount("*special:stop:{$wgCityId}|{$row->page_id}|{$row->page_title}");
 			$countAll++;
 		}
 	}
 }
-wfElementCount("*special:stop:{$wgCityId}|{$row->page_id}|{$row->page_title}");
-
-//if (isset($options['verbose'])) print($sql. "\n");
-//if (!isset($options['dryrun'])) $db->query($sql);
 
 echo "Parsed $countAll articles.\n";
 ?>
