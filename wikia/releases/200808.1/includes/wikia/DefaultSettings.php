de3fb2b4af51483ada2a596102fa8936c8b1b707
Inez Korczyński <inez@users.noreply.github.com>
2008-07-30 22:41:55 +0000

    * Increment $wgStyleVersion and similar

--- wikia/releases/200808.1/includes/wikia/DefaultSettings.php
+++ wikia/releases/200808.1/includes/wikia/DefaultSettings.php
@@ -27,9 +27,9 @@ if( !defined( 'MEDIAWIKI' ) ) {
  * Use revision number
  */
 
-$wgStyleVersion         = '589';
-$wgMergeStyleVersionJS  = '589';
-$wgMergeStyleVersionCSS = '589';
+$wgStyleVersion         = '591';
+$wgMergeStyleVersionJS  = '591';
+$wgMergeStyleVersionCSS = '591';
 
 /**
  * @name $wgCityId
