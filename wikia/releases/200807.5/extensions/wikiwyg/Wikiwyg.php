4a711195e031e08348615bd84ccc0c13127b032d
emil <emil@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-07-31 11:17:24 +0000

    a main entry file for wikiwyg (to make a migration to MW 1.13 easier)

---
+++ wikia/releases/200807.5/extensions/wikiwyg/Wikiwyg.php
@@ -0,0 +1,6 @@
+<?php
+
+include_once( "$IP/extensions/wikiwyg/share/MediaWiki/extensions/MediaWikiWyg.php" );
+include_once( "$IP/extensions/wikiwyg/share/MediaWiki/extensions/WikiwygEditing/WikiwygEditing.php" );
+include_once( "$IP/extensions/wikia/CreatePage/SpecialCreatePage.php" );
+include_once( "$IP/extensions/wikia/CreatePage/CreatePageCore.php" );
