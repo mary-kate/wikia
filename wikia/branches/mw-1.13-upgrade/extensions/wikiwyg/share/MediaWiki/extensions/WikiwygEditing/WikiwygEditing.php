f6aa6eb43141b0e7a10069f0c371e956051e538d
emil <emil@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-07-31 10:07:44 +0000

    there is no message file anyway so don't try to load messages

--- wikia/branches/mw-1.13-upgrade/extensions/wikiwyg/share/MediaWiki/extensions/WikiwygEditing/WikiwygEditing.php
+++ wikia/branches/mw-1.13-upgrade/extensions/wikiwyg/share/MediaWiki/extensions/WikiwygEditing/WikiwygEditing.php
@@ -34,8 +34,6 @@ function WikiwygAlternateEdit ($epage) {
     global $wgServer, $wgWikiwygJsPath, $wgWikiwygCssPath, $wgWikiwygImagePath, $wgStyleVersion;
     global $wgUser, $wgServer, $wgArticlePath, $wgEnableAjaxLogin;
 
-	wfLoadExtensionMessages('Wikiwyg');
-
     /* in-page disabled automatically disables this loading */
     if ( wfGetDependingOnSkin () == 0 ) {
 	return true;
