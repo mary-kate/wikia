c5e7e8a1ea35243ae6f0dbf80251e0bca34ef813
emil <emil@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-07-31 11:41:04 +0000

    changed paths

--- wikia/branches/mw-1.13-upgrade/extensions/wikiwyg/Wikiwyg.php
+++ wikia/branches/mw-1.13-upgrade/extensions/wikiwyg/Wikiwyg.php
@@ -8,14 +8,10 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 	exit( 1 );
 }
 
-$wgWikiwygEnabled = true;
-$wgWysiwygEnabled = true;
-$wgInPageEnabled = true;
-$wgYuiPath = "http://yui.yahooapis.com/2.3.0/build";
 require_once("$IP/extensions/wikiwyg/share/MediaWiki/extensions/MediaWikiWyg.php");
 require_once("$IP/extensions/wikiwyg/share/MediaWiki/extensions/WikiwygEditing/WikiwygEditing.php");
-require_once("$IP/extensions/wikiwyg/share/MediaWiki/extensions/CreatePage/SpecialCreatePage.php");
-require_once("$IP/extensions/wikiwyg/share/MediaWiki/extensions/CreatePage/CreatePageCore.php");
+require_once("$IP/extensions/wikia/CreatePage/SpecialCreatePage.php");
+require_once("$IP/extensions/wikia/CreatePage/CreatePageCore.php");
 
 $dir = dirname(__FILE__);
 $wgExtensionMessagesFiles['Wikiwyg'] = $dir . '/Wikiwyg.i18n.php';
