5d440fdb486b9c6010e527db0f010b8e73a8ef5a
macbre <macbre@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-08-05 07:53:20 +0000

    Ads: fix for Opera (floating FAST)

--- wikia/releases/200808.1/skins/monaco/js/ads.js
+++ wikia/releases/200808.1/skins/monaco/js/ads.js
@@ -259,6 +259,11 @@ TieDivLib = new function() {
 					if(items[i][2] == 'FAST_HOME1' || items[i][2] == 'FAST_HOME2' || items[i][2] == 'FAST_TOP' || (items[i][2] == 'FAST_BOTTOM' && !YAHOO.lang.isUndefined(fast_bottom_type) && fast_bottom_type == 'FAST4')) {
 						if($(items[i][0]).style.right == '') {
 							$(items[i][0]).style.right = Dom.getDocumentWidth() - (Dom.getX(items[i][1]) + $(items[i][1]).offsetWidth) + 'px';
+							$(items[i][0]).style.left = '';
+						}
+						if(Math.abs(xy.x - Dom.getX(items[i][0])) > 15) { 
+							$(items[i][0]).style.left = xy.x - aX + 'px'; 
+							$(items[i][0]).style.right = ''; 
 						}
 					} else {
 						if($(items[i][0]).style.left == '') {
