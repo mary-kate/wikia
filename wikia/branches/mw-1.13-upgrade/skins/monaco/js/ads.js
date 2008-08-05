1ce9c564e972da5538728e514df2bb87518ee4a1
emil <emil@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-08-05 09:50:01 +0000

    200808.2 sync

--- wikia/branches/mw-1.13-upgrade/skins/monaco/js/ads.js
+++ wikia/branches/mw-1.13-upgrade/skins/monaco/js/ads.js
@@ -250,36 +250,35 @@ TieDivLib = new function() {
 			pos.sidebarW = $('widget_sidebar').offsetWidth;
 			pos.sidebarH = $('widget_sidebar').offsetHeight;
 			for(i = 0; i < items.length; i++) {
-				if(!rtl && (items[i][2].substring(0, 4) == 'FAST' || items[i][2] == 'bl' || items[i][2] == 'r')) {
+
+				if(Dom.getStyle(items[i][0], 'display') != 'block') {
+					Dom.setStyle(items[i][0], 'display', 'block');
+				}
+
+				if(!rtl) {
 					xy = getAbsolutePosition($(items[i][1]));
 					xy.x = Dom.getX(items[i][1]);
-					if(Dom.getStyle(items[i][0], 'display') != 'block') {
-						Dom.setStyle(items[i][0], 'display', 'block');
-					}
-					if(items[i][2] == 'FAST_HOME1' || items[i][2] == 'FAST_HOME2' || items[i][2] == 'FAST_TOP' || (items[i][2] == 'FAST_BOTTOM' && fast_bottom_type == 'FAST4')) {
-						if($(items[i][0]).style.right == '') {
-							$(items[i][0]).style.right = Dom.getDocumentWidth() - (Dom.getX(items[i][1]) + $(items[i][1]).offsetWidth) + 'px';
-							$(items[i][0]).style.left = '';
+					if(items[i][2].substring(0, 4) == 'FAST' || items[i][2] == 'bl' || items[i][2] == 'r') {
+						if(items[i][2] == 'FAST_HOME1' || items[i][2] == 'FAST_HOME2' || items[i][2] == 'FAST_TOP' || (items[i][2] == 'FAST_BOTTOM' && fast_bottom_type == 'FAST4')) {
+							if($(items[i][0]).style.right == '') {
+								$(items[i][0]).style.right = 5 + Dom.getX('wikia_page') + $('wikia_page').offsetWidth - (Dom.getX(items[i][1]) + $(items[i][1]).offsetWidth) + 'px';
+							}
+						} else {
+							if($(items[i][0]).style.left == '') {
+								$(items[i][0]).style.left = Dom.getX(items[i][1]) - aX + 'px';
+							}
 						}
-						if(Math.abs(xy.x - Dom.getX(items[i][0])) > 15) {
-							$(items[i][0]).style.left = xy.x - aX + 'px';
-							$(items[i][0]).style.right = '';
+						if(xy.y != parseFloat($(items[i][0]).style.top) + pos.shrink) {
+							$(items[i][0]).style.top = (xy.y - pos.shrink + aY) + 'px';
 						}
 					} else {
-						if($(items[i][0]).style.left == '') {
-							$(items[i][0]).style.left = Dom.getX(items[i][1]) - aX + 'px';
+						if(xy.y != parseFloat($(items[i][0]).style.top) + pos.shrink || xy.x != parseFloat($(items[i][0]).style.left)) {
+							$(items[i][0]).style.top = xy.y - pos.shrink + aY + 'px';
+							$(items[i][0]).style.left = xy.x - aX + 'px';
 						}
 					}
-					if(xy.y != parseFloat($(items[i][0]).style.top) + pos.shrink) {
-						$(items[i][0]).style.top = (xy.y - pos.shrink + aY) + 'px';
-					}
 				} else {
-					if(Dom.getStyle(items[i][0], 'display') != 'block') {
-						Dom.setStyle(items[i][0], 'display', 'block');
-					}
-					if(Dom.getXY(items[i][0]) != Dom.getXY(items[i][1])) {
-						Dom.setXY(items[i][0], Dom.getXY(items[i][1]));
-					}
+					Dom.setXY(items[i][0], Dom.getXY(items[i][1]));
 				}
 			}
 		}
