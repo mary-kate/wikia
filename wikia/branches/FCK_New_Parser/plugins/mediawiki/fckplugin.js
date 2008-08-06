4a1005e7aa2dddf24ed59d30a4dd4099f88eb683
bartek <bartek@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-08-06 12:37:49 +0000

    FCKeditor: parse templates separately also on ajax call

--- wikia/branches/FCK_New_Parser/plugins/mediawiki/fckplugin.js
+++ wikia/branches/FCK_New_Parser/plugins/mediawiki/fckplugin.js
@@ -755,7 +755,9 @@ FCK.DataProcessor =
 
 		var loadHTMLFromAjax = function( result )
 		{
-			FCK.EditingArea.Textarea.value = result.responseText ;
+			console.log (result.responseText) ;
+			var splitResult = result.responseText.split ("<FCK_SajaxResponse_splitter_tag/>") ;
+			FCK.EditingArea.Textarea.value = splitResult [0] ;
 			parent.document.getElementById ('FCKmode').value = 'html' ;
 			original.apply( FCK, args ) ;
 		}
