563b5626fec8d34adc032b7b72e10bbeec6130d1
Lucas 'Tor' Garczewski <tor@wikia-inc.com>
2008-08-07 14:42:37 +0000

    modified poll msgs to match reality (closes #3370)

--- wikia/releases/200808.2/extensions/wikia/Poll/poll.i18n.php
+++ wikia/releases/200808.2/extensions/wikia/Poll/poll.i18n.php
@@ -12,7 +12,7 @@ $messages = array(
 	        "pollVoteError"  => "There was a problem with processing your vote, please try again.",
 	        "pollPercentVotes" => "$1% of all votes",
 	        "pollYourVote"   => "You already voted for \"$1\" on $2, you can change your vote by clicking an answer below.",
-        	"pollNoVote"     => "Please vote below or <a href=\"\">click here</a> to see the results.",
+        	"pollNoVote"     => "Please vote below.",
 	        "pollInfo"       => "There were $1 votes since the poll was created on $2.",
 	        "pollSubmitting" => "Please wait, submitting your vote."
  	 ),
@@ -22,7 +22,7 @@ $messages = array(
 	        "pollVoteError"  => "Wystąpił błąd w czasie dodawania głosu, proszę spróbować później.",
 	        "pollPercentVotes" => "$1% wszystkich głosów",
 	        "pollYourVote"   => "Zagłosowałeś juz na \"$1\" $2, możesz zaktualizować swój głos klikając na odpowiedź poniżej.",
-	        "pollNoVote"     => "Podaj swój głos poniżej lub <a href=\"\">kliknij tu</a>, żeby zobaczyć oddane głosy.",
+	        "pollNoVote"     => "Podaj swój głos poniżej.",
 	        "pollInfo"       => "Oddano już $1 głosy/ów od założenia ankiety dnia $2.",
 	        "pollSubmitting" => "Proszę czekać, trwa dodawanie głosu."
 	),
