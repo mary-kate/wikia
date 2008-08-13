d5dabcc7326dab9ff2627f9c46632db0e215fc90
bartek <bartek@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-08-13 10:05:30 +0000

    FCKeditor: make upper links functionable

--- wikia/branches/FCK_New_Parser/FCKeditor.body.php
+++ wikia/branches/FCK_New_Parser/FCKeditor.body.php
@@ -452,7 +452,11 @@ function onLoadFCKeditor()
 
 			var saveButton = document.getElementById ('wpSave') ;			
 			var previewButton = document.getElementById ('wpPreview') ;				
-			var diffButton = document.getElementById ('wpDiff') ;				
+			var diffButton = document.getElementById ('wpDiff') ;	
+			var cancelButton = document.getElementById ('wpCancel') ;			
+			var upperSave = document.getElementById ('upperFCKSave') ;
+			var upperPreview = document.getElementById ('upperFCKPreview') ;
+			var upperCancel = document.getElementById ('upperFCKCancel') ;
 
 			if (FCKinstance.isInedible ()) {
                                 var fckIframe = document.getElementById ("wpTextbox1___Frame")  ;
@@ -472,6 +476,9 @@ function onLoadFCKeditor()
 			} else {
 				window.onbeforeunload = confirmExit ;
 				saveButton.onclick = previewButton.onclick = diffButton.onclick = function () {needToConfirm = false ;} ;
+				upperSave.onclick = function () {saveButton.click () ;} ;
+				upperPreview.onclick = function () {previewButton.click () ;} ;
+				upperCancel.onclick = function () {cancelButton.click () ;} ;
 			}
 			document.getElementById ('wpSummaryLabel').style.display = 'inline' ;
 			saveButton.parentNode.parentNode.style.display = 'inline' ;			
