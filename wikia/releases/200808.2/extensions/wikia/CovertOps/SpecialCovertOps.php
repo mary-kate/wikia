78b09804de446795657ef21db5b547f87401ddd1
tor <tor@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-08-18 08:03:02 +0000

    CovertOps: cleanup

--- wikia/releases/200808.2/extensions/wikia/CovertOps/SpecialCovertOps.php
+++ wikia/releases/200808.2/extensions/wikia/CovertOps/SpecialCovertOps.php
@@ -15,21 +15,20 @@
  */
 
 if (!defined('MEDIAWIKI')) {
-	echo "This is MediaWiki extension named SiteWideMessages.\n";
+	echo "This is MediaWiki extension named CovertOps.\n";
 	exit(1) ;
 }
 
 $wgExtensionCredits['specialpage'][] = array(
-	'name' => 'SiteWideMessages',
-	'author' => '[http://www.wikia.com/wiki/User:Marooned Maciej Błaszkowski (Marooned)]',
-	'description' => 'This extension provides an interface for sending messages seen on all wikis.'
+	'name' => 'CovertOps',
+	'author' => '[http://www.wikia.com/wiki/User:TOR Lucas \'TOR\' Garczewski]',
+	'description' => '[classified]'
 );
 //Allow group STAFF to use this extension.
 $wgAvailableRights[] = 'covertops';
 $wgGroupPermissions['*']['covertops'] = false;
 $wgGroupPermissions['staff']['covertops'] = true;
 
-$wgExtensionFunctions[] = 'CovertOpsInit';
 $wgExtensionMessagesFiles['CovertOps'] = dirname(__FILE__) . '/SpecialCovertOps.i18n.php';
 
 //Register special page
@@ -37,48 +36,3 @@ if (!function_exists('extAddSpecialPage')) {
 	require("$IP/extensions/ExtensionFunctions.php");
 }
 extAddSpecialPage(dirname(__FILE__) . '/SpecialCovertOps_body.php', 'CovertOps', 'CovertOps');
-/**
- * Initialize hooks
- *
- */
-function CovertOpsInit() {
-
-	/* ... */
-	
-}
-
-
-/**
- * Load JS/CSS for extension
- *
- */
-function SiteWideMessagesIncludeJSCSS( $skin, & $bottomScripts) {
-	global $wgExtensionsPath, $wgStyleVersion;
-
-	$bottomScripts .= "<script type=\"text/javascript\" src=\"$wgExtensionsPath/wikia/SiteWideMessages/SpecialSiteWideMessages.js?$wgStyleVersion\"></script>";
-
-	return true;
-}
-
-
-/**
- * Return a content of all user's messages and add CSS styles
- *
- */
-function SiteWideMessagesGetUserMessagesContent($dismissLink = true, $parse = true, $useForDiff = false, $addJSandCSS = true) {
-	global $wgExtensionsPath, $wgStyleVersion, $wgOut, $wgUser, $wgRequest;
-	if ($wgRequest->getText('diff') == '' || $useForDiff) {
-		wfLoadExtensionMessages('SpecialSiteWideMessages');
-
-		if ($addJSandCSS) {
-			global $wgHooks;
-			$wgHooks['SkinAfterBottomScripts'][] = 'SiteWideMessagesIncludeJSCSS';
-			$wgOut->AddScript("<link rel=\"stylesheet\" type=\"text/css\" href=\"$wgExtensionsPath/wikia/SiteWideMessages/SpecialSiteWideMessages.css?$wgStyleVersion\" />");
-		}
-
-		$content = SiteWideMessages::getAllUserMessages($wgUser, $dismissLink);
-		return $parse ? $wgOut->Parse($content) : $content;
-	} else {
-		return '';
-	}
-}
