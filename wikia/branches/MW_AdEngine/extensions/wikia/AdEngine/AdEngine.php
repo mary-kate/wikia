9e655d9cbd9df27cd1e023fd7ec299834c7c5755
nick <nick@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-08-18 23:19:04 +0000

    Putting back to original schema and coding conventions.

--- wikia/branches/MW_AdEngine/extensions/wikia/AdEngine/AdEngine.php
+++ wikia/branches/MW_AdEngine/extensions/wikia/AdEngine/AdEngine.php
@@ -7,12 +7,12 @@ $wgExtensionCredits['other'][] = array(
 
 interface iAdProvider {
 	public static function getInstance();
-    public function getAd($slotname, $slot);
+	public function getAd($slotname, $slot);
 }
 
 class AdEngine {
 
-	const cacheKeyVersion = "1.3";
+	const cacheKeyVersion = "1.4";
 
 	const cacheTimeout = 1800;
 
@@ -25,7 +25,7 @@ class AdEngine {
 	protected function __construct() {
 		$this->loadConfig();
 		foreach($this->providers as $provider) {
-			require('AdProvider'.$provider.'.php');
+			require(dirname(__FILE__) . '/AdProvider'.$provider.'.php');
 		}
 	}
 
@@ -37,46 +37,49 @@ class AdEngine {
 	}
 
 	public function loadConfig() {
-		$skin_name = 'monaco';
+		$skin_name = 'monaco'; // Hard code for now.
 		global $wgMemc, $wgCityId;
 
 		$cacheKey = wfMemcKey('slots', $skin_name, self::cacheKeyVersion);
 		$this->slots = $wgMemc->get($cacheKey);
 
-		if(empty($this->slots) && !is_array($this->slots)) {
-			$db = wfGetDB(DB_SLAVE);
-
-			$sql = "SELECT ad_slot.id, ad_slot.name, ad_slot.size,
-					COALESCE(adso.provider_id, ad_slot.provider_id) AS provider_id,
-					COALESCE(adso.enabled, ad_slot.enabled) AS enabled
-					FROM wikicities.ad_slot
-					LEFT OUTER JOIN wikicities.ad_slot_override AS adso
-					  ON ad_slot.id = adso.ad_slot_id AND city_id=".intval($wgCityId)."
-					WHERE skin='".$db->strencode($skin_name)."'";
-
-			$res = $db->query($sql);
-
-			while($row = $db->fetchObject($res)){
-				$this->slots[$row->name] = array(
-					'ad_slot_id' => $row->id,
-					'size' => $row->size,
-					'provider_id' => $row->provider_id,
-					'enabled' => $row->enabled
-				);
-			}
-
-			$sql = "SELECT * FROM wikicities.ad_provider_value WHERE
-				 (city_id = ".intval($wgCityId)." OR city_id IS NULL)";
-			$res = $db->query($sql);
-			while($row = $db->fetchObject($res)) {
-				 foreach($this->slots as $slotname => $slot) {
-				 	if($slot['provider_id'] == $row->provider_id){
-						$this->slots[$slotname]['provider_values'][$row->keyname] = $row->keyvalue;
-				 	}
-				 }
-			}
-			$wgMemc->set($cacheKey, $this->slots, self::cacheTimeout);
+		if(is_array($this->slots)){
+			// Found a cached value
+			return true;
 		}
+			
+		$db = wfGetDB(DB_SLAVE);
+
+		$sql = "SELECT ad_slot.as_id, ad_slot.slot, ad_slot.size,
+				COALESCE(adso.provider_id, ad_slot.default_provider_id) AS provider_id,
+				COALESCE(adso.enabled, ad_slot.default_enabled) AS enabled
+				FROM wikicities.ad_slot
+				LEFT OUTER JOIN wikicities.ad_slot_override AS adso
+				  ON ad_slot.as_id = adso.as_id AND city_id=".intval($wgCityId)."
+				WHERE skin='".$db->strencode($skin_name)."'";
+
+		$res = $db->query($sql);
+
+		while($row = $db->fetchObject($res)){
+			$this->slots[$row->slot] = array(
+				'as_id' => $row->as_id,
+				'size' => $row->size,
+				'provider_id' => $row->provider_id,
+				'enabled' => $row->enabled
+			);
+		}
+
+		$sql = "SELECT * FROM wikicities.ad_provider_value WHERE
+			 (city_id = ".intval($wgCityId)." OR city_id IS NULL)";
+		$res = $db->query($sql);
+		while($row = $db->fetchObject($res)) {
+			 foreach($this->slots as $slotname => $slot) {
+			 	if($slot['provider_id'] == $row->provider_id){
+					$this->slots[$slotname]['provider_values'][$row->keyname] = $row->keyvalue;
+			 	}
+			 }
+		}
+		$wgMemc->set($cacheKey, $this->slots, self::cacheTimeout);
 
 		return true;
 	}
@@ -86,7 +89,9 @@ class AdEngine {
 			$provider = $this->getAdProvider($this->slots[$slotname]['provider_id']);
 			return $provider->getAd($slotname, $this->slots[$slotname]);
 		} else {
-			throw new Exception();
+			// Note: Don't throw an exception here. Fail gracefully for ads,
+			// don't under any circumstances fail the rendering of the page
+			return "<!-- Bad Ad Call -->";
 		}
 	}
 
