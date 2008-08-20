6f88d046c2bdfc9ba3db7611717c028d7a3742e8
Piotr Molski <moli@wikia-inc.com>
2008-08-20 10:01:29 +0000

    new branche

---
+++ wikia/branches/moli/extensions/wikiwyg/Wikiwyg.php
@@ -0,0 +1,6 @@
+<?php
+
+include( "$IP/extensions/wikiwyg/share/MediaWiki/extensions/MediaWikiWyg.php" );
+include( "$IP/extensions/wikiwyg/share/MediaWiki/extensions/WikiwygEditing/WikiwygEditing.php" );
+include( "$IP/extensions/wikia/CreatePage/SpecialCreatePage.php" );
+include( "$IP/extensions/wikia/CreatePage/CreatePageCore.php" );
