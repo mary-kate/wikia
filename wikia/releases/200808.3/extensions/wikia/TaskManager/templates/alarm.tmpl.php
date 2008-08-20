e08d9ab65b96c63fd68e7db294610bf4bbafb56c
Krzysztof Krzyżaniak <k.krzyzaniak@gmail.com>
2008-08-20 13:21:04 +0000

    not merged change, fix url for task manager

--- wikia/releases/200808.3/extensions/wikia/TaskManager/templates/alarm.tmpl.php
+++ wikia/releases/200808.3/extensions/wikia/TaskManager/templates/alarm.tmpl.php
@@ -1,6 +1,7 @@
 Task no <?php echo $task->getId() ?> (<?php echo $task->getType()?>) stucked
 and was killed by Task manager.
 
-Go to <https://www.wikia.com/wiki/Special:TaskManager> and check situation.
+Go to <http://www.wikia.com/wiki/Special:TaskManagerr?action=log&id=<?php echo $task->getId() ?>>
+and check situation.
 
 Yours Task Manager
