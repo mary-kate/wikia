c0314cf375533e46cd3f3eccb9c1af6cd604c3ed
eloy <eloy@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-08-25 11:03:29 +0000

    set server_name as well, it's for proper redirecting to main domain. Ticket #3509, Ticket #5229

--- wikia/releases/200808.4/extensions/wikia/WikiFactory/Loader/WikiFactoryLoader.php
+++ wikia/releases/200808.4/extensions/wikia/WikiFactory/Loader/WikiFactoryLoader.php
@@ -58,16 +58,17 @@ class WikiFactoryLoader {
 	 * @access public
 	 * @author Krzysztof Krzyżaniak <eloy@wikia.com>
 	 *
-	 * @param integer $id default null: explicite set wiki id
+	 * @param integer $id default null	explicite set wiki id
+	 * @param string $server_name default null	explicite set server name
 	 *
 	 * @return WikiFactoryLoader object
 	 */
-	public function  __construct( $id = null ) {
+	public function  __construct( $id = null, $server_name = null ) {
 		global $wgDBname, $wgSharedDB, $wgDevelEnvironment, $wgDevelDomains;
 
 		if( !is_null( $id ) ) {
 			$this->mCityID = $id;
-			$this->mServerName = null;
+			$this->mServerName = $server_name;
 		}
 		elseif( !empty($_SERVER['SERVER_NAME'])) {
 			$this->mServerName = strtolower( $_SERVER['SERVER_NAME'] );
