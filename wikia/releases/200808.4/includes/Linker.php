4c1bfe78d2965a8dec38e10867bd62aabd21d3c8
christian <christian@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-08-26 00:06:43 +0000

    reverted abtesting r1093

--- wikia/releases/200808.4/includes/Linker.php
+++ wikia/releases/200808.4/includes/Linker.php
@@ -1259,12 +1259,7 @@ class Linker {
 	 * @return string HTML headline
 	 */
 	public function makeHeadline( $level, $attribs, $anchor, $text, $link ) {
-		global $abtest;
-		if ( $abtest == 3 || $abtest == 6 ) {
-			return "<a name=\"$anchor\"></a><h$level$attribs <span class=\"mw-headline\">$text</span>$link</h$level>";
-		} else {
-			return "<a name=\"$anchor\"></a><h$level$attribs$link <span class=\"mw-headline\">$text</span></h$level>";
-		}
+		return "<a name=\"$anchor\"></a><h$level$attribs$link <span class=\"mw-headline\">$text</span></h$level>";
 	}
 
 	/**
