25d43a2153f03bb9d871a0ef2dd44a6fa80c15f9
nick <nick@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-08-29 19:34:19 +0000

    wgCat and wgCatId aren't always available which breaks stuff, create a function that grabs it and returns it. Function has a memcache and static layer for performance

--- wikia/releases/200809.1/extensions/wikia/AdEngine/AdProviderDART.php
+++ wikia/releases/200809.1/extensions/wikia/AdEngine/AdProviderDART.php
@@ -100,10 +100,10 @@ EOT;
 	}
 
 	function getDartSite(){
-		global $wgCat;
-		if(!empty($wgCat['name'])) {
-			if(!empty($this->sites[$wgCat['name']])) {
-				return $this->sites[$wgCat['name']];
+		$cat=AdEngine::getCachedCategory();
+		if(!empty($cat['name'])) {
+			if(!empty($this->sites[$cat['name']])) {
+				return $this->sites[$cat['name']];
 			}
 		}
 		return 'wka.wikia';
