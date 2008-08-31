139d76a856a1737ccd776b110f170a9ded5fb7aa
Krzysztof Krzyżaniak <k.krzyzaniak@gmail.com>
2008-08-31 06:43:12 +0000

    Get the toggle back and forth between old / new Monaco working.
    Still work in progress.

--- wikia/releases/200809.1/skins/Monaco.php
+++ wikia/releases/200809.1/skins/Monaco.php
@@ -1,12 +1,13 @@
 <?php
 /* We need an easy way to switch back and forth between Old Monaco (AdServer) and New Monaco (AdEngine)
  * Monaco_AdServer, and the if statement below, can be removed once everyone is using $wgEnableAdEngine 
-if (@$wgEnableAdEngine != true){
+*/
+global $wgEnableAdEngine;
+if ($wgEnableAdEngine != true){
 	// Use the old code
         require dirname(__FILE__) . '/Monaco_200808.4.php';
         return;
 }
-*/
 
 /**
  * Monaco skin
