d74c3d5ceeb993f844fb1ae3409f40eb0d487c31
inez <inez@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-09-09 12:28:56 +0000

    * Initial import of WysiwygInterface extension - it's development extension

---
+++ wikia/branches/wysiwyg/extensions/wikia/WysiwygInterface/WysiwygInterface_body.php
@@ -0,0 +1,61 @@
+<?php
+
+class WysiwygParser extends Parser {
+
+	/**
+	 * Tag hook handler for 'pre'.
+	 */
+	function renderPreTag( $text, $attribs ) {
+		// Backwards-compatibility hack
+		$content = StringUtils::delimiterReplace( '<nowiki>', '</nowiki>', '$1', $text, 'i' );
+
+		$attribs = Sanitizer::validateTagAttributes( $attribs, 'pre' );
+		$attribs['washtml'] = 1;
+		return wfOpenElement( 'pre', $attribs ) .
+			Xml::escapeTagsOnly( $content ) .
+			'</pre>';
+	}
+
+}
+
+class WysiwygInterface extends SpecialPage {
+
+		function WysiwygInterface() {
+			SpecialPage::SpecialPage("WysiwygInterface");
+			wfLoadExtensionMessages('WysiwygInterface');
+		}
+
+		function execute( $par ) {
+			global $wgRequest, $wgOut, $wgTitle;
+			$this->setHeaders();
+
+			if(empty($par)) {
+				$wikitext = $wgRequest->getText('wikitext');
+				$wgOut->addHTML('<form method="POST"><textarea name="wikitext" style="height: 140px; width: 800px;">'.$wikitext.'</textarea><br /><br /><input type="submit" value="Post" /></form>');
+			} else {
+				$title = Title::newFromText($par);
+				if($title->exists()) {
+					$rev = Revision::newFromTitle($title);
+					$wikitext = $rev->getText();
+				} else {
+					$wikitext = '-';
+				}
+			}
+
+			$options = new ParserOptions();
+			$options->setTidy(true);
+
+			$parser = new WysiwygParser();
+			$parser->setOutputType(OT_HTML);
+			$out = $parser->parse($wikitext, $wgTitle, $options, false)->getText();
+			$out = htmlspecialchars($out);
+
+			$pre = new Preprocessor_DOM($parser);
+			print_pre($pre->preprocessToObj("<!---haha--->'''123'''"));
+			exit();
+
+			$wgOut->addHTML('<br />'.$out);
+			//$wgOut->addHTML('<br /><hr /><pre>'.print_r($parser->getElementsInfo(), true).'</pre>');
+		}
+
+}
