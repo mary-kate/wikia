9d64b49c3642e474a1f6fc7ffc23adeb5d336390
emil <emil@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-09-16 09:47:21 +0000

    200809.3 release

---
+++ wikia/releases/200809.3/extensions/wikia/UserChangesHistory/UserChangesHistory.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Register when & where user is logged in
+ *
+ * @author Krzysztof Krzyżaniak <eloy@wikia.com>
+ */
+
+if( $wgDefaultExternalStore && $wgEnableExternalStorage ) {
+	$wgHooks[ "UserLoginComplete" ][ ] = array( "UserChangesHistory::LoginHistoryHook", 1 /* UserChangesHistory::LOGIN_FORM */ );
+	#$wgHooks[ "UserLoadFromSession" ][ ] = array( "UserChangesHistory::LoginHistoryInsert", 0 /* UserChangesHistory::LOGIN_AUTO */ );
+	$wgHooks[ "SavePreferences" ][ ] = array( "UserChangesHistory::SavePreferencesHook" );
+	$wgHooks[ 'AddNewAccount' ][ ] = array( 'UserChangesHistory::LoginHistoryHook', 2 /* UserChangesHistory::LOGIN_REGISTRATION */ );
+
+	/**
+	 * load file with class
+	 */
+	$wgAutoloadClasses[ "UserChangesHistory" ] =  dirname(__FILE__) . "/UserChangesHistory.class.php";
+}
