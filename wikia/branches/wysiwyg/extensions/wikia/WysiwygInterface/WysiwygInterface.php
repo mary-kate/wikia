5614ad375b02e4371bec599d3d00ab3f4e720b43
inez <inez@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-09-18 12:06:54 +0000

    * Improve wikisyntax plugin to send wysiwygData information

--- wikia/branches/wysiwyg/extensions/wikia/WysiwygInterface/WysiwygInterface.php
+++ wikia/branches/wysiwyg/extensions/wikia/WysiwygInterface/WysiwygInterface.php
@@ -67,24 +67,24 @@ function wfReverseParserAjax($html, $data = array()) {
 }
 
 $wgAjaxExportList[] = 'wfWikisyntaxToHtml';
-function wfWikisyntaxToHtml($wikitext, $wysiwygData = null) {
+function wfWikisyntaxToHtml($wikitext) {
 	require(dirname(__FILE__).'/WysiwygInterface_body.php');
 	$options = new ParserOptions();
 	$title = new Title();
 
 	$parser = new WysiwygParser();
 	$parser->setOutputType(OT_HTML);
-	global $FCKparseEnable;
+	global $FCKparseEnable, $FCKmetaData;
 	$FCKparseEnable = true;
-	return $parser->parse($wikitext, $title, $options)->getText();
+	return $parser->parse($wikitext, $title, $options)->getText() . '--||--' . Wikia::json_encode($FCKmetaData, true);
 }
 
 $wgAjaxExportList[] = 'wfHtmlToWikisyntax';
-function wfHtmlToWikisyntax($html) {
-	$FCKmetaData = array();
+function wfHtmlToWikisyntax($html, $wysiwygData) {
+	$wysiwygData = Wikia::json_decode($wysiwygData);
 	// call ReverseParser to parse HTML back to wikimarkup
 	require(dirname(__FILE__).'/ReverseParser.php');
 	$reverseParser = new ReverseParser();
-	$result = $reverseParser->parse($html, $FCKmetaData);
+	$result = $reverseParser->parse($html, $wysiwygData);
 	return $result;
 }
