dbeb7b854b6b5701f2024583ecdce70955362031
inez <inez@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-09-18 21:05:12 +0000

    * Initial import of cleanedup Wysiwyg extension

---
+++ wikia/branches/wysiwyg/extensions/wikia/Wysiwyg/Wysiwyg_setup.php
@@ -0,0 +1,36 @@
+<?php
+$wgExtensionCredits['other'][] = array(
+	'name' => 'Wysiwyg',
+	'description' => 'FCKeditor integration for MediaWiki'
+);
+
+$wgHooks['EditPage::showEditForm:initial'][] = 'WysiwygInitial';
+
+function WysiwygInitial($form) {
+
+	// if namespace of edited article is main or image
+	if($form->mTitle->mNamespace == NS_MAIN || $form->mTitle->mNamespace == NS_IMAGE) {
+
+		// if article wikitext does not contain '<!-', '{{{' and '}}}'
+		if(!strpos($form->textbox1, '<!-') && !strpos($form->textbox1, '{{{') && !strpos($form->textbox1, '}}}')) {
+
+			global $wgExtensionsPath, $wgOut;
+			$script = '<script type="text/javascript" src="'.$wgExtensionsPath.'/wikia/Wysiwyg/fckeditor/fckeditor.js"></script>';
+			$script .= <<<EOT
+<script type="text/javascript">
+function initEditor() {
+	var oFCKeditor = new FCKeditor("wpTextbox1");
+	oFCKeditor.BasePath = "$wgExtensionsPath/wikia/Wysiwyg/fckeditor/";
+	oFCKeditor.ReplaceTextarea();
+}
+addOnloadHook(initEditor);
+</script>
+EOT;
+			$wgOut->addScript($script);
+
+		}
+
+	}
+
+	return true;
+}
