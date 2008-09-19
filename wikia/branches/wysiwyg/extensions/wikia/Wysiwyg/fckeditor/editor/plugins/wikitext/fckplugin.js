2dadac1cc2905a3bcbca44a78536c54241d17612
Inez Korczyński <inez@users.noreply.github.com>
2008-09-19 14:20:31 +0000

    * Important changes in ReverseParser and FCK plugin

--- wikia/branches/wysiwyg/extensions/wikia/Wysiwyg/fckeditor/editor/plugins/wikitext/fckplugin.js
+++ wikia/branches/wysiwyg/extensions/wikia/Wysiwyg/fckeditor/editor/plugins/wikitext/fckplugin.js
@@ -2,6 +2,7 @@
 FCKToolbarItems.RegisterItem( 'Source', new FCKToolbarButton( 'Source', 'Wikitext', null, FCK_TOOLBARITEM_ICONTEXT, true, true, 1 ) ) ;
 
 (function() {
+
 	var original = FCK.SwitchEditMode ;
 
 	FCK.SwitchEditMode = function() {
@@ -9,28 +10,41 @@ FCKToolbarItems.RegisterItem( 'Source', new FCKToolbarButton( 'Source', 'Wikitex
 		var args = arguments;
 
 		if(FCK.EditMode == FCK_EDITMODE_SOURCE) {
+			FCK.ToolbarSet.Items[0].Disable();
+			FCK.ToolbarSet.Items[0].RefreshState();
 
 			FCK.EditingArea.TargetElement.className = 'childrenHidden';
 
 			window.parent.sajax_request_type = 'POST';
-			window.parent.sajax_do_call('wfWysywigAjax', ['wiki2html', FCK.EditingArea.Textarea.value], function(res) {
-				FCK.EditingArea.Textarea.value = res.responseText;
+			window.parent.sajax_do_call('wfWysywigAjax', ['wiki2html', FCK.EditingArea.Textarea.value, false, window.parent.wgArticleId], function(res) {
+				var separator = res.getResponseHeader('X-sep');
+				if(typeof separator == "undefined") {
+					separator = res.getResponseHeader('X-Sep');
+				}
+				var res_array = res.responseText.split('--'+separator+'--');
+				window.parent.document.getElementById('wysiwygData').value = res_array[1];
+				FCK.EditingArea.Textarea.value = res_array[0];
 				FCK.EditingArea.TargetElement.className = '';
 				original.apply(FCK, args);
+				FCK.ToolbarSet.Items[0].Enable();
+				FCK.ToolbarSet.Items[0].RefreshState();
 			});
 
 		} else if(FCK.EditMode == FCK_EDITMODE_WYSIWYG) {
+			FCK.ToolbarSet.Items[0].Disable();
+			FCK.ToolbarSet.Items[0].RefreshState();
 			original.apply(FCK, args);
 		}
 
 	}
-})() ;
+})();
 
 
 FCK.DataProcessor.ConvertToDataFormat = function(rootNode, excludeRoot, ignoreIfEmptyParagraph, format) {
 	FCK.EditingArea.TargetElement.className = 'childrenHidden';
 
 	var html = FCKDataProcessor.prototype.ConvertToDataFormat.call(this, rootNode, excludeRoot, ignoreIfEmptyParagraph, format);
+	alert(html);
 	var wysiwygData = window.parent.document.getElementById('wysiwygData').value;
 
 	window.parent.sajax_request_type = 'POST';
@@ -38,6 +52,8 @@ FCK.DataProcessor.ConvertToDataFormat = function(rootNode, excludeRoot, ignoreIf
 		window.parent.document.getElementById('wysiwygData').value = '';
 		FCK.EditingArea.Textarea.value = res.responseText;
 		FCK.EditingArea.TargetElement.className = '';
+		FCK.ToolbarSet.Items[0].Enable();
+		FCK.ToolbarSet.Items[0].RefreshState();
 	});
 
 	return '';
--- wikia/branches/wysiwyg/extensions/wikia/Wysiwyg/wysiwyg_config.js
+++ wikia/branches/wysiwyg/extensions/wikia/Wysiwyg/wysiwyg_config.js
@@ -2,7 +2,7 @@ FCKConfig.FormatIndentator = '';
 FCKConfig.FontFormats = 'p;h1;h2;h3;pre' ;
 
 FCKConfig.ToolbarSets["Default"] = [
-	['Source','-','Cut','Copy','Paste','PasteText','Undo','Redo','-','Find','Replace','-','Bold','Italic','Underline','StrikeThrough','OrderedList','UnorderedList','Outdent','Indent','Link','Unlink','Table','-','FontFormat']
+	['Source','-','Cut','Copy','Paste','PasteText','Undo','Redo','-','Find','Replace','-','Bold','Italic','Underline','StrikeThrough','OrderedList','UnorderedList','Outdent','Indent','Link','Unlink','Table','Rule','-','FontFormat']
 ];
 
 FCKConfig.StyleVersion = window.parent.wgStyleVersion;
