206323b644b7176219031153e0d4d16064ab511d
moli <moli@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-09-23 10:58:01 +0000

    bug fixed

--- wikia/branches/moli/extensions/wikia/WikiaStats/templates/main-local-form.tmpl.php
+++ wikia/branches/moli/extensions/wikia/WikiaStats/templates/main-local-form.tmpl.php
@@ -546,8 +546,8 @@ YAHOO.util.Event.onDOMReady(function () {
 /*]]>*/
 </script>
 <input type="hidden" id="wk-stats-city-id" value="<?=$selCity?>">
-<fieldset style="border:2px solid red;">
-<legend class="legend-subtitle"><?=wfMsg('wikiastats_main_statistics_legend')?></legend>
+<fieldset>
+<legend><?=wfMsg('wikiastats_main_statistics_legend')?></legend>
 <div id="ws-upload">
 	<div id="ws-progress-bar"></div>
 	<div style="text-align:right; float:right; margin:0px 0px 0px 20px;">
