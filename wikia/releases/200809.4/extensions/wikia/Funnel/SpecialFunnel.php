b770744cad880a9c1576dabba42cf29ce8fecec7
emil <emil@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-09-23 10:36:17 +0000

    200809.4 release

---
+++ wikia/releases/200809.4/extensions/wikia/Funnel/SpecialFunnel.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * This extension will funnel all content pages and their derivatives that requested via index.php back into /wiki/Page_Name.
+ * Author Andrew Yasinky andrewy at wikia.com
+ */
+
+if (!defined('MEDIAWIKI')) {
+        echo <<<EOT
+To install this extension, put the following line in LocalSettings.php:
+require_once( "$IP/extensions/wikia/Funnel/SpecialFunnel.php" );
+EOT;
+    exit( 1 );
+}
+
+$wgExtensionFunctions[] = 'wfSpecialFunnel';
+$wgExtensionCredits['specialpage'][] = array(
+	'name' => 'Funnel',
+	'author' => 'Andrew Yasinsky',
+	'description' => 'Funnel Redirects',
+);
+
+$wgSpecialPages['Funnel'] = array( /*class*/ 'Funnel', /*name*/ 'Funnel', false, false );
+
+function wfSpecialFunnel() {
+  //init
+}
+
+function wfFunnel(){
+  global $IP, $wgMessageCache, $wgAutoloadClasses, $wgSpecialPages, $wgOut, $wgRequest, $wgTitle, $wgLanguageCode;
+ 
+  $prefix = '/wiki/';
+  
+  $isContent = $wgTitle->getNamespace() == NS_MAIN;
+
+  $params = array();
+
+  parse_str( $_SERVER['QUERY_STRING'], $params );  
+
+  if( ( $_SERVER['REQUEST_METHOD'] == 'GET' ) && ( $_SERVER['PHP_SELF'] == '/index.php' ) && ( trim( $params['title'] ) != '' ) && ( $isContent ) ){ 
+		
+		$page = $params['title'];
+		
+		unset( $params['title'] );
+		
+		$query = '';
+		
+		if( count($params) > 0 ){
+			$query = '?' . http_build_query( $params );
+		}
+		
+		$url = 'http://'.$_SERVER['SERVER_NAME'].$prefix.$page.$query;
+	  	header( "Location: {$url}", true, 301);
+		exit(0);
+		
+  }
+ 
+ return true;	 
+}
+
+if( !empty( $wgEnableSpecialFunnel ) ){
+	$wgHooks['BeforePageDisplay'][] = array( 'wfFunnel', array() );
+}
