fddfe60c4655fc0a4566b5a9ab4ccbaac59addf2
bartek <bartek@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-09-25 13:37:18 +0000

    SharedHelp: updating branch up to latest for this extension

--- wikia/branches/Bartek/extensions/wikia/SharedHelp/SharedHelp.php
+++ wikia/branches/Bartek/extensions/wikia/SharedHelp/SharedHelp.php
@@ -4,18 +4,29 @@
  * @subpackage SharedHelp
  *
  * @author Inez Korczynski <inez@wikia.com>
+ * @author Maciej Brencz <macbre(at)wikia-inc.com>
  */
 
 if(!defined('MEDIAWIKI')) {
 	exit( 1 );
 }
 
+$wgExtensionCredits['other'][] = array(
+        'name' => 'SharedHelp',
+	'version' => 0.11,
+        'description' => 'Takes pages from [[w:c:Help|Help Wikia]] and inserts them into Help namespace on this wiki',
+        'author' => array('Maciej Brencz, Inez Korczyński')
+);
+
 $wgHooks['OutputPageBeforeHTML'][] = 'SharedHelpHook';
+$wgHooks['EditPage::showEditForm:initial'][] = 'SharedHelpEditPageHook';
+$wgHooks['SearchBeforeResults'][] = 'SharedHelpSearchHook';
+$wgHooks['ParserReplaceLinkHolders'][] = 'SharedHelpReplaceLinkHolders';
 
 function SharedHelpHook(&$out, &$text) {
-	global $wgTitle, $wgMemc, $wgSharedDB, $wgDBname;
+	global $wgTitle, $wgMemc, $wgSharedDB, $wgDBname, $wgCityId;
 
-	if($wgDBname == 'help') { # Do not process for help.wikia.com
+	if($wgCityId == 3322) { # Do not process for help.wikia.com
 		return true;
 	}
 
@@ -25,63 +36,116 @@ function SharedHelpHook(&$out, &$text) {
 
 	if($wgTitle->getNamespace() == 12) { # Process only for pages in namespace Help (12)
 
-		$sharedArticlesKey = $wgSharedDB . ':' . 'sharedArticles';
-		$sharedArticles = $wgMemc->get($sharedArticlesKey);
+		$sharedArticleKey = $wgSharedDB . ':sharedArticles:' . $wgTitle->getDBkey();
+		$sharedArticle = $wgMemc->get($sharedArticleKey);
 
 		# Try to get content from memcache
-		if(is_array($sharedArticles)) {
-			if(isset($sharedArticles[$wgTitle->getDBkey()])) {
-				if(isset($sharedArticles[$wgTitle->getDBkey()]['timestamp'])) {
-			 		if(wfTimestamp() - (int) $sharedArticles[$wgTitle->getDBkey()]['timestamp'] < 600) {
-			 			if(isset($sharedArticles[$wgTitle->getDBkey()]['cachekey'])) {
-			 				$key1 = str_replace('-1!', '-0!', $sharedArticles[$wgTitle->getDBkey()]['cachekey']);
-			 				$key2 = str_replace('-0!', '-1!', $sharedArticles[$wgTitle->getDBkey()]['cachekey']);
-			 				$parser = $wgMemc->get($key1);
-			 				if(!empty($parser) && is_object($parser)) {
-			 					$content = $parser->mText;
-			 				} else {
-			 					$parser = $wgMemc->get($key2);
-			 					if(!empty($parser) && is_object($parser)) {
-			 						$content = $parser->mText;
-			 					}
-			 				}
-			 			} else if($sharedArticles[$wgTitle->getDBkey()]['exists'] == 0) {
-			 				return true;
-			 			}
-			 		}
-			 	}
+		if ( !empty($sharedArticle['timestamp']) ) {
+	 		if( (wfTimestamp() - (int) ($sharedArticle['timestamp'])) < 600) {
+	 			if(isset($sharedArticle['cachekey'])) {
+					wfDebug("SharedHelp: trying parser cache {$sharedArticle['cachekey']}\n");
+					$key1 = str_replace('-1!', '-0!', $sharedArticle['cachekey']);
+	 				$key2 = str_replace('-0!', '-1!', $sharedArticle['cachekey']);
+	 				$parser = $wgMemc->get($key1);
+	 				if(!empty($parser) && is_object($parser)) {
+						$content = $parser->mText;
+					} else {
+						$parser = $wgMemc->get($key2);
+						if(!empty($parser) && is_object($parser)) {
+							$content = $parser->mText;
+						}
+					}
+				} else if($sharedArticle['exists'] == 0) {
+	 				return true;
+				}
 			}
 		}
-
 		# If getting content from memcache failed (invalidate) then just download it via HTTP
 		if(empty($content)) {
-			$urlTemplate = "http://help.wikia.com/index.php?title=Help:%s&action=render";
+			global $wgDevelEnvironment;
+			if (empty($wgDevelEnvironment)) {
+				$urlTemplate = "http://help.wikia.com/index.php?title=Help:%s&action=render";
+			}
+			else {
+				$urlTemplate = "http://help.macbre.dev.poz.wikia-inc.com/index.php?title=Help:%s&action=render"; // for testing purposes
+			}
+
 			$articleUrl = sprintf($urlTemplate, $wgTitle->getDBkey());
 			$content = HTTP::get($articleUrl);
 
 			if(strpos($content, '"noarticletext"') > 0) {
-				$sharedArticles[$wgTitle->getDBkey()] = array('exists' => 0, 'timestamp' => wfTimestamp());
-				$wgMemc->set($sharedArticlesKey, $sharedArticles);
+				$sharedArticle = array('exists' => 0, 'timestamp' => wfTimestamp());
+				$wgMemc->set($sharedArticleKey, $sharedArticle);
 				return true;
 			} else {
-				$contentA = split("\n", $content);
+				$contentA = explode("\n", $content);
 				$tmp = $contentA[count($contentA)-2];
 				$idx1 = strpos($tmp, 'key');
 				$idx2 = strpos($tmp, 'end');
 				$key = trim(substr($tmp, $idx1+4, $idx2-$idx1));
-				$sharedArticles[$wgTitle->getDBkey()] = array('cachekey' => $key, 'timestamp' => wfTimestamp());
-				$wgMemc->set($sharedArticlesKey, $sharedArticles);
+				$sharedArticle = array('cachekey' => $key, 'timestamp' => wfTimestamp());
+				$wgMemc->set($sharedArticleKey, $sharedArticle);
+				wfDebug("SharedHelp: using parser cache {$sharedArticle['cachekey']}\n");
 			}
 		}
 
 		$content = preg_replace("|<span class=\"editsection\">\[<a href=\"(.*?)\" title=\"(.*?)\">(.*?)<\/a>\]<\/span>|", "", $content);
-		$content = str_replace("http://help.wikia.com/wiki/Help:","/wiki/Help:",$content);
+		$content = str_replace("http://help.wikia.com/wiki/Help:", "/wiki/Help:", $content);
+
+		// "this text is stored..."
+		$info = '<div class="sharedHelpInfo" style="text-align: right; font-size: smaller;padding: 5px">' . wfMsgExt('shared_help_info', 'parseinline', $wgTitle->getDBkey()) . '</div>';
 
 		if(strpos($text, '"noarticletext"') > 0) {
-			$text = "<div style='border: solid 1px;padding: 10px;margin: 5px;'>" . $content . "</div>";
+			$text = '<div style="border: solid 1px; padding: 10px; margin: 5px" class="sharedHelp">' . $info . $content . '</div>';
 		} else {
-			$text = "<div style='border: solid 1px;padding: 10px;margin: 5px;'>" . $content . "</div><br/>" . $text;
+			$text = '<div style="border: solid 1px; padding: 10px; margin: 5px" class="sharedHelp">' . $info . $content . '</div><br/>' . $text;
 		}
+	}
+	return true;
+}
+
+function SharedHelpEditPageHook(&$editpage) {
+	global $wgTitle, $wgCityId;
+
+	// do not show this message on help.wikia.com
+	if ($wgCityId == 3322) {
+		return true;
+	}
+
+	// show message only when editing pages from Help namespace
+	if ( $wgTitle->getNamespace() != 12 ) {
+		return true;
+	}
+
+	$msg = '<div style="border: solid 1px; padding: 10px; margin: 5px" class="sharedHelpEditInfo">'.wfMsgExt('shared_help_edit_info', 'parseinline', $wgTitle->getDBkey()).'</div>';
+
+	$editpage->editFormPageTop .= $msg;
+
+	return true;
+}
+
+function SharedHelpSearchHook(&$searchPage, &$term) {
+	global $wgOut, $wgCityId;
+
+	// do not show this message on help.wikia.com
+	if ($wgCityId == 3322) {
+		return true;
+	}
+
+	$msg = '<div style="border: solid 1px; padding: 10px; margin: 5px" class="sharedHelpSearchInfo plainlinks">'.wfMsgExt('shared_help_search_info', 'parseinline', urlencode($term)).'</div>';
+
+	$wgOut->addHTML($msg);
+
+	return true;
+}
+
+
+function SharedHelpReplaceLinkHolders($title, $colours, $key) {
+	if ($title->getNamespace() == 12) {
+		$dbr = wfGetDB( DB_SLAVE );
+		$table = 'help.'.$dbr->tableName('page');
+		$page_title = $dbr->selectField($table, 'page_title', array('page_namespace' => 12, 'page_title' => $title->getDBkey()), __METHOD__);
+		$colours[$key] = $page_title !== false ? '' : 'new';
 	}
 	return true;
 }
