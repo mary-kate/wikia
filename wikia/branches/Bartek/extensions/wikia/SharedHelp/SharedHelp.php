c1d4550217ffc9f6e12cf420d7cb43546a9bc325
bartek <bartek@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-09-26 11:07:20 +0000

    SharedHelp: extending the existing class, I will need that

--- wikia/branches/Bartek/extensions/wikia/SharedHelp/SharedHelp.php
+++ wikia/branches/Bartek/extensions/wikia/SharedHelp/SharedHelp.php
@@ -23,6 +23,80 @@ $wgHooks['EditPage::showEditForm:initial'][] = 'SharedHelpEditPageHook';
 $wgHooks['SearchBeforeResults'][] = 'SharedHelpSearchHook';
 $wgHooks['ParserReplaceLinkHolders'][] = 'SharedHelpReplaceLinkHolders';
 
+class SharedHttp extends Http {
+
+	static function request() {
+		global $wgHTTPTimeout, $wgHTTPProxy, $wgVersion, $wgTitle;
+
+		wfDebug( __METHOD__ . ": $method $url\n" );
+		# Use curl if available
+		if ( function_exists( 'curl_init' ) ) {
+			$c = curl_init( $url );
+			if ( self::isLocalURL( $url ) ) {
+				curl_setopt( $c, CURLOPT_PROXY, 'localhost:80' );
+			} else if ($wgHTTPProxy) {
+				curl_setopt($c, CURLOPT_PROXY, $wgHTTPProxy);
+			}
+
+			if ( $timeout == 'default' ) {
+				$timeout = $wgHTTPTimeout;
+			}
+			curl_setopt( $c, CURLOPT_TIMEOUT, $timeout );
+			curl_setopt( $c, CURLOPT_USERAGENT, "MediaWiki/$wgVersion" );
+			if ( $method == 'POST' )
+				curl_setopt( $c, CURLOPT_POST, true );
+			else
+				curl_setopt( $c, CURLOPT_CUSTOMREQUEST, $method );
+
+			# Set the referer to $wgTitle, even in command-line mode
+			# This is useful for interwiki transclusion, where the foreign
+			# server wants to know what the referring page is.
+			# $_SERVER['REQUEST_URI'] gives a less reliable indication of the
+			# referring page.
+			if ( is_object( $wgTitle ) ) {
+				curl_setopt( $c, CURLOPT_REFERER, $wgTitle->getFullURL() );
+			}
+
+			ob_start();
+			curl_exec( $c );
+			$text = ob_get_contents();
+			ob_end_clean();
+
+
+                        # Don't return the text of error messages, return false on error
+                        if ( curl_getinfo( $c, CURLINFO_HTTP_CODE ) != 200 ) {
+                                $text = false;
+                        }
+                        # Don't return truncated output
+                        if ( curl_errno( $c ) != CURLE_OK ) {
+                                $text = false;
+                        }
+                        curl_close( $c );
+                } else {
+                        # Otherwise use file_get_contents...
+                        # This may take 3 minutes to time out, and doesn't have local fetch capabilities
+
+                        global $wgVersion;
+                        $headers = array( "User-Agent: MediaWiki/$wgVersion" );
+                        if( strcasecmp( $method, 'post' ) == 0 ) {
+                                // Required for HTTP 1.0 POSTs
+                                $headers[] = "Content-Length: 0";
+                        }
+                        $opts = array(
+                                'http' => array(
+                                        'method' => $method,
+                                        'header' => implode( "\r\n", $headers ) ) );
+                        $ctx = stream_context_create($opts);
+
+                        $url_fopen = ini_set( 'allow_url_fopen', 1 );
+                        $text = file_get_contents( $url, false, $ctx );
+                        ini_set( 'allow_url_fopen', $url_fopen );
+                }
+                return $text;
+	}
+
+}
+
 function SharedHelpHook(&$out, &$text) {
 	global $wgTitle, $wgMemc, $wgSharedDB, $wgDBname, $wgCityId, $wgHelpWikiId;
 
@@ -71,7 +145,7 @@ function SharedHelpHook(&$out, &$text) {
 			}
 
 			$articleUrl = sprintf($urlTemplate, $wgTitle->getDBkey());
-			$content = HTTP::get($articleUrl);
+			$content = SharedHttp::get($articleUrl);
 
 			if(strpos($content, '"noarticletext"') > 0) {
 				$sharedArticle = array('exists' => 0, 'timestamp' => wfTimestamp());
