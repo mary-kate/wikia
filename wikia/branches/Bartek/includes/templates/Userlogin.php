d43c486b45daa6cb52628b3bfbf74615f3ef867f
bartek <bartek@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-10-02 12:55:27 +0000

    RegistrationPage: restore this

--- wikia/branches/Bartek/includes/templates/Userlogin.php
+++ wikia/branches/Bartek/includes/templates/Userlogin.php
@@ -25,6 +25,7 @@ class UserloginTemplate extends QuickTemplate {
 
 <div id="userloginForm">
 <form name="userlogin" method="post" action="<?php $this->text('action') ?>">
+	<h2><?php $this->msg('login') ?></h2>
 	<p id="userloginlink"><?php $this->html('link') ?></p>
 	<?php $this->html('header'); /* pre-table point for form plugins... */ ?>
 	<div id="userloginprompt"><?php  $this->msgWiki('loginprompt') ?></div>
