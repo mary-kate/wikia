b155d5a153f3ab81cd8d1c5f1c1c5ff502fd9f33
Maciej Brencz <maciej.brencz@gmail.com>
2008-10-03 22:02:14 +0000

    Wysiwyg: changes requested by Angie

--- wikia/branches/wysiwyg/extensions/wikia/Wysiwyg/Wysiwyg.i18n.php
+++ wikia/branches/wysiwyg/extensions/wikia/Wysiwyg/Wysiwyg.i18n.php
@@ -2,10 +2,10 @@
 $messages = array();
 $messages['en'] = array(
 	'wysiwyg' => 'FCKeditor integration for MediaWiki',
-	'wysiwyg-edgecase-info' => 'Wysiwyg editing has been disabled for this page because:',
-	'wysiwyg-edgecase-comment' => 'this page contains HTML comments',
-	'wysiwyg-edgecase-triplecurls' => 'this page contains template parameters',
-	'wysiwyg-edgecase-nowysiwyg' => 'this page contains the "no wysiwyg" command',
-	'wysiwyg-edgecase-complex-description' => 'this page contains an external URL or template within a link description',
-	'wysiwyg-edgecase-template-with-link' => 'this page contains a template with a link as a parameter',
+	'wysiwyg-edgecase-info' => 'Rich text editing has been disabled for this page because article contains:',
+	'wysiwyg-edgecase-comment' => 'HTML comments',
+	'wysiwyg-edgecase-triplecurls' => 'template parameters',
+	'wysiwyg-edgecase-nowysiwyg' => 'the "no wysiwyg" command',
+	'wysiwyg-edgecase-complex-description' => 'an external URL or template within a link description',
+	'wysiwyg-edgecase-template-with-link' => 'a template with a link as a parameter',
 );
