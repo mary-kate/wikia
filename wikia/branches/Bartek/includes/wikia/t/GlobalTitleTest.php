1be34033c92a84de8fc54109ab9b392eb746eb2c
bartek <bartek@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-10-22 18:41:01 +0000

    private branch update

--- wikia/branches/Bartek/includes/wikia/t/GlobalTitleTest.php
+++ wikia/branches/Bartek/includes/wikia/t/GlobalTitleTest.php
@@ -47,6 +47,13 @@ class GlobalTitleCase extends UnitTestCase {
 		$this->assertTrue( $title->getFullURL() === $url, sprintf("%s = %s, NOT MATCH", $title->getFullURL(), $url ) );
 
 		/**
+		 * spaces
+		 */
+		$title = GlobalTitle::newFromText( "Test Ze Spacjami", NS_TALK, 177 );
+		$url = "http://www.wikia.com/wiki/Talk:Test_Ze_Spacjami";
+		$this->assertTrue( $title->getFullURL() === $url, sprintf("%s = %s, NOT MATCH", $title->getFullURL(), $url ) );
+
+		/**
 		 * Polish wikia
 		 */
 		$title = GlobalTitle::newFromText( "WikiFactory", NS_SPECIAL, 1686 ); # pl.wikia.com
@@ -57,6 +64,9 @@ class GlobalTitleCase extends UnitTestCase {
 		$this->assertTrue( $title->getFullURL( wfArrayToCGI(array( "diff" => 0, "oldid" => 500 ) ) ) === $url, sprintf("%s = %s, NOT MATCH", $title->getFullURL(), $url ) );
 		;
 
+		$title = GlobalTitle::newFromText( "Strona główna", false, 1686 ); # pl.wikia.com
+		$url = "http://pl.wikia.com/wiki/Strona_g%C5%82%C3%B3wna?diff=0&oldid=500";
+		$this->assertTrue( $title->getFullURL( wfArrayToCGI(array( "diff" => 0, "oldid" => 500 ) ) ) === $url, "NOT MATCH" );
 	}
 
 };
