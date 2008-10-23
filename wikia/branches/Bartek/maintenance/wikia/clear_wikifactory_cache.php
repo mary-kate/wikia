15a4722b40e4e50412ebdc3f40f15f6a0aeb8954
bartek <bartek@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-10-23 09:09:02 +0000

    private branch update, maintenance

--- wikia/branches/Bartek/maintenance/wikia/clear_wikifactory_cache.php
+++ wikia/branches/Bartek/maintenance/wikia/clear_wikifactory_cache.php
@@ -7,39 +7,40 @@
  *
  * Usage:
  * (particular wiki)
- * maintenance/wikia/clear_wikifactory_cache.php -i <city_id_from_city_list>
+ * SERVER_ID=<city_id_from_city_list> php maintenance/wikia/clear_wikifactory_cache.php
  *
  * or
  * (whole cache, all wikis)
- * maintenance/wikia/clear_wikifactory_cache.php
+ * SERVER_ID=177 php maintenance/wikia/clear_wikifactory_cache.php --all
  */
 
 ini_set( "include_path", dirname(__FILE__)."/.." );
 require_once( "commandLine.inc" );
 
-$wikiId = isset( $options['i'] ) ? $options['i'] : null;
 
-if( is_null( $wikiId ) ) {
-    WikiFactory::clearCache( $wikiId );
+$all = isset( $options[ "all" ] ) ? $options[ "all" ] : false;
+
+if ( ! $all ) {
+	WikiFactory::clearCache( $wgCityId );
+	printf("%s removing %5d:%s from cache\n", wfTimestamp( TS_DB, time() ), $wgCityId, $wgDBname );
 }
 else {
-    $dbw = wfGetDB( DB_SLAVE );
+	/**
+	 * iterate through city_list
+	 */
+	$dbr = wfGetDB( DB_SLAVE );
 
-    $oRes = $dbw->select(
-    	wfSharedTable("city_list"),
-    	array( "city_id", "city_dbname" ),
-    	array( "city_public = 1"),
-    	__FILE__,
-    	array( "ORDER BY" => "city_id" )
-    );
+	$res = $dbr->select(
+		wfSharedTable( "city_list" ),
+		array( "city_id", "city_dbname" ),
+		array( "city_public"  => 1 ),
+		__FILE__,
+		array( "ORDER BY" => "city_id" )
+	);
 
-    $aWikis = array();
-
-    while ( $oRow = $dbw->fetchObject( $oRes ) ) {
-    	WikiFactory::clearCache( $oRow->city_id );
-    	echo "{$oRow->city_id}\t{$oRow->city_dbname}\tremoved from cache\n";
-    }
-
-    $dbw->freeResult( $oRes );
-    $dbw->close();
+	while ( $row = $dbr->fetchObject( $res ) ) {
+		WikiFactory::clearCache( $row->city_id );
+		printf("%s removing %5d:%s from cache\n", wfTimestamp( TS_DB, time() ), $row->city_id, $row->city_dbname  );
+	}
+	$dbr->close();
 }
