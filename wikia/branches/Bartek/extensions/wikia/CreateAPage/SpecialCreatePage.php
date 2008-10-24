6fe8f30e1f8bab9c4eb0f71175577462b13f1323
bartek <bartek@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-10-24 09:33:43 +0000

    CreateAPage: extension version++

--- wikia/branches/Bartek/extensions/wikia/CreateAPage/SpecialCreatePage.php
+++ wikia/branches/Bartek/extensions/wikia/CreateAPage/SpecialCreatePage.php
@@ -23,7 +23,7 @@ $wgExtensionCredits['specialpage'][] = array(
    'name' => 'CreatePage',
    'author' => 'Bartek Łapiński, Lucas \'TOR\' Garczewski, Przemek Piotrowski'  ,
    'url' => 'http://help.wikia.com/wiki/Help:CreatePage' ,
-   'version' => '3.63' ,
+   'version' => '3.64' ,
    'description' => 'easy to use interface for creating new articles'
 );
 
