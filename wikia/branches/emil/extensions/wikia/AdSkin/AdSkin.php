ed56f3996abc57d0f3adcf38532c29eb5db613f7
emil <emil@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-10-31 11:06:01 +0000

    new priv branch

---
+++ wikia/branches/emil/extensions/wikia/AdSkin/AdSkin.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * Author: Christian Williams 
+ */
+
+$wgHooks['GetHTMLAfterBody'][] = 'RenderAdSkin';
+		
+function RenderAdSkin() {
+	global $wgAdSkin, $wgExtensionsPath, $wgUser, $wgStyleVersion;
+
+	// Disable for logged in users
+	if (is_object($wgUser) && $wgUser->isLoggedIn() ){
+		return true;
+	}
+
+	if (isset($wgAdSkin)) {
+		switch ($wgAdSkin) {
+		case "wow_lich_king":
+			echo '<link rel="stylesheet" type="text/css" href="'. $wgExtensionsPath .'/wikia/AdSkin/css/wow_lich_king.css?'. $wgStyleVersion .'" />';
+			break;
+		case "wow_lich_king_warhammer":
+			echo '<link rel="stylesheet" type="text/css" href="'. $wgExtensionsPath .'/wikia/AdSkin/css/wow_lich_king_warhammer.css?'. $wgStyleVersion .'" />';
+			break;
+		}	
+	}
+
+	return true;
+}
