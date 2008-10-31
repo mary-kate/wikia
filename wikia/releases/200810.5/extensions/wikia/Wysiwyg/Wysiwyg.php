35eaf799663613f23b4c0d9548273eb236709c51
inez <inez@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-10-31 10:09:08 +0000

    * Wysiwyg: Remove code which is already moved to Wysiwyg_SetDomain function

--- wikia/releases/200810.5/extensions/wikia/Wysiwyg/Wysiwyg.php
+++ wikia/releases/200810.5/extensions/wikia/Wysiwyg/Wysiwyg.php
@@ -108,15 +108,6 @@ function Wysiwyg_Initial($form) {
 	$script = <<<EOT
 <script type="text/javascript" src="$wgExtensionsPath/wikia/Wysiwyg/fckeditor/fckeditor.js?$wgStyleVersion"></script>
 <script type="text/javascript">
-/*
-if(document.domain != 'localhost') {
-	var chunks = document.domain.split('.');
-	var d = chunks.pop(); // com
-	d = chunks.pop() + '.' + d; // wikia.com
-	document.domain = d;
-}
-*/
-
 function FCKeditor_OnComplete(editorInstance) {
 	editorInstance.LinkedField.form.onsubmit = function() {
 		if(editorInstance.EditMode == FCK_EDITMODE_SOURCE) {
