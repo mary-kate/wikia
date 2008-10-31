19bdf4b7c8c1c0ccc13b01e6b0e2a7b5f4105419
Inez Korczyński <inez@users.noreply.github.com>
2008-10-31 09:56:40 +0000

    * Wysiwyg: Temp-fix for document.domain

--- wikia/releases/200810.5/extensions/wikia/Wysiwyg/fckeditor/editor/plugins/wikitext/fckplugin.js
+++ wikia/releases/200810.5/extensions/wikia/Wysiwyg/fckeditor/editor/plugins/wikitext/fckplugin.js
@@ -209,7 +209,10 @@ FCK.Tracker = (typeof window.parent.YAHOO != 'undefined' && typeof window.parent
 
 FCK.Track = function(fakeUrl) {
 	if (FCK.Tracker) {
-//		FCK.Tracker.trackByStr(null, 'wysiwyg'+fakeUrl);
+		var cur_document_domain = document.domain;
+		document.domain = window.parent.org_document_domain;
+		FCK.Tracker.trackByStr(null, 'wysiwyg'+fakeUrl);
+		document.domain = cur_document_domain;
 	}
 }
 
