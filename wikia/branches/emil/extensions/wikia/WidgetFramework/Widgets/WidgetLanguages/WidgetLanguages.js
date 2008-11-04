bd996326d867c5e8f40bea62390112bd981dcfd4
emil <emil@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-11-04 10:47:34 +0000

    no longer needed

--- wikia/branches/emil/extensions/wikia/WidgetFramework/Widgets/WidgetLanguages/WidgetLanguages.js
+++
@@ -1,5 +0,0 @@
-function WidgetLanguagesHandleRedirect(elem) {
-	var selected = elem.options[elem.selectedIndex];
-	YAHOO.Wikia.Tracker.trackByStr(null, 'widget/WidgetLanguages/' + selected.text);
-	window.location.href = selected.value;
-}
