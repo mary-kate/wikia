d5fd241ab8e36c4ffb01e7dc80374da1cf3dd3a5
Krzysztof Krzyżaniak <k.krzyzaniak@gmail.com>
2008-11-04 12:58:03 +0000

    styleversion++

--- wikia/releases/200811.1/includes/wikia/DefaultSettings.php
+++ wikia/releases/200811.1/includes/wikia/DefaultSettings.php
@@ -27,9 +27,9 @@ if( !defined( 'MEDIAWIKI' ) ) {
  * Use revision number
  */
 
-$wgStyleVersion         = '2881';
-$wgMergeStyleVersionJS  = '2881';
-$wgMergeStyleVersionCSS = '2881';
+$wgStyleVersion         = '3200.';
+$wgMergeStyleVersionJS  = '3200';
+$wgMergeStyleVersionCSS = '3200';
 
 /**
  * @name $wgCityId
