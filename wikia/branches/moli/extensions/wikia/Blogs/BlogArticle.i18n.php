301614c49def5a7831426650ce92914c85e4a7f2
moli <moli@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-11-05 14:40:13 +0000

    progress

--- wikia/branches/moli/extensions/wikia/Blogs/BlogArticle.i18n.php
+++ wikia/branches/moli/extensions/wikia/Blogs/BlogArticle.i18n.php
@@ -4,6 +4,6 @@ $wgBlogArticleMessages = array();
 
 $wgBlogArticleMessages["en"] = array(
 	'blog_invalidparams' => "Invalid parameters",
-	'blog_invalidparam' => "Invalid parameter: $1",
-	'blog_useoneparam' => "Please use one of params: $1",
+	'blog_invalidparam' => "Unknown parameter '$1' is ignored. Please use one of available parameters: <code>$2</code>.",
+	'blog_emptyparam' => "Name of parameter cannot be empty",
 );
