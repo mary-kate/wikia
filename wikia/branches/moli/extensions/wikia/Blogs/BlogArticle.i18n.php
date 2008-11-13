61990d0d7f74c6029c5eb83958e0bf8706d07686
moli <moli@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-11-13 16:45:59 +0000

    blogs - commit changes in branch

--- wikia/branches/moli/extensions/wikia/Blogs/BlogArticle.i18n.php
+++ wikia/branches/moli/extensions/wikia/Blogs/BlogArticle.i18n.php
@@ -6,4 +6,5 @@ $wgBlogArticleMessages["en"] = array(
 	'blog_invalidparams' => "Invalid parameters",
 	'blog_invalidparam' => "Unknown parameter '$1' is ignored. Please use one of available parameters: <code>$2</code>.",
 	'blog_emptyparam' => "Name of parameter cannot be empty",
+	'blog_defaulttitle' => "Blogs",
 );
