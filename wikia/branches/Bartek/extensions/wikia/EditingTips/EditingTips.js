6fbca7597411605ccf0b7d8cf0d3b57dd7679112
bartek <bartek@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-11-24 14:13:57 +0000

    EditingTips: correct the condition

--- wikia/branches/Bartek/extensions/wikia/EditingTips/EditingTips.js
+++ wikia/branches/Bartek/extensions/wikia/EditingTips/EditingTips.js
@@ -92,7 +92,7 @@ YAHOO.util.Event.addListener("toggleWideScreen", "click", function(e) {
 YAHOO.util.Event.onDOMReady(function() {
 	var editingTipsHeaders = $('editingTips').getElementsByTagName("dt");
 
-	if (et_widescreen) {
+	if (1 == et_widescreen) {
                 YAHOO.util.Dom.addClass(document.body, "editingWide");
                 YAHOO.util.Dom.addClass(document.body, "editingTips");
                 if($("toggleWideScreen")) {
