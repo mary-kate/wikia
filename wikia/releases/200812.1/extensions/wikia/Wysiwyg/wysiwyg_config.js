9cadb33d7fa7e7f771ba80a9385784a3fa62e8c2
macbre <macbre@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-12-03 08:44:46 +0000

    Wysiwyg: remove "junk" from FCK toolbar

--- wikia/releases/200812.1/extensions/wikia/Wysiwyg/wysiwyg_config.js
+++ wikia/releases/200812.1/extensions/wikia/Wysiwyg/wysiwyg_config.js
@@ -4,6 +4,7 @@ FCKConfig.FontFormats = 'p;h2;h3;pre' ;
 FCKConfig.ToolbarSets["Default"] = [
 	['Source','-','Undo','Redo','-','Bold','Italic','Underline','StrikeThrough','Link','Unlink','-','FontFormat','-','OrderedList','UnorderedList','Outdent','Indent','-','AddImage','Table','Tildes','Rule','InsertTemplate','FitWindow']
 ];
+FCKConfig.ToolbarCanCollapse = false;
 
 FCKConfig.StyleVersion = window.parent.wgStyleVersion;
 FCKConfig.EditorAreaCSS = FCKConfig.BasePath + 'css/fck_editorarea.css';
