fd88074cdadb90ebf50ea6943a60023ccafb2ffa
bartek <bartek@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-12-05 15:12:01 +0000

    UserMailer: use a localised language object instead, fix for rt1294

--- wikia/branches/Bartek/includes/UserMailer.php
+++ wikia/branches/Bartek/includes/UserMailer.php
@@ -581,8 +581,12 @@ class EmailNotification {
 		# $PAGEEDITDATE is the time and date of the page change
 		# expressed in terms of individual local time of the notification
 		# recipient, i.e. watching user
+		
+		// RT #1294 Bartek 5.12.2008, use the language object with the code the user does
+		$wlang = Language::factory( $watchingUser->getOption( 'language' ) );	
+
 		$body = str_replace('$PAGEEDITDATE',
-			$wgLang->timeanddate( $this->timestamp, true, false, $timecorrection ),
+			$wlang->timeanddate( $this->timestamp, true, false, $timecorrection ),
 			$body);
 
 		return UserMailer::send($to, $this->from, $this->subject, $body, $this->replyto);
