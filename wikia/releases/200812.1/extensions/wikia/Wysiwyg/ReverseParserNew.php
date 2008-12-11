2dd0c8f469853170b31e79675e02f89d7acd886e
Inez Korczyński <inez@users.noreply.github.com>
2008-12-11 10:35:50 +0000

    * Wysiwyg: ReverseParserNew: Support for tags with washtml=true

--- wikia/releases/200812.1/extensions/wikia/Wysiwyg/ReverseParserNew.php
+++ wikia/releases/200812.1/extensions/wikia/Wysiwyg/ReverseParserNew.php
@@ -81,111 +81,137 @@ class ReverseParser {
 		$textContent = ($childOut != '') ? $childOut : $node->textContent;
 
 		if($node->nodeType == XML_ELEMENT_NODE) {
-			$refid = $node->getAttribute('refid');
 
-			if(is_numeric($refid)) {
-				$nodeData = $this->data[$refid];
-			}
+			$washtml = $node->getAttribute('washtml');
 
-			switch($node->nodeName) {
-				case 'body':
-					$out = $textContent;
-					break;
+			if(empty($washtml)) {
 
-				case 'br':
-					// e.g. "<br/><!--NEW_LINE_1-->" => "\n"
-					if($node->nextSibling && $node->nextSibling->nodeType == XML_COMMENT_NODE && $node->nextSibling->data == "NEW_LINE_1") {
-						$out = "\n";
+				$refid = $node->getAttribute('refid');
+
+				if(is_numeric($refid)) {
+					$nodeData = $this->data[$refid];
+				}
+
+				switch($node->nodeName) {
+					case 'body':
+						$out = $textContent;
+						break;
+
+					case 'br':
+						// e.g. "<br/><!--NEW_LINE_1-->" => "\n"
+						if($node->nextSibling && $node->nextSibling->nodeType == XML_COMMENT_NODE && $node->nextSibling->data == "NEW_LINE_1") {
+							$out = "\n";
+						}
+						break;
+
+					case 'p':
+						// if the first previous XML_ELEMENT_NODE (so no text and no comment) of the current
+						// node is <p> then add new line before the current one
+						if(($previousNode = $this->getPreviousElementNode($node)) && $previousNode->nodeName == 'p') {
+							$textContent = "\n" . $textContent;
+						}
+
+						$out = $textContent;
+						break;
+
+					case 'h1':
+					case 'h2':
+					case 'h3':
+					case 'h4':
+					case 'h5':
+					case 'h6':
+						$head = str_repeat("=", $node->nodeName{1});
+						if(!empty($nodeData)) {
+							$linesBefore = ((($previousNode = $this->getPreviousElementNode($node)) && $this->isHeaderNode($previousNode)) || empty($node->previousSibling)) ? 0 : ($nodeData['linesBefore']+1)%2;
+							$linesAfter = $nodeData['linesAfter']-1;
+						} else {
+							$linesBefore = 0;
+							$linesAfter = 1;
+							$textContent = " ".trim($textContent)." ";
+						}
+
+						$out = str_repeat("\n", $linesBefore) . $head . $textContent . $head . str_repeat("\n", $linesAfter);
+						break;
+
+					// text formatting
+					// 1 '</b><i><b>' => '<i>'
+					// 2 '</i><b><i>' => '<b>'
+					// 3 '</b></i><b>' => '</i>'
+					// 4 '</i></b><i>' => '</b>'
+					case 'i':
+					case 'b':
+						switch($node->nodeName) {
+							case 'i':
+								$open = $close = "''";
+								break;
+							case 'b':
+								$open = $close = "'''";
+								break;
+						}
+
+						// A) opening tags
+						// 1, 2
+						if($node->parentNode && $node->parentNode->previousSibling &&
+							$node->isSameNode($node->parentNode->firstChild) &&
+							in_array($node->parentNode->nodeName, array('i','b')) &&
+							$node->parentNode->nodeName != $node->nodeName &&
+							$node->parentNode->previousSibling->nodeName == $node->nodeName) {
+							// don't open bold (1) / italic (2)
+							$open = '';
+						}
+
+						// 3, 4
+						if($node->previousSibling && $node->previousSibling->hasChildNodes() &&
+							in_array($node->previousSibling->nodeName, array('i','b')) &&
+							$node->previousSibling->nodeName != $node->nodeName &&
+							$node->previousSibling->lastChild->nodeName == $node->nodeName) {
+							// don't open bold (3) / italic (4)
+							$open = '';
+						}
+
+						// B) closing tags
+						// 1, 2
+						if($node->nextSibling && $node->nextSibling->hasChildNodes() &&
+							in_array($node->nextSibling->nodeName, array('i','b')) &&
+							$node->nextSibling->nodeName != $node->nodeName &&
+							$node->nextSibling->firstChild->nodeName == $node->nodeName) {
+							// don't close bold (1) / italic (2)
+							$close = '';
+						}
+
+						// 3, 4
+						if($node->parentNode && $node->parentNode->nextSibling &&
+							$node->isSameNode($node->parentNode->lastChild) &&
+							in_array($node->parentNode->nodeName, array('i','b')) &&
+							$node->parentNode->nodeName != $node->nodeName &&
+							$node->parentNode->nextSibling->nodeName == $node->nodeName) {
+							// don't close bold (3) / italic (4)
+							$close = '';
+						}
+
+						$out = "{$open}{$textContent}{$close}";
+						break;
+
+					case 'tbody':
+						$out = $textContent;
+						break;
+				}
+
+			} else {
+
+				$attstr = $this->getAttributesStr($node);
+
+				if($node->nodeName == 'br' || $node->nodeName == 'hr') {
+					$out = "<{$node->nodeName}{$attStr} />";
+				} else {
+					// remove prohibited HTML tags (may occur after pasting content from external site)
+					if(in_array($node->nodeName, array('script', 'embed'))) {
+						$out = '';
+						break;
 					}
-					break;
+					$out = "<{$node->nodeName}{$attStr}>{$textContent}</{$node->nodeName}>";
+				}
 
-				case 'p':
-					// if the first previous XML_ELEMENT_NODE (so no text and no comment) of the current
-					// node is <p> then add new line before the current one
-					if(($previousNode = $this->getPreviousElementNode($node)) && $previousNode->nodeName == 'p') {
-						$textContent = "\n" . $textContent;
-					}
-
-					$out = $textContent;
-					break;
-
-				case 'h1':
-				case 'h2':
-				case 'h3':
-				case 'h4':
-				case 'h5':
-				case 'h6':
-					$head = str_repeat("=", $node->nodeName{1});
-					if(!empty($nodeData)) {
-						$linesBefore = ((($previousNode = $this->getPreviousElementNode($node)) && $this->isHeaderNode($previousNode)) || empty($node->previousSibling)) ? 0 : ($nodeData['linesBefore']+1)%2;
-						$linesAfter = $nodeData['linesAfter']-1;
-					} else {
-						$linesBefore = 0;
-						$linesAfter = 1;
-						$textContent = " ".trim($textContent)." ";
-					}
-
-					$out = str_repeat("\n", $linesBefore) . $head . $textContent . $head . str_repeat("\n", $linesAfter);
-					break;
-
-				// text formatting
-				// 1 '</b><i><b>' => '<i>'
-				// 2 '</i><b><i>' => '<b>'
-				// 3 '</b></i><b>' => '</i>'
-				// 4 '</i></b><i>' => '</b>'
-				case 'i':
-				case 'b':
-					switch($node->nodeName) {
-						case 'i':
-							$open = $close = "''";
-							break;
-						case 'b':
-							$open = $close = "'''";
-							break;
-					}
-
-					// A) opening tags
-					// 1, 2
-					if($node->parentNode && $node->parentNode->previousSibling &&
-						$node->isSameNode($node->parentNode->firstChild) &&
-						in_array($node->parentNode->nodeName, array('i','b')) &&
-						$node->parentNode->nodeName != $node->nodeName &&
-						$node->parentNode->previousSibling->nodeName == $node->nodeName) {
-						// don't open bold (1) / italic (2)
-						$open = '';
-					}
-
-					// 3, 4
-					if($node->previousSibling && $node->previousSibling->hasChildNodes() &&
-						in_array($node->previousSibling->nodeName, array('i','b')) &&
-						$node->previousSibling->nodeName != $node->nodeName &&
-						$node->previousSibling->lastChild->nodeName == $node->nodeName) {
-						// don't open bold (3) / italic (4)
-						$open = '';
-					}
-
-					// B) closing tags
-					// 1, 2
-					if($node->nextSibling && $node->nextSibling->hasChildNodes() &&
-						in_array($node->nextSibling->nodeName, array('i','b')) &&
-						$node->nextSibling->nodeName != $node->nodeName &&
-						$node->nextSibling->firstChild->nodeName == $node->nodeName) {
-						// don't close bold (1) / italic (2)
-						$close = '';
-					}
-
-					// 3, 4
-					if($node->parentNode && $node->parentNode->nextSibling &&
-						$node->isSameNode($node->parentNode->lastChild) &&
-						in_array($node->parentNode->nodeName, array('i','b')) &&
-						$node->parentNode->nodeName != $node->nodeName &&
-						$node->parentNode->nextSibling->nodeName == $node->nodeName) {
-						// don't close bold (3) / italic (4)
-						$close = '';
-					}
-
-					$out = "{$open}{$textContent}{$close}";
-					break;
 			}
 
 			// if current processed node contains attribute _wysiwyg_new_line (added in Parser.php)
@@ -229,5 +255,21 @@ class ReverseParser {
 		return ($node->nodeName{0} == 'h') && is_numeric($node->nodeName{1});
 	}
 
-
+	/**
+	 * input: '<div id="123" washtml="true" _wysiwyg_new_line="false" style="border: solid 1px">'
+	 * output: ' id="123" style="border: solid 1px"'
+	 */
+	private function getAttributesStr($node) {
+		if(!$node->hasAttributes()) {
+			return '';
+		}
+		$attStr = '';
+		foreach ($node->attributes as $attrName => $attrNode) {
+			if($attrName == 'washtml' || $attrName == '_wysiwyg_new_line') {
+				continue;
+			}
+			$attStr .= ' ' . $attrName . '="' . $attrNode->nodeValue  . '"';
+		}
+		return $attStr;
+	}
 }
