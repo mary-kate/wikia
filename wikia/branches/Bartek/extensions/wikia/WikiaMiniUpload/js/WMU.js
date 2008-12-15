832c8ca0dd57ec3d44f63319cdd4201602c8c62d
bartek <bartek@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-12-15 16:11:49 +0000

    WikiaMiniUpload: undo r4418 and r4419 after feedback

--- wikia/branches/Bartek/extensions/wikia/WikiaMiniUpload/js/WMU.js
+++ wikia/branches/Bartek/extensions/wikia/WikiaMiniUpload/js/WMU.js
@@ -106,10 +106,23 @@ function WMU_manualWidthInput( elem ) {
 	if( WMU_orgThumbSize == null ) {
 		var WMU_orgThumbSize = [image.width, image.height];
 	}
-	image.height = elem.value / WMU_ratio;
-	image.width = elem.value;
-	WMU_thumbSize = [image.width, image.height];
-	$( 'ImageSize' ).innerHTML = elem.value + 'px';
+	if ( elem.value > WMU_width ) {
+		if (!WMU_shownMax) {
+			image.width = WMU_width;
+			image.height = WMU_width / WMU_ratio;
+			WMU_thumbSize = [image.width, image.height];
+			$( 'ImageSize' ).innerHTML = image.width + 'px';		
+			$( 'ImageUploadManualWidth' ).value = image.width;
+			WMU_shownMax = true;
+			alert (wmu_max_thumb);
+		}
+	} else {
+		image.height = elem.value / WMU_ratio;
+		image.width = elem.value;
+		WMU_thumbSize = [image.width, image.height];
+		$( 'ImageSize' ).innerHTML = elem.value + 'px';
+		WMU_shownMax = false;			
+	}
 }
 
 function WMU_show(e) {
