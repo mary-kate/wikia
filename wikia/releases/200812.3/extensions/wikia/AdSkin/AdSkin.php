fd46265166a009e777c9a1b0635647e306a07d40
Inez Korczyński <inez@users.noreply.github.com>
2008-12-16 14:11:12 +0000

    * New production branch

---
+++ wikia/releases/200812.3/extensions/wikia/AdSkin/AdSkin.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * Author: Christian Williams 
+ */
+
+$wgHooks['GetHTMLAfterBody'][] = 'RenderAdSkin';
+		
+function RenderAdSkin() {
+	global $wgAdSkin, $wgExtensionsPath, $wgUser, $wgStyleVersion;
+
+	// Disable for logged in users
+	if (is_object($wgUser) && $wgUser->isLoggedIn() ){
+		return true;
+	}
+
+	if (isset($wgAdSkin)) {
+		switch ($wgAdSkin) {
+		case "wow_lich_king":
+			echo '<link rel="stylesheet" type="text/css" href="'. $wgExtensionsPath .'/wikia/AdSkin/css/wow_lich_king.css?'. $wgStyleVersion .'" />';
+			break;
+		case "wow_lich_king_warhammer":
+			echo '<link rel="stylesheet" type="text/css" href="'. $wgExtensionsPath .'/wikia/AdSkin/css/wow_lich_king_warhammer.css?'. $wgStyleVersion .'" />';
+			break;
+		case "dragonball_origins":
+			echo '<link rel="stylesheet" type="text/css" href="'. $wgExtensionsPath .'/wikia/AdSkin/css/dragonball_origins.css?'. $wgStyleVersion .'" />';
+			break;
+		case "dnd":
+			echo '<link rel="stylesheet" type="text/css" href="'. $wgExtensionsPath .'/wikia/AdSkin/css/dnd.css?'. $wgStyleVersion .'" />';
+			break;
+		}	
+	}
+
+	return true;
+}
