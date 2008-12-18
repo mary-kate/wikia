c5f2bc353a4542ba213e64733c33a44de60b9b3e
eloy <eloy@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-12-18 15:19:10 +0000

    * svn merge https://svn.wikia-code.com/wikia/trunk/extensions/wikia/Blogs/ -r4521:HEAD (r4522:4560)

--- wikia/releases/200812.3/extensions/wikia/Blogs/templates/comment.tmpl.php
+++ wikia/releases/200812.3/extensions/wikia/Blogs/templates/comment.tmpl.php
@@ -14,6 +14,9 @@
 		<?php if( $canDelete ): ?>
 			<a href="<?php echo $comment[ "title" ]->getLocalUrl( "redirect=no&action=delete" ) ?>">delete</a>
 		<?php endif; ?>
+		<?php if( $canToggle ): ?>
+			<a href="#" class="blog-comm-hide" id="<?php echo $comment[ "title" ]->getArticleId() ?>">hide</a>
+		<?php endif; ?>
 		</div>
 		<?php
 			echo $comment["text"];
@@ -21,11 +24,14 @@
 	</div>
 <?php
 	else:
+		echo "<div class=\"blog-comment-hidden\">\n";
 		echo wfMsg( "blogs-comment-hidden" );
-		echo '&nbsp;<a href="#" class="blog-comm-hide" id="'.$comment[ "title" ]->getArticleId().'">unhide</a>';
+		if( $canToggle ):
+			echo '&nbsp;<a href="#" class="blog-comm-hide" id="'.$comment[ "title" ]->getArticleId().'">unhide</a>';
+		endif;
+		echo "</div>\n";
 	endif; ### comment[ "hiddent" ]
 ?>
 </div>
 <br style="clear: both;" />
 <!-- e:<?= __FILE__ ?> -->
-
