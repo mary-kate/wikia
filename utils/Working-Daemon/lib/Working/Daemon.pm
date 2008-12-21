234d093aefd8c93e4a0e71bfc96c2b67483eb461
artur <artur@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-12-21 06:22:06 +0000

    add test binary plus some more changes to option parsing

--- utils/Working-Daemon/lib/Working/Daemon.pm
+++ utils/Working-Daemon/lib/Working/Daemon.pm
@@ -3,32 +3,120 @@ package Working::Daemon;
 use 5.008008;
 use strict;
 use warnings;
+use Data::Dumper;
 
-require Exporter;
-use AutoLoader qw(AUTOLOAD);
+use Getopt::Long;
 
-our @ISA = qw(Exporter);
+our ($uid, $gid);
+our %config;
+sub new {
+    my $class = shift;
+    my $self = bless {}, $class;
+    return $self;
+}
 
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use Working::Daemon ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-	
-);
-
-our $VERSION = '0.01';
+sub parse_options {
+    my $self = shift;
+    my @args = @_;
+    my %options;
+    GetOptions(\%options, @args);
+    print Dumper(\%options);
+    exit;
 
 
+}
+
+sub change_root {
+    my $self = shift;
+
+    mkdir ("/tmp/glbdns.$$") || die;
+    chown($uid,$gid, "/tmp/glbdns.$$") || die;
+    chroot("/tmp/glbdns.$$") || die;
+    chdir("/");
+    $self->drop_privs();
+    mkdir ("/etc") || die;
+    open(my $protocol, "+>/etc/protocols") || die;
+    print $protocol "tcp     6       TCP\n";
+    close($protocol);
+
+}
+
+sub stop {
+    my $class = shift;
+
+    if(my $pid = get_pid()) {
+        while(check_pid($pid)) {
+            kill(2, $pid);
+            mylog(0, 'info', "sent SIGINT to $pid - waiting on stopped pid $pid");
+            print STDERR "sent SIGINT to $pid - waiting on stopped pid $pid\n";
+            sleep 1;
+        }
+        mylog(0, 'info',"Stopped $config{name} on $pid");
+        print STDERR "$config{name} stopped on $pid\n";
+        return $pid;
+    }
+    return 0;
+}
+
+sub get_pid {
+    my $class = shift;
+    if(-r $config{pidfile}) {
+        open(my $pidfile, "<$config{pidfile}") || die;
+        my $line = <$pidfile>;
+        close($pidfile);
+        $line =~/(\d+)/;
+        if(my $pid_to_check = $1) {
+            $ENV{PATH} = '';
+            return $pid_to_check if(check_pid($pid_to_check));
+        }
+    }
+   return 0;
+}
+
+sub check_pid {
+    my $self = shift;
+    my $pid  = shift;
+    my $grep = "/bin/grep";
+    $grep = "/usr/bin/grep" if ($^O eq 'darwin');
+    return !system("/bin/ps aux | $grep $pid | $grep -v grep | $grep $config{name}");
+}
+
+
+sub daemonize {
+    my $self = shift;
+    use POSIX qw(setsid);
+    defined(my $pid = fork) || die "Can't fork: $!";
+    if ($pid) {
+        print "$config{name} started on $pid\n";
+        exit 0;
+    }
+    setsid() || die "Can't start a new session: $!";
+    open (STDIN , '/dev/null') || die "Can't read /dev/null: $!";
+    open (STDOUT, '>/dev/null') || die "Can't write to /dev/null: $!";
+    open (STDERR, '>/dev/null') || die "Can't write to /dev/null: $!";
+}
+
+sub mylog {
+  my ($level, $prio, $msg) = @_;
+  return 1 if ($level > $config{loglevel});
+  if ($config{syslog}) {
+    syslog($prio, $msg) || die "$!";
+  } else {
+    print STDERR "$prio - $msg\n";
+  }
+}
+
+
+
+
+sub drop_privs {
+  # drop user to nobody
+  $< = $uid;
+  $> = $uid;
+  # drop group to nobody
+  $( = $gid;
+  $) = $gid;
+}
 # Preloaded methods go here.
 
 # Autoload methods go after =cut, and are processed by the autosplit program.
