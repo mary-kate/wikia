3534e61996fa46d6ce441b387aef0b339994ceee
artur <artur@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-12-21 07:49:14 +0000

    added daemon and foreground options

--- utils/Working-Daemon/bin/test.pl
+++ utils/Working-Daemon/bin/test.pl
@@ -4,14 +4,15 @@ use strict;
 use warnings;
 
 use Working::Daemon;
+use Data::Dumper;
 my $daemon = Working::Daemon->new();
 
 $daemon->parse_options("bool","integer=i","string=s","multi=s%");
-
+print Dumper($daemon->options);
 $daemon->user("sky");
 $daemon->group("sky");
 $daemon->name("testdaemon");
-$daemon->chroot();
+$daemon->change_root();
 $daemon->drop_privs();
 
 
