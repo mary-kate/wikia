000a3601f0a26056df5dbdbf3bf7b41d751b3c05
emil <emil@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-12-22 09:32:22 +0000

    now lets move with extensions

---
+++ wikia/branches/emil/extensions/wikia/UserBoard/UserBoard.php
@@ -0,0 +1,27 @@
+<?php
+$wgExtensionFunctions[] = 'wfUserBoardReadLang';
+
+//read in localisation messages
+function wfUserBoardReadLang(){
+	require_once( "UserBoard.i18n.php" );
+	# Add messages
+	global $wgMessageCache;
+	foreach( $wgUserBoardMessages as $key => $value ) {
+		$wgMessageCache->addMessages( $wgUserBoardMessages[$key], $key );
+	}
+}
+
+$wgUserBoard = true;
+
+$wgAutoloadClasses['BoardBlast'] = "{$wgUserBoardDirectory}/SpecialSendBoardBlast.php";
+$wgSpecialPages['SendBoardBlast'] = 'BoardBlast';
+
+$wgAutoloadClasses['ViewUserBoard'] = "{$wgUserBoardDirectory}/SpecialUserBoard.php";
+$wgSpecialPages['UserBoard'] = 'ViewUserBoard';
+$wgSpecialPageGroups['UserBoard'] = 'users';
+
+require_once( "{$wgUserBoardDirectory}/UserBoard_AjaxFunctions.php" );
+	
+$wgAutoloadClasses["UserBoard"] = "{$wgUserBoardDirectory}/UserBoardClass.php";
+$wgUserProfileDisplay['board'] = true;
+?>
