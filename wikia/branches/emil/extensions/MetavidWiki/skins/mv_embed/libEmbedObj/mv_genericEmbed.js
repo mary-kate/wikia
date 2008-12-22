96f6950b932b5cc720462cfa260682473b427b6f
emil <emil@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-12-22 17:44:02 +0000

    del

--- wikia/branches/emil/extensions/MetavidWiki/skins/mv_embed/libEmbedObj/mv_genericEmbed.js
+++
@@ -1,9 +0,0 @@
-/* the most simple implementation used for unknown application/ogg plugin */
-var genericEmbed={
-	instanceOf:'genericEmbed',
-    getEmbedHTML:function(){
-    	return '<object type="application/ogg" '+
-			      'width="'+this.width+'" height="'+this.height+'" ' +
-		    	  'data="' + this.media_element.selected_source.getURI(this.seek_time_sec) + '"></object>';
-    }
-}
