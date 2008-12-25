54f02f48e13b0781306fc8824f12d3b5de6f4d0c
artur <artur@d23c911d-4c54-0410-aefe-1982d53a7c61>
2008-12-25 06:53:09 +0000

    grumble grumble

--- utils/Working-Daemon/bin/test.pl
+++ utils/Working-Daemon/bin/test.pl
@@ -7,19 +7,14 @@ use Working::Daemon;
 use Data::Dumper;
 my $daemon = Working::Daemon->new();
 our $VERSION = 0.45;
-$daemon->parse_options("bool"      => "Test if you can set bools",
-                       "integer=i" => "Integer settings",
-                       "string=s"  => "String setting",
-                       "multi=s%"  => "Multiset variable");
-$daemon->chroot(0);
-#$daemon->daemon(0);
-$daemon->user("sky");
-$daemon->group("sky");
-$daemon->name("testdaemon");
 
-$daemon->do_action();
-$daemon->change_root();
-$daemon->drop_privs();
+$daemon->name("testdaemon");
+$daemon->standard("bool"      => "Test if you can set bools",
+                  "integer=i" => "Integer settings",
+                  "string=s"  => "String setting",
+                  "multi=s%"  => "Multiset variable");
+
+
 
 sleep 10;
 1;
