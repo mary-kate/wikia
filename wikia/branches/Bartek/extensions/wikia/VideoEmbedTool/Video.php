0817a6b25e5400553f3ada202712e9d42bcec5e3
bartek <bartek@d23c911d-4c54-0410-aefe-1982d53a7c61>
2009-01-08 13:54:47 +0000

    VideoEmbedTool: more functions

--- wikia/branches/Bartek/extensions/wikia/VideoEmbedTool/Video.php
+++ wikia/branches/Bartek/extensions/wikia/VideoEmbedTool/Video.php
@@ -1,10 +1,18 @@
 <?php
-	class Video {
 
+// use the same namespace as in old NY extension
+define( 'NS_VIDEO', 400 );
 
+class Video {
+	var 	$id,
+		$provider;
+	
 
+	public function __construct( $id, $provider ) {
 
-
+		
 	}
+
+}
 
 ?>
