a17707fbad56b7dcd0c91e8b709cb7cea35fe679
bartek <bartek@d23c911d-4c54-0410-aefe-1982d53a7c61>
2009-01-08 13:35:40 +0000

    VideoEmbedTool: Video class plus some skeleton functions

--- wikia/branches/Bartek/extensions/wikia/VideoEmbedTool/VideoPage.php
+++ wikia/branches/Bartek/extensions/wikia/VideoEmbedTool/VideoPage.php
@@ -4,15 +4,28 @@
 // main video page class
 class VidePage extends Article {
 
+        function __construct (&$title){
+                parent::__construct(&$title);
+        }
+
+
+	function view() {
 
 
 
+	}
+
+	function getContent() {
 
 
+		return Article:getContent();
+	}
 
 
+	function revert() {
 
 
+	}
 
 }
 
