b35cc0506bbc54f01114396d72fcbe0aa20478dd
Inez Korczyński <inez@users.noreply.github.com>
2009-01-13 13:37:07 +0000

    * Create weekly release branch

---
+++ wikia/releases/200901.3/extensions/wikia/EditingTips/EditingTips.i18n.php
@@ -0,0 +1,11 @@
+<?php
+$messages = array();
+$messages['en'] = array(
+	'tog-disableeditingtips' => 'Do not show editing tips',
+	'tog-widescreeneditingtips' => 'Use the Widescreen editing',	
+	'editingtips_enter_widescreen' => 'Enter Widescreen' ,
+	'editingtips_exit_widescreen' => 'Exit Widescreen' ,	
+	'editingtips_hide' => 'Hide Editing Tips' ,
+	'editingtips_show' => 'Show Editing Tips' ,
+	'editingTips' => 'Add your tips [[MediaWiki:EditingTips|here]]',
+);
