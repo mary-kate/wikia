c4a09b48a1edb58815c20925e3438564235fda23
per.artur.bergman <per.artur.bergman@d23c911d-4c54-0410-aefe-1982d53a7c61>
2009-01-19 12:14:26 +0000

    VideoEmbedTool: correction

--- wikia/branches/Bartek/extensions/wikia/VideoEmbedTool/templates/main.tmpl.php
+++ wikia/branches/Bartek/extensions/wikia/VideoEmbedTool/templates/main.tmpl.php
@@ -32,7 +32,7 @@
 				}
 				?>
 					<form onsubmit="return AIM.submit(this, VET_uploadCallback)" action="<?= $wgScriptPath ?>/index.php?action=ajax&rs=VET&method=insertVideo" id="VideoEmbedForm" method="POST" enctype="multipart/form-data">
-					<label for="VideoEmbedUrl"><?= wfMsg('vet-upload') ?></label><input id="VideoEmbedUrl" name="VideoEmbedUrl" type="text" size="32" />
+					<label for="VideoEmbedUrl"><?= wfMsg('vet-upload') ?></label><input id="VideoEmbedUrl" name="wpVideoEmbedUrl" type="text" size="32" />
 					<input type="submit" value="<?= wfMsg('vet-upload-btn') ?>" onclick="return VET_upload(event);" />
 					</form>
 					<?php
