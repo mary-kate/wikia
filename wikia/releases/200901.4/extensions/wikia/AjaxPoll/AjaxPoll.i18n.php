1838d0b1a54652a60f2e7ea4817874a22ce6b7ec
eloy <eloy@d23c911d-4c54-0410-aefe-1982d53a7c61>
2009-01-20 11:21:38 +0000

    create new release tree

---
+++ wikia/releases/200901.4/extensions/wikia/AjaxPoll/AjaxPoll.i18n.php
@@ -0,0 +1,14 @@
+<?php
+
+$messages = array();
+$messages["en"] = array(
+	"ajaxpoll-percentVotes" => "of all votes",
+	"ajaxpoll-thankyou" => "Thank You for your vote!",
+	"ajaxpoll-error" => "There was a problem with processing your vote, please try again.",
+	"ajaxpoll-created" => "The poll was created on ",
+	"ajaxpoll-total" => ", and so far ",
+	"ajaxpoll-voted" => "people voted.",
+	"ajaxpoll-submit" => "Vote!",
+	"ajaxpoll-closed" => "Poll is closed now. Enjoy the results.",
+	"ajaxpoll-submitting" => "Please wait, submitting your vote..."
+);
---
+++ wikia/releases/200901.4/extensions/wikia/DefaultMessages/DefaultMessages.php
@@ -0,0 +1,36 @@
+<?php
+
+$wgExtensionFunctions[] = 'efDefaultMessagesSetup';
+$wgAutoloadClasses['DefaultMessagesCache'] = dirname(__FILE__) . '/DefaultMessagesCache.php';
+
+function efDefaultMessagesSetup() {
+	global $wgDefaultMessagesDB, $wgDefaultMessagesCache;
+	global $wgDBname, $wgHooks;
+	global $messageMemc, $wgUseDatabaseMessages, $wgMsgCacheExpiry;
+
+	if( empty( $wgDefaultMessagesDB ) ) {
+		$wgDefaultMessagesDB = 'messaging';
+	}
+
+	if( $wgDefaultMessagesDB != $wgDBname ) {
+		$wgHooks['MsgGetFromNamespaceAfter'][] = 'efGetDefaultMessage';
+		$wgDefaultMessagesCache = new DefaultMessagesCache( $messageMemc, $wgUseDatabaseMessages, $wgMsgCacheExpiry );
+	}
+}
+
+function efGetDefaultMessage( $title, $lang, &$message ) {
+	if( $message === false ) {
+		global $wgDefaultMessagesCache;
+		if( is_object( $wgDefaultMessagesCache ) ) {
+			if( $lang !== 'en' ) {
+				$pos = strrpos( $title, '/' );
+				if( $pos === false ) {
+					$title .= '/' . $lang;
+				}
+			}
+			$message = $wgDefaultMessagesCache->get( $title, $lang );
+		}
+	}
+	return true;
+}
+
