6bf6aac45a778f92db06fed944500aa97b952bb6
artur <artur@d23c911d-4c54-0410-aefe-1982d53a7c61>
2009-01-22 06:47:12 +0000

    more work, still a mess in how to store domains

--- utils/GlbDNS/bin/glbdns.pl
+++ utils/GlbDNS/bin/glbdns.pl
@@ -2,137 +2,35 @@ use strict;
 use warnings;
 use GlbDNS;
 
-use Sys::Syslog qw(:DEFAULT setlogsock :macros);
-my @arguments = @ARGV;
-setlogsock('unix') || die "$!";
-use Getopt::Long;
-use Carp::Heavy;
-use Scalar::Util;
-our %config = (
-              foreground => 0,
-              port    => 53,
-              address => '0.0.0.0',
-              syslog   => 1,
-              debug    => 0,
-              name     => 'glbdns',
-              pidfile  => '',
-              loglevel => 1,
-              config   => '/etc/glbdns/',
-              );
+use Working::Daemon::Syslog;
+my $daemon = Working::Daemon->new();
 
-my $uid = getpwnam('nobody');
-my $gid = getpwnam('nobody');
+$daemon->name("glbdns");
+$daemon->parse_options(
+    "port=i"     => 53             => "Which port number to listen to",
+    "address=s"  => "0.0.0.0"      => "IP Address to listen to",
+    "syslog"     => 0              => "Syslog",
+    "config=s"   => "/etc/glbdns/" => "Configuration directory",
+    "loglevel=i" => 1              => "What level of messaes to log, higher is more verbose",
+    );
 
-my $results = GetOptions("foreground" => \$config{foreground},
-                         "port=i"     => \$config{port},
-                         "address=s"  => \$config{address},
-                         "syslog"     => \$config{syslog},
-                         "debug"      => \$config{debug},
-                         "name=s"     => \$config{name},
-                         "pidfile=s"  => \$config{pidfile},
-                         "config=s"   => \$config{config},
-                         "logleve=i"  => \$config{loglevel},
-                         );
-
-my $command = shift || 'start';
-$config{pidfile} = "/var/run/$config{name}.pid" unless $config{pidfile};
-$config{foreground} = 1 if($config{debug});
-
-if($command eq 'restart') {
-    openlog("$config{name}", 'ndelay,pid', LOG_DAEMON) if($config{syslog});
-    # we don't do anything on restart
-    stop();
-    closelog();
-    $command = 'start';
+# this should be support cleaner by Working::Daemon
+if ($daemon->options->{syslog}) {
+    bless $daemon, 'Working::Daemon::Syslog';
+    $daemon->init;
 }
 
-if($command eq 'status') {
-    if(my $pid = get_pid()) {
-        print "$config{name} is running on $pid\n";
-        exit 0;
-    } else {
-        print "$config{name} is not running\n";
-        exit 1;
-    }
-}
-if($command eq 'start') {
-    if(my $pid = get_pid()) {
-        #already running just return
-        openlog("$config{name}", 'ndelay,pid', LOG_DAEMON) if($config{syslog});
-        mylog(0, 'info', "exiting - process already running on $pid");
-        print STDERR "exiting - process already running on $pid\n";
-        exit 1;
-    }
-}
+$daemon->do_action;
 
-if($command eq 'stop') {
-    openlog("$config{name}", 'ndelay,pid', LOG_DAEMON) if($config{syslog});
-    if(stop()) {
-        exit 0;
-    } else {
-        # no running
-        mylog(0, 'info', "failed to stop daemon - cannot find process");
-        print STDERR "$config{name} failed to stop daemon - cannot find process\n";
-        exit 1;
-    }
-}
-
-
-
-daemonize() unless $config{foreground};
-
-{
-    if(my $pid = fork()) {
-
-        # this is the master session
-        # it makes sure to cleanup from the slave
-        # it stays as superuser
-
-        open(my $pidfile, "+>$config{pidfile}") || die;
-        openlog("$config{name}", 'ndelay,pid', LOG_DAEMON) if($config{syslog});
-        mylog(1, 'info', "started master session - child is $pid") || die "$!";
-        select($pidfile); $|++; select(STDOUT);
-        print $pidfile "$$";
-        $SIG{INT} = sub { kill(2,$pid) };
-        $0 = "$config{name} - waiting for child $pid";
-        waitpid($pid, 0);
-        mylog(1, 'info', "exiting master session - child is $pid") || die "$!";
-        unlink("/tmp/glbdns.$pid/etc/protocols") || die "$!";
-        rmdir("/tmp/glbdns.$pid/etc/") || die;
-        rmdir("/tmp/glbdns.$pid/") || die;
-        unlink($config{pidfile}) || die $!;
-        exit;
-    }
-
-    openlog("$config{name} worker", 'ndelay,pid', LOG_DAEMON) if($config{syslog});
-    mylog(1, 'info', "started session - ready to listen on $config{address}:$config{port}");
-    $0 = "$config{name} worker - starting up";
-}
-
-my $dns = GlbDNS->new();
+my $dns = GlbDNS->new($daemon);
 
 use GlbDNS::Config;
 
-GlbDNS::Config->load_configs($dns, $config{config});
-
-{
-    mkdir ("/tmp/glbdns.$$") || die;
-    chown($uid,$gid, "/tmp/glbdns.$$") || die;
-    chroot("/tmp/glbdns.$$") || die;
-    chdir("/");
-    drop_privs();
-    mkdir ("/etc") || die;
-    open(my $protocol, "+>/etc/protocols") || die;
-    print $protocol "tcp     6       TCP\n";
-    print $protocol "udp     17      UDP\n";
-    close($protocol);
-}
-
-$SIG{INT} = sub { exit };
+GlbDNS::Config->load_configs($dns, $daemon->options->{config});
 
 $dns->start();
 exit;
-
+__END__
 sub stop {
     if(my $pid = get_pid()) {
         while(check_pid($pid)) {
@@ -188,7 +86,7 @@ sub mylog {
   my ($level, $prio, $msg) = @_;
   return 1 if ($level > $config{loglevel});
   if ($config{syslog}) {
-    syslog($prio, $msg) || die "$!";
+
   } else {
     print STDERR "$prio - $msg\n";
   }
