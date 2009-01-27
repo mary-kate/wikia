6ff618b1270111a3ab0b7524fc7d6bbb4ac0ee17
bartek <bartek@d23c911d-4c54-0410-aefe-1982d53a7c61>
2009-01-27 16:32:44 +0000

    VideoEmbedTool: update private development branch

--- wikia/branches/Bartek/extensions/wikia/VideoEmbedTool/templates/main.tmpl.php
+++ wikia/branches/Bartek/extensions/wikia/VideoEmbedTool/templates/main.tmpl.php
@@ -1,7 +1,7 @@
 <div id="VideoEmbedError"></div>
 <?php
 	$uploadmesg = wfMsgExt( 'vet-uploadtext', 'parse' );
-	$uploadmesg = preg_replace( '/(<a[^>]+)/', '$1 target="_new" ', $uploadmesg );
+	$uploadmesg = preg_replace( '/(<a[^>]+)/', '$1 target="_blank" ', $uploadmesg );
 
 ?>
 
@@ -12,9 +12,11 @@
 		</td>
 	</tr>
 <?php
-	global $wgStylePath, $wgUser, $wgScriptPath;
+	global $wgStylePath, $wgUser, $wgScriptPath, $wgExtensionsPath;
 ?>
 	<tr id="VideoEmbedSearch">
+		<th><h1><?= wfMsg('vet-upload') ?></h1></th>
+
 		<td>
 			<img src="<?= $wgStylePath; ?>/monaco/images/widget_loading.gif" id="VideoEmbedProgress2" style="visibility: hidden;"/>
 			<?php
@@ -32,34 +34,68 @@
 				}
 				?>
 					<form onsubmit="return AIM.submit(this, VET_uploadCallback)" action="<?= $wgScriptPath ?>/index.php?action=ajax&rs=VET&method=insertVideo" id="VideoEmbedForm" method="POST" enctype="multipart/form-data">
-					<label for="VideoEmbedUrl"><?= wfMsg('vet-upload') ?></label><input id="VideoEmbedUrl" name="wpVideoEmbedUrl" type="text" size="32" />
-					<input type="submit" value="<?= wfMsg('vet-upload-btn') ?>" onclick="return VET_upload(event);" />
+					<input id="VideoEmbedUrl" name="wpVideoEmbedUrl" type="text" size="32" />
+					<input id="VideoEmbedUrlSubmit" type="submit" value="<?= wfMsg('vet-upload-btn') ?>" onclick="return VET_upload(event);" />
+					<div id="VideoEmbedMainMesg"><?= wfMsg( 'vet-main-info' ) ?></div>
 					</form>
 					<?php
 			}
 ?>
 </td>
-<td>
-<?php
-
-if( ( $wgUser->isLoggedIn() ) && ( $wgUser->isAllowed( 'upload' ) ) ) {
-?>
-			<div id="VideoEmbedSupported">
-				<?= wfMsg( 'vet-supported' ) ?>
-			</div>
-		</td>
-<?php
-}
-?>
-	</tr>
+</tr>
 </table>
 
 <div id="VET_results_0">
 	<?= $result ?>
 </div>
+<table id="VideoEmbedToolProvidersTable" class="gallery" cellpadding="6" cellspacing="6">
+	<tbody><tr>
+		<td><div class="gallerybox" style="width: 170px;">
+			<div class="thumb" style="padding: 49px 0pt; width: 165px;"><div style="margin-left: auto; margin-right: auto; width: 120px;"><a href="http://www.5min.com" class="image" title="5min" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/5min.gif" height="47" width="120" border="0"></a></div></div>
+			<div class="gallerytext">
+			</div>
+		</div></td>
+		<td><div class="gallerybox">
+			<div class="thumb" style="padding: 47px 0pt; width: 165px;"><div style="margin-left: auto; margin-right: auto; width: 120px;"><a href="http://gamevideos.1up.com/" class="image" title="Gamevideos"  target="_blank"><img alt="Gamevideos" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/gamevideos.jpg" height="51" width="120" border="0"></a></div></div>
+			<div class="gallerytext">
+
+			</div>
+		</div></td>
+		<td><div class="gallerybox" style="width: 170px;">
+			<div class="thumb" style="padding: 56px 0pt; width: 165px;"><div style="margin-left: auto; margin-right: auto; width: 120px;"><a href="http://www.metacafe.com" class="image" title="Metacafe" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/metacafe.gif" height="33" width="120" border="0"></a></div></div>
+			<div class="gallerytext">
+			</div>
+		</div></td>
+		<td><div class="gallerybox" style="width: 170px;">
+			<div class="thumb" style="padding: 55px 0pt; width: 165px;"><div style="margin-left: auto; margin-right: auto; width: 120px;"><a href="http://www.myvideo.de" class="image" title="Myvideo" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/myvideo.jpg" height="35" width="120" border="0"></a></div></div>
+			<div class="gallerytext">
+			</div>
+		</div></td>
+	</tr>
+	<tr>
+		<td><div class="gallerybox" style="width: 170px;">
+			<div class="thumb" style="padding: 62px 0pt; width: 165px;"><div style="margin-left: auto; margin-right: auto; width: 120px;"><a href="http://en.sevenload.com" class="image" title="Sevenload" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/sevenload.jpg" height="21" width="120" border="0"></a></div></div>
+			<div class="gallerytext">
+			</div>
+		</div></td>
+		<td><div class="gallerybox" style="width: 170px;">
+			<div class="thumb" style="padding: 37px 0pt; width: 165px;"><div style="margin-left: auto; margin-right: auto; width: 120px;"><a href="http://www.vimeo.com" class="image" title="Vimeo" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/vimeo.png" height="72" width="120" border="0"></a></div></div>
+			<div class="gallerytext">
+
+			</div>
+		</div></td>
+		<td><div class="gallerybox" style="width: 170px;">
+			<div class="thumb" style="padding: 30px 0pt; width: 165px;"><div style="margin-left: auto; margin-right: auto; width: 120px;"><a href="http://www.youtube.com" class="image" title="YouTube" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/youtube.jpg" height="85" width="120" border="0"></a></div></div>
+			<div class="gallerytext">
+			</div>
+		</div></td>
+
+		<td></td>
+	</tr>
+</tbody></table>
 
 <div id="VET_results_1" style="display: none;">
-	<br/><br/><br/><br/><br/>
+<br/><br/><br/><br/><br/>
 	<div style="text-align: center;">
 		<img src="<?= $wgStylePath ?>/../extensions/wikia/VideoEmbedTool/images/flickr_logo.gif" />
 		<div class="VideoEmbedSourceNote"><?= wfMsg('vet-flickr-inf') ?></div>
