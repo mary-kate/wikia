d119df5b7f81d2509f18157ab0e16ffd5672d7e3
inez <inez@d23c911d-4c54-0410-aefe-1982d53a7c61>
2009-01-28 02:13:03 +0000

    * Sidebar - loging

--- wikia/branches/InezSidebar/includes/wikia/GlobalFunctions.php
+++ wikia/branches/InezSidebar/includes/wikia/GlobalFunctions.php
@@ -581,7 +581,7 @@ function getMenu() {
 
 	$id = $wgRequest->getVal('id');
 	if($id) {
-		wfErrorLog("SideBar wgCityId: $wgCityId id: $id");
+		wfDebug("SideBar wgCityId: $wgCityId id: $id");
 
 		$menuArray = $wgMemc->get($id);
 		$ar = new AjaxResponse('var menuArray = '.Wikia::json_encode($menuArray).';');
@@ -592,7 +592,7 @@ function getMenu() {
 
 	$words = $wgRequest->getVal('words');
 	if($words) {
-		wfErrorLog("SideBar wgCityId: $wgCityId id: $words");
+		wfDebug("SideBar wgCityId: $wgCityId id: $words");
 
 		$magicWords = array();
 		$map = array('voted' => array('highest_ratings', 'GetTopVotedArticles'), 'popular' => array('most_popular', 'GetMostPopularArticles'), 'visited' => array('most_visited', 'GetMostVisitedArticles'), 'newlychanged' => array('newly_changed', 'GetNewlyChangedArticles'), 'topusers' => array('community', 'GetTopFiveUsers'));
