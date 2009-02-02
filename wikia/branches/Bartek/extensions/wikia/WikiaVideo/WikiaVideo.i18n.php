a358302f469c5dd9057ec123fa88a6d34e669012
bartek <bartek@d23c911d-4c54-0410-aefe-1982d53a7c61>
2009-02-02 16:21:22 +0000

    VideoEmbedTool: protect red links

--- wikia/branches/Bartek/extensions/wikia/WikiaVideo/WikiaVideo.i18n.php
+++ wikia/branches/Bartek/extensions/wikia/WikiaVideo/WikiaVideo.i18n.php
@@ -5,5 +5,6 @@ $messages['en'] = array(
 	'wikiavideo-reverted' => 'The video $1 was successfully reverted. Go back to $2.',
 	'wikiavideo-back' => 'back',
 	'wikiavideo-create' => 'Add Video',
+	'wikiavideo-replace' => 'Replace Video',
 );
 
