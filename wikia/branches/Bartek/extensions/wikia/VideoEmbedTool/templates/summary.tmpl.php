8b6c38a3d1cc9009c2956f3e031b526985710c70
bartek <bartek@d23c911d-4c54-0410-aefe-1982d53a7c61>
2009-02-02 15:01:57 +0000

    VideoEmbedTool: change the layout a bit

--- wikia/branches/Bartek/extensions/wikia/VideoEmbedTool/templates/summary.tmpl.php
+++ wikia/branches/Bartek/extensions/wikia/VideoEmbedTool/templates/summary.tmpl.php
@@ -3,6 +3,6 @@
 </div>
 <div style="text-align: center;">
 	<div id="VideoEmbedTag" style="border-bottom: 1px solid #CCC; padding: 15px 0; margin: 15px;"><?= $tag ?></div>
-	<div id="VideoEmbedPageSuccess" style="display:none;"><?= wfMsg( 'vet-page-success' ) ?></div>
+	<div id="VideoEmbedPageSuccess" style="display:none;"><?= wfMsg( 'vet-page-success' ) ?></br><br/></div>
 	<input onclick="VET_close(event);" type="button" value="<?= wfMsg( 'vet-return' ) ?>" />
 </div>
