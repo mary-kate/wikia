2a9d79c144e180cb3b056dc487976ba51d2e85cb
inez <inez@d23c911d-4c54-0410-aefe-1982d53a7c61>
2009-02-03 18:08:23 +0000

    * Release branch

---
+++ wikia/releases/200902.1/extensions/wikia/HAWelcome/HAWelcome.php
@@ -0,0 +1,172 @@
+<?php
+
+/**
+ * HAWelcomeJob -- Welcome user after first edit
+ *
+ * @file
+ * @ingroup JobQueue
+ *
+ * @copyright Copyright © Krzysztof Krzyżaniak for Wikia Inc.
+ * @author Krzysztof Krzyżaniak (eloy) <eloy@wikia-inc.com>
+ * @date 2009-02-02
+ * @version 0.1
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+
+/**
+ * used hooks
+ */
+$wgHooks[ "RevisionInsertComplete" ][]	= "HAWelcomeJob::revisionInsertComplete";
+
+/**
+ * register job class
+ */
+$wgJobClasses[ "HAWelcome" ] = "HAWelcomeJob";
+
+/**
+ * used messages
+ */
+$wgExtensionMessagesFiles["HAWelcome"] = dirname(__FILE__) . '/HAWelcome.i18n.php';
+
+class HAWelcomeJob extends Job {
+
+	private $mUser,
+		$isAnon;
+
+	/**
+	 * Construct a job
+	 * @param Title $title The title linked to
+	 * @param array $params Job parameters (table, start and end page_ids)
+	 * @param integer $id job_id
+	 */
+	public function __construct( $title, $params, $id = 0 ) {
+		Wikia::log( __METHOD__, $params );
+		wfLoadExtensionMessages( "HAWelcome" );
+		parent::__construct( "HAWelcome", $title, $params, $id );
+		$user_id = $params[ "user_id" ];
+
+		$this->isAnon = (bool )$params[ "is_anon" ];
+		$this->mUser = User::newFromId( $user_id );
+	}
+
+	/**
+	 * main entry point
+	 *
+	 * @access public
+	 */
+	public function run() {
+
+		global $wgUser;
+
+		/**
+		 * overwrite $wgUser for ~~~~ expanding
+		 */
+		$tmpUser = $wgUser;
+
+		if( $this->mUser ) {
+			/**
+			 * check again if talk page exists
+			 */
+			$talkPage  = $this->mUser->getUserPage()->getTalkPage();
+			if( $talkPage ) {
+				$wgUser    = $this->getLastSysop();
+				$sysopPage = $wgUser->getUserPage()->getTalkPage();
+
+				if( $this->isAnon ) {
+					$welcomeMsg = wfMsg( "hawelcome-message-anon",
+						array( sprintf("%s:%s", $this->title->getNsText(), $this->title->getText() ) ),
+						array( sprintf("%s:%s", $sysopPage->getNsText(), $sysopPage->getText() ) )
+					);
+				}
+				else {
+					$welcomeMsg = wfMsg( "hawelcome-message-user",
+						array( sprintf("%s:%s", $this->title->getNsText(), $this->title->getText() ) ),
+						array( sprintf("%s:%s", $sysopPage->getNsText(), $sysopPage->getText() ) )
+					);
+				}
+				$talkArticle = new Article( $talkPage, 0 );
+				if( ! $talkArticle->exists ) {
+					$talkArticle->doEdit( $welcomeMsg, wfMsg( "hawelcome-message-log" ) );
+				}
+			}
+		}
+
+		$wgUser = $tmpUser;
+		return true;
+	}
+
+	/**
+	 * get last active sysop for this wiki, use local user database and blobs
+	 * from dataware
+	 *
+	 * @access public
+	 */
+	public function getLastSysop() {
+		global $wgCityId;
+
+		$dbr = wfGetDBExt( DB_SLAVE );
+		$Row = $dbr->selectRow(
+			array( "city_local_users", "blobs" ),
+			array( "rev_timestamp", "lu_user_id" ),
+			array(
+				"lu_user_id = rev_user",
+				"lu_allgroups like '%sysop%'",
+				"rev_wikia_id" => $wgCityId
+			),
+			__METHOD__,
+			array( "order by" => "rev_timestamp desc" )
+		);
+
+		return User::newFromId( $Row->lu_user_id );
+#		select rev_timestamp, lu_user_name from city_local_users, blobs where lu_user_name = rev_user_text and lu_allgroups like '%sysop%' and rev_wikia_id = 165 order by rev_timestamp desc limit 1;
+	}
+
+	/**
+	 * revisionInsertComplete
+	 *
+	 * static method called as hook
+	 *
+	 * @static
+	 * @access public
+	 *
+	 * @param Revision	$revision	revision object
+	 * @param string	$url		url to external object
+	 * @param string	$flags		flags for this revision
+	 *
+	 * @return true means process other hooks
+	 */
+	public static function revisionInsertComplete( &$revision, &$url, &$flags ) {
+		global $wgTitle, $wgUser;
+
+		/**
+		 * check if talk page for wgUser exists
+		 *
+		 * @todo check editcount for user
+		 */
+		$talkPage = $wgUser->getUserPage()->getTalkPage();
+		if( $talkPage ) {
+			$talkArticle = new Article( $talkPage, 0 );
+			if( !$talkArticle->exists( ) || 1 ) { // for while every edition create job
+				$welcomeJob = new HAWelcomeJob(
+					$wgTitle,
+					array(
+						"is_anon" => $wgUser->isAnon(),
+						"user_id" => $wgUser->getId(),
+						"user_name" => $wgUser->getName(),
+					)
+				);
+				$welcomeJob->insert();
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * @access public
+	 *
+	 * @return Title instance of Title object
+	 */
+	public function getTitle() {
+		return $this->title;
+	}
+}
---
+++ wikia/releases/200902.1/extensions/wikia/WikiaVideo/WikiaVideo.php
@@ -0,0 +1,84 @@
+<?php
+if(!defined('MEDIAWIKI')) {
+	exit(1);
+}
+
+$wgHooks['ParserBeforeStrip'][] = 'WikiaVideoParserBeforeStrip';
+$wgHooks['ArticleFromTitle'][] = 'WikiaVideoArticleFromTitle';
+$wgHooks['MWNamespace:isMovable'][] = 'WikiaVideoIsNotMovable';
+
+
+function WikiaVideoIsNotMovable( $result, $index ) {
+	global $IP, $wgAllowImageMoving;
+        require_once( "$IP/extensions/wikia/WikiaVideo/VideoPage.php" );
+	$result = !( $index < NS_MAIN || ($index == NS_IMAGE && !$wgAllowImageMoving) || ( $index == NS_VIDEO )  || $index == NS_CATEGORY );
+	return true;
+}
+
+function WikiaVideoParserBeforeStrip($parser, $text, $strip_state) {
+	// TODO change this to accomodate more cases ie parser inside, links and all
+	// MW parser stuff 
+      	$pattern = "@(\[\[Video:)([^\]]*?)].*?\]@si";   	 	 
+	$text = preg_replace_callback($pattern, 'WikiaVideoRenderVideo', $text);
+	return true;
+}
+
+function WikiaVideoRenderVideo( $matches ) {
+        global $IP, $wgOut;
+        require_once( "$IP/extensions/wikia/WikiaVideo/VideoPage.php" );
+        $name = $matches[2];
+        $params = explode( "|", $name );
+        $video_name = $params[0];
+	global $wgCapitalLinks;
+	if( $wgCapitalLinks ) {
+		$video_name = ucfirst( $video_name );
+	}
+
+        $x = 1;
+
+        $width = 300;
+        $align = 'left';
+        $caption = '';
+	$thumb = '';
+
+        foreach($params as $param){
+                if($x > 1) {
+                        $width_check = strpos( $param, "px" );				
+                        if( false !== $width_check ) {
+                                $width = str_replace("px", "", $param);
+			} else if ('thumb' == $param) {
+				$thumb = 'thumb';
+				
+
+                        } else if ( ( 'left' == $param ) || ( 'right' == $param ) ) {
+                                $align = $param;
+                        } else {
+                                $caption = $param;
+                        }
+                }
+                $x++;
+        }
+
+	// macbre: add FCK support
+	global $wgWysiwygParserEnabled;
+
+	if (empty($wgWysiwygParserEnabled)) {
+		$output = "<video name=\"{$video_name}\" width=\"{$width}\" align=\"{$align}\" caption=\"{$caption}\" thumb=\"{$thumb}\"></video>";
+	}
+	else {
+		$output = "<video>[[Video:{$matches[2]}]]</video>";
+	}
+	return $output;
+}
+
+function WikiaVideoArticleFromTitle( $title, $article ) {
+        global $wgUser, $IP;
+
+        require_once( "$IP/extensions/wikia/WikiaVideo/VideoPage.php" );
+
+        if( NS_VIDEO == $title->getNamespace() ) {
+                $article = new VideoPage( $title );
+        }
+        return true;
+}
+
