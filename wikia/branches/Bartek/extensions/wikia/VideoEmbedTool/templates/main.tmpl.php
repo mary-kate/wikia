20b446ecbf30efb41854985f16b8e5ddfbf3da36
bartek <bartek@d23c911d-4c54-0410-aefe-1982d53a7c61>
2009-02-04 15:11:50 +0000

    VideoEmbedTool: change layout for upcoming search

--- wikia/branches/Bartek/extensions/wikia/VideoEmbedTool/templates/main.tmpl.php
+++ wikia/branches/Bartek/extensions/wikia/VideoEmbedTool/templates/main.tmpl.php
@@ -19,6 +19,9 @@
 			<input onkeydown="VET_trySendQuery(event);" type="text" id="VideoQuery" />
                         <input onclick="VET_trySendQuery(event);" type="button" id="VideoQuerySearch" value="<?= wfMsg('vet-find-btn') ?>" />
 		</td>		
+		<td>
+<a href="http://www.metacafe.com" class="image" title="Metacafe" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/metacafe.gif" height="33" width="120" border="0"></a>				
+		</td>
 	<tr id="VideoEmbedAdd">
 		<th><h1><?= wfMsg('vet-upload') ?></h1></th>
 
@@ -41,63 +44,27 @@
 					<form onsubmit="return AIM.submit(this, VET_uploadCallback)" action="<?= $wgScriptPath ?>/index.php?action=ajax&rs=VET&method=insertVideo" id="VideoEmbedForm" method="POST" enctype="multipart/form-data">
 					<input id="VideoEmbedUrl" name="wpVideoEmbedUrl" type="text" size="32" />
 					<input id="VideoEmbedUrlSubmit" type="submit" value="<?= wfMsg('vet-upload-btn') ?>" onclick="return VET_upload(event);" />
-					<div id="VideoEmbedMainMesg"><?= wfMsg( 'vet-main-info' ) ?></div>
 					</form>
 					<?php
 			}
 ?>
 </td>
+<td>
+</td>
 </tr>
 </table>
-
+<div id="VideoEmbedMainMesg"><?= wfMsg( 'vet-main-info' ) ?></div>
+<br/>
 <div id="VET_results_0">
 	<?= $result ?>
 </div>
-<table id="VideoEmbedToolProvidersTable" class="gallery" cellpadding="6" cellspacing="6">
-	<tbody><tr>
-		<td><div class="gallerybox" style="width: 170px;">
-			<div class="thumb" style="padding: 49px 0pt; width: 165px;"><div style="margin-left: auto; margin-right: auto; width: 120px;"><a href="http://www.5min.com" class="image" title="5min" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/5min.gif" height="47" width="120" border="0"></a></div></div>
-			<div class="gallerytext">
-			</div>
-		</div></td>
-		<td><div class="gallerybox">
-			<div class="thumb" style="padding: 47px 0pt; width: 165px;"><div style="margin-left: auto; margin-right: auto; width: 120px;"><a href="http://gamevideos.1up.com/" class="image" title="Gamevideos"  target="_blank"><img alt="Gamevideos" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/gamevideos.jpg" height="51" width="120" border="0"></a></div></div>
-			<div class="gallerytext">
 
-			</div>
-		</div></td>
-		<td><div class="gallerybox" style="width: 170px;">
-			<div class="thumb" style="padding: 56px 0pt; width: 165px;"><div style="margin-left: auto; margin-right: auto; width: 120px;"><a href="http://www.metacafe.com" class="image" title="Metacafe" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/metacafe.gif" height="33" width="120" border="0"></a></div></div>
-			<div class="gallerytext">
-			</div>
-		</div></td>
-		<td><div class="gallerybox" style="width: 170px;">
-			<div class="thumb" style="padding: 55px 0pt; width: 165px;"><div style="margin-left: auto; margin-right: auto; width: 120px;"><a href="http://www.myvideo.de" class="image" title="Myvideo" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/myvideo.jpg" height="35" width="120" border="0"></a></div></div>
-			<div class="gallerytext">
-			</div>
-		</div></td>
-	</tr>
-	<tr>
-		<td><div class="gallerybox" style="width: 170px;">
-			<div class="thumb" style="padding: 62px 0pt; width: 165px;"><div style="margin-left: auto; margin-right: auto; width: 120px;"><a href="http://en.sevenload.com" class="image" title="Sevenload" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/sevenload.jpg" height="21" width="120" border="0"></a></div></div>
-			<div class="gallerytext">
-			</div>
-		</div></td>
-		<td><div class="gallerybox" style="width: 170px;">
-			<div class="thumb" style="padding: 37px 0pt; width: 165px;"><div style="margin-left: auto; margin-right: auto; width: 120px;"><a href="http://www.vimeo.com" class="image" title="Vimeo" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/vimeo.png" height="72" width="120" border="0"></a></div></div>
-			<div class="gallerytext">
-
-			</div>
-		</div></td>
-		<td><div class="gallerybox" style="width: 170px;">
-			<div class="thumb" style="padding: 30px 0pt; width: 165px;"><div style="margin-left: auto; margin-right: auto; width: 120px;"><a href="http://www.youtube.com" class="image" title="YouTube" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/youtube.jpg" height="85" width="120" border="0"></a></div></div>
-			<div class="gallerytext">
-			</div>
-		</div></td>
-
-		<td></td>
-	</tr>
-</tbody></table>
+<a href="http://www.5min.com" class="image" title="5min" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/5min.gif" height="47" width="120" border="0"></a>
+<a href="http://gamevideos.1up.com/" class="image" title="Gamevideos"  target="_blank"><img alt="Gamevideos" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/gamevideos.jpg" height="51" width="120" border="0"></a>
+<a href="http://www.myvideo.de" class="image" title="Myvideo" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/myvideo.jpg" height="35" width="120" border="0"></a>
+<a href="http://en.sevenload.com" class="image" title="Sevenload" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/sevenload.jpg" height="21" width="120" border="0"></a>
+<a href="http://www.vimeo.com" class="image" title="Vimeo" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/vimeo.png" height="72" width="120" border="0"></a>
+<a href="http://www.youtube.com" class="image" title="YouTube" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/youtube.jpg" height="85" width="120" border="0"></a>
 
 <div id="VET_results_1" style="display: none;">
 <br/><br/><br/><br/><br/>
