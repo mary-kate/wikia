0360e6b1cae528df65d6e7c2de8d48cc6517ac64
bartek <bartek@d23c911d-4c54-0410-aefe-1982d53a7c61>
2009-02-05 14:17:51 +0000

    VideoEmbedTool: load QVA messages separately from VET

---
+++ wikia/branches/Bartek/extensions/wikia/VideoEmbedTool/QuickVideoAdd.i18n.php
@@ -0,0 +1,10 @@
+<?php
+$messages = array();
+$messages['en'] = array(
+	'qva-success' => 'Video page $1 was successfully added.',
+        'qva-failure' => 'Please supply valid parameters.',
+        'qva-name' => 'Enter the name:',
+        'qva-url' => 'Enter the URL:',
+        'qva-add' => 'Add',
+        'qva-title' => 'Quickly Add Video From URL',
+);
