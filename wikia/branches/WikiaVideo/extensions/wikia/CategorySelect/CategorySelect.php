eac1307f194e6a8331eb8ba7afcf034bc648b83c
Inez Korczyński <inez@users.noreply.github.com>
2009-02-12 17:43:13 +0000

    * Create branch to work on WikiaVideo extension

---
+++ wikia/branches/WikiaVideo/extensions/wikia/CategorySelect/CategorySelect.php
@@ -0,0 +1,322 @@
+<?php
+
+/**
+ * CategorySelect
+ *
+ * A CategorySelect extension for MediaWiki
+ * Provides an interface for managing categories in article without editing whole article
+ *
+ * @author Maciej Błaszkowski (Marooned) <marooned at wikia-inc.com>
+ * @date 2009-01-13
+ * @copyright Copyright (C) 2009 Maciej Błaszkowski, Wikia Inc.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ * @package MediaWiki
+ *
+ * To activate this functionality, place this file in your extensions/
+ * subdirectory, and add the following line to LocalSettings.php:
+ *     require_once("$IP/extensions/wikia/CategorySelect/CategorySelect.php");
+ */
+
+if (!defined('MEDIAWIKI')) {
+	echo "This is MediaWiki extension named CategorySelect.\n";
+	exit(1) ;
+}
+
+$wgExtensionCredits['other'][] = array(
+	'name' => 'CategorySelect',
+	'author' => '[http://www.wikia.com/wiki/User:Marooned Maciej Błaszkowski (Marooned)]',
+	'description' => 'Provides an interface for managing categories in article without editing whole article.'
+);
+
+$wgExtensionFunctions[] = 'CategorySelectInit';
+$wgExtensionMessagesFiles['CategorySelect'] = dirname(__FILE__) . '/CategorySelect.i18n.php';
+$wgAjaxExportList[] = 'CategorySelectAjaxGetCategories';
+
+/**
+ * Initialize hooks
+ *
+ * @author Maciej Błaszkowski <marooned at wikia-inc.com>
+ */
+function CategorySelectInit() {
+	global $wgHooks, $wgCategorySelectEnabled, $wgAutoloadClasses;
+	$wgAutoloadClasses['CategorySelect'] = 'extensions/wikia/CategorySelect/CategorySelect_body.php';
+//	$wgHooks['OutputPageBeforeHTML'][] = 'CategorySelectOutput';
+	$wgHooks['EditPageAfterGetContent'][] = 'CategorySelectReplaceContent';
+	$wgHooks['EditPage::CategoryBox'][] = 'CategorySelectCategoryBox';
+	$wgHooks['EditPage::importFormData::finished'][] = 'CategorySelectImportFormData';
+	$wgHooks['EditPage::showEditForm:fields'][] = 'CategorySelectAddFormFields';
+	$wgHooks['EditPage::showDiff::begin'][] = 'CategorySelectDiffArticle';
+	$wgHooks['EditForm::MultiEdit:Form'][] = 'CategorySelectDisplayCategoryBox';
+	$wgHooks['getCategoryLinks'][] = 'CategorySelectGetCategoryLinks';
+	$wgHooks['ExtendJSGlobalVars'][] = 'CategorySelectSetupVars';
+	wfLoadExtensionMessages('CategorySelect');
+}
+
+/**
+ * Set variables for JS usage
+ *
+ * @author Maciej Błaszkowski <marooned at wikia-inc.com>
+ */
+function CategorySelectSetupVars($vars) {
+	global $wgParser, $wgContLang;
+
+	$vars['csAddCategoryButtonText'] = wfMsg('categoryselect-addcategory-button');
+	$vars['csProvideCategoryCaption'] = wfMsg('categoryselect-provide-order');
+	$vars['csProvideCategoryText'] = wfMsg('categoryselect-provide-sortkey');
+	$vars['csProvideCategorySave'] = wfMsg('save');
+	$vars['csDefaultSort'] = $wgParser->getDefaultSort();
+	$vars['csCategoryNamespaces'] = 'Category|' . $wgContLang->getNsText(NS_CATEGORY);
+
+	return true;
+}
+
+/**
+ * Get categories via AJAX that are matching typed text [for suggestion dropdown]
+ *
+ * @author Maciej Błaszkowski <marooned at wikia-inc.com>
+ */
+function CategorySelectAjaxGetCategories() {
+	global $wgRequest;
+	$cat = $wgRequest->getText('query');
+
+	$dbr = wfGetDB(DB_SLAVE);
+	$res = $dbr->select(
+		'category',
+		'cat_title',
+		'cat_title LIKE "%' . $dbr->escapeLike($cat) . '%"',
+		__METHOD__,
+		array('LIMIT' => '10')
+	);
+
+	$categories = '';
+	while($row = $dbr->fetchObject($res)) {
+		$categories .= str_replace('_', ' ', $row->cat_title) . "\n";
+	}
+
+	$ar = new AjaxResponse($categories);
+	$ar->setCacheDuration(60 * 20);
+
+	return $ar;
+}
+
+/**
+ * Replace content of edited article [with cutted out categories]
+ *
+ * @author Maciej Błaszkowski <marooned at wikia-inc.com>
+ */
+function CategorySelectReplaceContent($text) {
+	$data = CategorySelect::SelectCategoryAPIgetData($text);
+	$text = $data['wikitext'];
+	return true;
+}
+
+/**
+ * Remove hidden category box
+ *
+ * @author Maciej Błaszkowski <marooned at wikia-inc.com>
+ */
+function CategorySelectCategoryBox($text) {
+	$text = '';
+	return true;
+}
+
+/**
+ * Get categories via AJAX
+ *
+ * @author Maciej Błaszkowski <marooned at wikia-inc.com>
+ */
+//function CategorySelectAjaxGetCategoriesXYZ($titleName) {
+//	$result = array('error' => null, 'wikitext' => null, 'categories' => null);
+//	$title = Title::newFromText($titleName);
+//	if($title->exists()) {
+//		$rev = Revision::newFromTitle($title);
+//		$wikitext = $rev->getText();
+//		$data = CategorySelect::SelectCategoryAPIgetData($wikitext);
+//		$result = array_merge($results, $data);
+//	} else {
+//		$result['error'] = wfMsg('');
+//	}
+//	$ar = new AjaxResponse(Wikia::json_encode($results));
+//	$ar->setCacheDuration(60 * 20);
+//	return $ar;
+//}
+
+/**
+ * Test function - display CS above article in view mode
+ *
+ * @author Maciej Błaszkowski <marooned at wikia-inc.com>
+ */
+function CategorySelectOutput($out, $text) {
+	global $wgOut, $wgExtensionsPath, $wgStyleVersion, $wgTitle, $wgCategorySelectMetaData;
+
+	if (!is_array($wgCategorySelectMetaData)) {
+		$rev = Revision::newFromTitle($wgTitle);
+		if (!is_null($rev)) {
+			$wikitext = $rev->getText();
+			CategorySelect::SelectCategoryAPIgetData($wikitext);
+		}
+	}
+
+	$html = CategorySelectGenerateHTML();
+	$wgOut->addHTML($html);
+
+	return true;
+}
+
+/**
+ * Change format of categories metadata
+ *
+ * @author Maciej Błaszkowski <marooned at wikia-inc.com>
+ */
+function CategorySelectChangeFormat($categories, $from, $to) {
+	if ($from == 'json') {
+		$categories = Wikia::json_decode($categories, true);
+	}
+
+	if ($to == 'wiki') {
+		$categoriesStr = '';
+		foreach($categories as $c) {
+			$catTmp = '[[' . $c['namespace'] . ':' . $c['category'] . ($c['sortkey'] == '' ? '' : ('|' . $c['sortkey'])) . ']]';
+			if ($c['outerTag'] != '') {
+				$catTmp = '<' . $c['outerTag'] . '>' . $catTmp . '</' . $c['outerTag'] . '>';
+			}
+			$categoriesStr .= $catTmp . "\n";
+		}
+		return "\n" . $categoriesStr;
+	} elseif ($to == 'array') {
+		return $categories;
+	}
+
+	if ($from == 'array' && $to == 'json') {
+		return Wikia::json_encode($categories);
+	}
+}
+
+/**
+ * Add hidden field with category metadata
+ *
+ * @author Maciej Błaszkowski <marooned at wikia-inc.com>
+ */
+function CategorySelectAddFormFields($editPage, $wgOut) {
+	global $wgCategorySelectMetaData;
+	$categories = '';
+	if (!empty($wgCategorySelectMetaData)) {
+		$categories = htmlspecialchars(CategorySelectChangeFormat($wgCategorySelectMetaData['categories'], 'array', 'json'));
+	}
+	$wgOut->addHTML("<input type=\"hidden\" value=\"$categories\" name=\"wpCategorySelectWikitext\" id=\"wpCategorySelectWikitext\" />");
+	$wgOut->addHTML('<input type="hidden" value="wiki" id="wpCategorySelectSourceType" name="wpCategorySelectSourceType" />');
+	return true;
+}
+
+/**
+ * Concatenate categories on EditPage POST
+ *
+ * @author Maciej Błaszkowski <marooned at wikia-inc.com>
+ */
+function CategorySelectImportFormData($editPage, $request) {
+	global $wgCategorySelectCategoriesInWikitext;
+	if ($request->wasPosted()) {
+		$sourceType = $request->getVal('wpCategorySelectSourceType');
+		if ($sourceType == 'wiki') {
+			$categories = $editPage->safeUnicodeInput($request, 'csWikitext');
+		} else {	//json
+			$categories = $editPage->safeUnicodeInput($request, 'wpCategorySelectWikitext');
+			$categories = CategorySelectChangeFormat($categories, 'json', 'wiki');
+		}
+
+		if ($editPage->preview || $editPage->diff) {
+			CategorySelect::SelectCategoryAPIgetData($categories);
+		} else {	//saving article
+			$editPage->textbox1 .= "\n" . $categories;
+		}
+		$wgCategorySelectCategoriesInWikitext = $categories;
+	}
+	return true;
+}
+
+/**
+ * Add categories to article for DiffEngine
+ *
+ * @author Maciej Błaszkowski <marooned at wikia-inc.com>
+ */
+function CategorySelectDiffArticle($editPage, $oldtext, $newtext) {
+	global $wgCategorySelectCategoriesInWikitext;
+	//add categories only for whole article editing
+	if ($editPage->section == '' && isset($wgCategorySelectCategoriesInWikitext)) {
+		$newtext .= "\n" . $wgCategorySelectCategoriesInWikitext;
+	}
+	return true;
+}
+
+/**
+ * Display category box
+ *
+ * @author Maciej Błaszkowski <marooned at wikia-inc.com>
+ */
+function CategorySelectDisplayCategoryBox($rows, $cols, $ew, $textbox) {
+	global $wgCategorySelectMetaData, $wgRequest, $wgOut;
+
+	$action = $wgRequest->getVal('action', 'view');
+	if ($action != 'view' && $action != 'purge') {
+		if (!is_array($wgCategorySelectMetaData)) {
+			global $wgTitle;
+			$rev = Revision::newFromTitle($wgTitle);
+			if (!is_null($rev)) {
+				$wikitext = $rev->getText();
+				CategorySelect::SelectCategoryAPIgetData($wikitext);
+			}
+		}
+
+		$wgOut->addHTML( CategorySelectGenerateHTML('editform') );
+	}
+	return true;
+}
+
+/**
+ * Remove regular category list under article
+ *
+ * @author Maciej Błaszkowski <marooned at wikia-inc.com>
+ */
+function CategorySelectGetCategoryLinks($categoryLinks) {
+	global $wgRequest;
+
+	$action = $wgRequest->getVal('action', 'view');
+	//remove it for edit page
+	if ($action == 'edit' || $action == 'submit') {
+		$categoryLinks = '';
+		return false;
+	}
+	return true;
+}
+
+/**
+ * Add required JS & CSS and return HTML
+ *
+ * @author Maciej Błaszkowski <marooned at wikia-inc.com>
+ */
+function CategorySelectGenerateHTML($formId = '') {
+	global $wgOut, $wgExtensionsPath, $wgStyleVersion, $wgCategorySelectMetaData;
+
+	$wgOut->addScript("<script type=\"text/javascript\">var formId = '$formId';</script>");
+	$wgOut->addScript("<script type=\"text/javascript\" src=\"$wgExtensionsPath/wikia/CategorySelect/CategorySelect.js?$wgStyleVersion\"></script>");
+	$wgOut->addScript("<link rel=\"stylesheet\" type=\"text/css\" href=\"$wgExtensionsPath/wikia/CategorySelect/CategorySelect.css?$wgStyleVersion\" />");
+
+	$categories = is_null($wgCategorySelectMetaData) ? '' : CategorySelectChangeFormat($wgCategorySelectMetaData['categories'], 'array', 'wiki');
+
+	$result = '
+	<script type="text/javascript">document.write(\'<style type="text/css">#csWikitextContainer {display: none}</style>\');</script>
+	<div id="csMainContainer">
+		<div id="csSuggestContainer">
+			<div id="csHintContainer">' . wfMsg('categoryselect-suggest-hint') . '</div>
+		</div>
+		<div id="csItemsContainer">
+			<input id="csCategoryInput" type="text" style="display: none" />
+		</div>
+		<div id="csWikitextContainer"><textarea id="csWikitext" name="csWikitext">' . $categories . '</textarea></div>
+		<div id="csCodeView"><a href="#" onclick="toggleCodeView(); return false;" onfocus="this.blur()" tabindex="-1">' . wfMsg('categoryselect-code-view') . '</a></div>
+		<div class="clearfix"></div>
+	</div>
+	';
+
+	return $result;
+}
---
+++ wikia/branches/WikiaVideo/extensions/wikia/HAWelcome/HAWelcome.php
@@ -0,0 +1,362 @@
+<?php
+
+/**
+ * HAWelcomeJob -- Welcome user after first edit
+ *
+ * @file
+ * @ingroup JobQueue
+ *
+ * @copyright Copyright © Krzysztof Krzyżaniak for Wikia Inc.
+ * @author Krzysztof Krzyżaniak (eloy) <eloy@wikia-inc.com>
+ * @date 2009-02-02
+ * @version 0.1
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+	echo "This is a MediaWiki extension and cannot be used standalone.\n";
+	exit( 1 );
+}
+
+$wgExtensionCredits['other'][] = array(
+	'name' => 'HAWelcome',
+	'version' => '0.1',
+	'author' => 'Krzysztof Krzyźaniak',
+	'description' => 'Highly Automated Welcome Tool ',
+);
+
+
+/**
+ * used hooks
+ */
+$wgHooks[ "RevisionInsertComplete" ][]	= "HAWelcomeJob::revisionInsertComplete";
+
+/**
+ * register job class
+ */
+$wgJobClasses[ "HAWelcome" ] = "HAWelcomeJob";
+
+/**
+ * used messages
+ */
+$wgExtensionMessagesFiles[ "HAWelcome" ] = dirname(__FILE__) . '/HAWelcome.i18n.php';
+
+/**
+ * register task
+ */
+$wgWikiaBatchTasks[ "welcome" ] = "HAWelcomeTask";
+
+class HAWelcomeJob extends Job {
+
+	private
+		$mUser,
+		$mAnon,
+		$mSysop;
+
+	const WELCOMEUSER = "Wikia";
+
+	/**
+	 * Construct a job
+	 * @param Title $title The title linked to
+	 * @param array $params Job parameters (table, start and end page_ids)
+	 * @param integer $id job_id
+	 */
+	public function __construct( $title, $params, $id = 0 ) {
+		Wikia::log( __METHOD__, $params );
+		wfLoadExtensionMessages( "HAWelcome" );
+		parent::__construct( "HAWelcome", $title, $params, $id );
+		$user_id = $params[ "user_id" ];
+
+		$this->mAnon = (bool )$params[ "is_anon" ];
+		$this->mUser = User::newFromId( $user_id );
+	}
+
+	/**
+	 * main entry point
+	 *
+	 * @access public
+	 */
+	public function run() {
+		global $wgUser, $wgDevelEnvironment;
+
+		wfProfileIn( __METHOD__ );
+		/**
+		 * overwrite $wgUser for ~~~~ expanding
+		 */
+		$tmpUser = $wgUser;
+		$wgUser  = User::newFromName( self::WELCOMEUSER );
+
+		if( $this->mUser && $this->mUser->getName() !== self::WELCOMEUSER ) {
+			/**
+			 * check again if talk page exists
+			 */
+			$talkPage  = $this->mUser->getUserPage()->getTalkPage();
+			if( $talkPage ) {
+				$sysop     = $this->getLastSysop();
+				$sysopPage = $sysop->getUserPage()->getTalkPage();
+				$signature = $this->expandSig();
+
+				$talkArticle = new Article( $talkPage, 0 );
+
+				if( ! $talkArticle->exists() || $wgDevelEnvironment ) {
+					if( $this->mAnon ) {
+						$welcomeMsg = wfMsg( "hawelcome-message-anon", array(
+							sprintf("%s:%s", $this->title->getNsText(), $this->title->getText() ),
+							sprintf("%s:%s", $sysopPage->getNsText(), $sysopPage->getText() ),
+							$signature
+						));
+					}
+					else {
+						$welcomeMsg = wfMsg( "hawelcome-message-user", array(
+							sprintf("%s:%s", $this->title->getNsText(), $this->title->getText() ),
+							sprintf("%s:%s", $sysopPage->getNsText(), $sysopPage->getText() ),
+							$signature
+						));
+					}
+					$talkArticle->doEdit( $welcomeMsg, wfMsg( "hawelcome-message-log" ) );
+				}
+			}
+		}
+
+		$wgUser = $tmpUser;
+		wfProfileOut( __METHOD__ );
+
+		return true;
+	}
+
+	/**
+	 * get last active sysop for this wiki, use local user database and blobs
+	 * from dataware
+	 *
+	 * @access public
+	 */
+	public function getLastSysop() {
+		global $wgCityId;
+
+		wfProfileIn( __METHOD__ );
+
+		if( ! $this->mSysop instanceof User ) {
+			$dbr = wfGetDB( DB_SLAVE );
+			$res = $dbr->query("
+				SELECT rev_user, rev_timestamp
+				FROM revision
+				WHERE revision.rev_user IN (
+					SELECT ug_user
+					FROM user_groups
+					WHERE ug_group IN ( 'staff', 'sysop', 'helper')
+				)
+				ORDER BY rev_timestamp DESC
+				LIMIT 1",
+				__METHOD__
+			);
+			$row = $dbr->fetchObject( $res );
+			$dbr->freeResult( $res );
+
+			$this->mSysop = User::newFromId( $row->rev_user );
+		}
+
+		wfProfileOut( __METHOD__ );
+
+		return $this->mSysop;
+	}
+
+	/**
+	 * revisionInsertComplete
+	 *
+	 * static method called as hook
+	 *
+	 * @static
+	 * @access public
+	 *
+	 * @param Revision	$revision	revision object
+	 * @param string	$url		url to external object
+	 * @param string	$flags		flags for this revision
+	 *
+	 * @return true means process other hooks
+	 */
+	public static function revisionInsertComplete( &$revision, &$url, &$flags ) {
+		global $wgTitle, $wgUser, $wgDevelEnvironment, $wgCityId;
+
+		wfProfileIn( __METHOD__ );
+		if( trim( wfMsg( "hawelcome" ) ) !== "@disabled" ) {
+			/**
+			 * check if talk page for wgUser exists
+			 *
+			 * @todo check editcount for user
+			 */
+			$talkPage = $wgUser->getUserPage()->getTalkPage();
+			if( $talkPage ) {
+				$talkArticle = new Article( $talkPage, 0 );
+				if( !$talkArticle->exists( ) || $wgDevelEnvironment ) {
+					$welcomeJob = new HAWelcomeJob(
+						$wgTitle,
+						array(
+							"is_anon" => $wgUser->isAnon(),
+							"user_id" => $wgUser->getId(),
+							"user_name" => $wgUser->getName(),
+						)
+					);
+					$welcomeJob->insert();
+
+					/**
+					 * inform task manager
+					 */
+					$Task = new HAWelcomeTask();
+					$Task->createTask( array( "city_id" => $wgCityId ), TASK_QUEUED  );
+				}
+			}
+		}
+		wfProfileOut( __METHOD__ );
+
+		return true;
+	}
+
+	/**
+	 * expandSig -- hack, expand signature from message for sysop
+	 *
+	 * @access private
+	 */
+	private function expandSig( ) {
+
+		global $wgContLang, $wgUser;
+
+		wfProfileIn( __METHOD__ );
+
+		$Sysop = $this->getLastSysop();
+		$tmpUser = $wgUser;
+		$wgUser = $Sysop;
+		$signature = sprintf(
+			"-- [[User:%s|%s]] ([[User_talk:%s|%s]]) %s",
+			wfEscapeWikiText( $Sysop->getName() ),
+			wfEscapeWikiText( $Sysop->getName() ),
+			wfEscapeWikiText( $Sysop->getName() ),
+			wfMsg( "talkpagelinktext" ),
+			$wgContLang->timeanddate( wfTimestampNow( TS_MW ) )
+		);
+		$wgUser = $tmpUser;
+
+		wfProfileOut( __METHOD__ );
+
+		return $signature;
+	}
+
+	/**
+	 * @access public
+	 *
+	 * @return Title instance of Title object
+	 */
+	public function getTitle() {
+		return $this->title;
+	}
+}
+
+
+/**
+ * Task, will run runJobs for specified city_id
+ */
+class HAWelcomeTask extends BatchTask {
+
+	private $mParams;
+
+	/**
+	 * contructor
+	 * @access public
+	 */
+	public function  __construct() {
+		$this->mType = "welcome";
+		$this->mVisible = false;
+		$this->mTTL = 1800;
+		parent::__construct();
+		$this->mDebug = false;
+	}
+
+	/**
+	 * execute
+	 *
+	 * entry point for TaskExecutor
+	 *
+	 * @access public
+	 * @author eloy@wikia
+	 *
+	 * @param mixed $params default null - task data from wikia_tasks table
+	 *
+	 * @return boolean - status of operation
+	 */
+	public function execute( $params = null ) {
+		global $IP, $wgWikiaLocalSettingsPath, $wgWikiaAdminSettingsPath;
+
+		$this->mParams = unserialize( $params->task_arguments );
+		$city_id = $this->mParams["city_id"];
+		if( $city_id ) {
+			/**
+			 * execute maintenance script
+			 */
+			$cmd = sprintf( "SERVER_ID={$city_id} php {$IP}/maintenance/runJobs.php --type HAWelcome --conf {$wgWikiaLocalSettingsPath} --aconf {$wgWikiaAdminSettingsPath}" );
+			$this->addLog( "Running {$cmd}" );
+			$retval = wfShellExec( $cmd, $status );
+			$this->addLog( $retval );
+		}
+
+		return true;
+	}
+
+	/**
+	 * getForm
+	 *
+	 * this task is not visible in selector so it doesn't have real HTML form
+	 *
+	 * @access public
+	 * @author eloy@wikia
+	 *
+	 * @param Title $title: Title struct
+	 * @param mixes $data: params from HTML form
+	 *
+	 * @return false
+	 */
+	public function getForm( $title, $data = false ) {
+		return false;
+	}
+
+	/**
+	 * getType
+	 *
+	 * return string with codename type of task
+	 *
+	 * @access public
+	 * @author eloy@wikia
+	 *
+	 * @return string: unique name
+	 */
+	public function getType() {
+		return $this->mType;
+	}
+
+	/**
+	 * isVisible
+	 *
+	 * check if class is visible in TaskManager from dropdown
+	 *
+	 * @access public
+	 * @author eloy@wikia
+	 *
+	 * @return boolean: visible or not
+	 */
+	public function isVisible() {
+		return $this->mVisible;
+	}
+
+	/**
+	 * submitForm
+	 *
+	 * since this task is invisible for form selector we use this method for
+	 * saving request data in database
+	 *
+	 * @access public
+	 * @author eloy@wikia
+	 *
+	 * @return true
+	 */
+	public function submitForm() {
+		return true;
+	}
+}
