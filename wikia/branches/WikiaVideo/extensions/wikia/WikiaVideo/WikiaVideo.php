77d913bcde921d79d22af05878f88bd69a554c75
Inez Korczyński <inez@users.noreply.github.com>
2009-02-13 15:56:52 +0000

    * Video gallery tag support

--- wikia/branches/WikiaVideo/extensions/wikia/WikiaVideo/WikiaVideo.php
+++ wikia/branches/WikiaVideo/extensions/wikia/WikiaVideo/WikiaVideo.php
@@ -3,8 +3,67 @@ if(!defined('MEDIAWIKI')) {
 	exit(1);
 }
 
-$wgExtraNamespaces[NS_VIDEO] = 'Video';
-$wgAutoloadClasses['VideoPage'] = dirname(__FILE__). '/VideoPage.php';
+$wgExtensionFunctions[] = 'WikiaVideo_init';
+
+function WikiaVideo_init() {
+	global $wgExtraNamespaces, $wgAutoloadClasses, $wgParser;
+	$wgExtraNamespaces[NS_VIDEO] = 'Video';
+	$wgAutoloadClasses['VideoPage'] = dirname(__FILE__). '/VideoPage.php';
+	$wgParser->setHook('videogallery', 'WikiaVideo_renderVideoGallery');
+	return true;
+}
+
+function WikiaVideo_renderVideoGallery($input, $args, $parser) {
+	$out = '';
+	$videos = array();
+
+	$lines = explode("\n", $input);
+	foreach($lines as $line) {
+		$matches = array();
+		preg_match( "/^([^|]+)(\\|(.*))?$/", $line, $matches );
+
+		if(count($matches) == 0) {
+			continue;
+		}
+
+		if(strpos($matches[0], '%') !== false) {
+			$matches[1] = urldecode($matches[1]);
+		}
+
+		$tp = Title::newFromText($matches[1]);
+		$nt =& $tp;
+
+		if(is_null($nt)) {
+			continue;
+		}
+
+		if(isset($matches[3])) {
+			$html = $parser->recursiveTagParse(trim($matches[3]));
+		} else {
+			$html = '';
+		}
+
+		$videos[] = array($tp, $html);
+	}
+
+	if(count($videos) > 0) {
+		$out .= '<table class="gallery" cellspacing="0" cellpadding="0"><tr>';
+
+		for($i = 0; $i < count($videos); $i++) {
+			$video = new VideoPage($videos[$i][0]);
+			$video->load();
+			$out .= '<td><div class="gallerybox" style="width: 335px;"><div class="thumb" style="padding: 13px 0; width: 330px;"><div style="margin-left: auto; margin-right: auto; width: 300px;">'.$video->getEmbedCode().'</div></div><div class="gallerytext">'.(!empty($videos[$i][1]) ? $videos[$i][1] : '').'</div></div></td>';
+
+			if($i%2 == 1) {
+				$out .= '</tr></tr>';
+			}
+		}
+
+		$out .= '</tr></table>';
+	}
+
+	return $out;
+}
 
 function WikiaVideo_makeVideo($title, $options, $sk) {
 	wfProfileIn('WikiaVideo_makeVideo');
