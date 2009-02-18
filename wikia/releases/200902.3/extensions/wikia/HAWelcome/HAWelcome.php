2e0e3b87f4bd386109056417a1c7246c7e87cf01
eloy <eloy@d23c911d-4c54-0410-aefe-1982d53a7c61>
2009-02-18 18:31:30 +0000

    debug file

--- wikia/releases/200902.3/extensions/wikia/HAWelcome/HAWelcome.php
+++ wikia/releases/200902.3/extensions/wikia/HAWelcome/HAWelcome.php
@@ -352,11 +352,14 @@ class HAWelcomeTask extends BatchTask {
 	 * @access public
 	 */
 	public function  __construct() {
+		global $wgDebugLogFile;
+
 		$this->mType = "welcome";
 		$this->mVisible = false;
 		$this->mTTL = 1800;
 		parent::__construct();
 		$this->mDebug = false;
+		$wgDebugLogFile  = "/tmp/welcome-task.log";
 	}
 
 	/**
