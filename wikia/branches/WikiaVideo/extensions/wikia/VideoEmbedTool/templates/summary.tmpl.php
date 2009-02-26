32fef1b863a1d9e10f66a6c8884460224bab890c
bartek <bartek@d23c911d-4c54-0410-aefe-1982d53a7c61>
2009-02-26 09:58:25 +0000

    VideoEmbedTool: updating WikiaVideo dev branch up to latest trunk

--- wikia/branches/WikiaVideo/extensions/wikia/VideoEmbedTool/templates/summary.tmpl.php
+++ wikia/branches/WikiaVideo/extensions/wikia/VideoEmbedTool/templates/summary.tmpl.php
@@ -1,9 +1,10 @@
 <div id="VideoEmbedSuccess">
-<?= wfMsg('vet-success') ?>
+<?= wfMsg( 'vet-success' ) ?>
+<?= $message ?>
 </div>
 <div style="text-align: center;">
-	<div id="VideoEmbedTag" style="border-bottom: 1px solid #CCC; padding: 15px 0; margin: 15px;"><?= $tag ?></div>
-	<div id="VideoEmbedPageSuccess" style="display:none;"><?= wfMsg( 'vet-page-success' ) ?></br><br/></div>
+	<div id="VideoEmbedPageSuccess" style="display:none;"><?= $message ?></br><br/></div>
 	<input onclick="VET_close(event);" type="button" value="<?= wfMsg( 'vet-return' ) ?>" />
 	<div id="VideoEmbedCode" style="display: none;" ><?= $code ?></div>
+	<input type="hidden" id="VideoEmbedTag" value="<?= htmlspecialchars( $tag ) ?>" />
 </div>
