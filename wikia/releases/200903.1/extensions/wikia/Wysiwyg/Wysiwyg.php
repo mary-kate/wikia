66da195269b82efa909feddae4a58e73c21ee7b3
inez <inez@d23c911d-4c54-0410-aefe-1982d53a7c61>
2009-03-06 15:24:17 +0000

    * Merge r6669,6674 from trunk

--- wikia/releases/200903.1/extensions/wikia/Wysiwyg/Wysiwyg.php
+++ wikia/releases/200903.1/extensions/wikia/Wysiwyg/Wysiwyg.php
@@ -401,6 +401,9 @@ function Wysiwyg_WikiTextToHtml($wikitext, $articleId = -1, $encode = false) {
 
 		$html = preg_replace('/\x7f-wtb-(\d+)-\x7f.*?\x7f-wte-\1-\x7f/sie', "'<input type=\"button\" refid=\"\\1\" _fck_type=\"template\" value=\"'.\$wgWysiwygMetaData[\\1]['name'].'\" class=\"wysiwygDisabled wysiwygTemplate\" /><input value=\"'.htmlspecialchars(stripslashes(\$templateCallsParsed[\\1])).'\" style=\"display:none\" />'", $html);
 	}
+	$html = str_replace("\n", "<!--EOL1-->\n", $html);
+
+	$html = preg_replace_callback("/<li>(\s*)/", create_function('$matches','return "<li space_after=\"".$matches[1]."\">";'), $html);
 
 	wfDebug("Wysiwyg_WikiTextToHtml html: {$html}\n");
 
@@ -409,6 +412,12 @@ function Wysiwyg_WikiTextToHtml($wikitext, $articleId = -1, $encode = false) {
 
 function Wysiwyg_HtmlToWikiText($html, $wysiwygData, $decode = false) {
 	global $wgUseNewReverseParser;
+
+	$html = str_replace("<!--EOL1-->\n", "", $html);
+	$html = str_replace("<!--EOL1--> ", " ", $html);
+	$html = str_replace("<!--EOL1-->", " ", $html);
+
+	$html = preg_replace_callback("/<li space_after=\"(\s*)\">/", create_function('$matches','return "<li>".$matches[1];'), $html);
 
 	if (empty($wgUseNewReverseParser)) {
 		require_once(dirname(__FILE__).'/ReverseParser.php');
