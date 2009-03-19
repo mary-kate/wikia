bd42368537569b1e228c423d7aa368eab429e957
inez <inez@d23c911d-4c54-0410-aefe-1982d53a7c61>
2009-03-19 15:40:05 +0000

    * Improvement

--- wikia/releases/200903.3/skins/Monaco.php
+++ wikia/releases/200903.3/skins/Monaco.php
@@ -2067,7 +2067,7 @@ if(count($wikiafooterlinks) > 0) {
 			<div id="sidebar_1" class="sidebar">
 			<?= WidgetFramework::getInstance()->Draw(1) ?>
 <div style="display: none">
-<img src="http://images.wikia.com/common/releases_200902.4/skins/monobook/blank.gif?js_test"/><noscript><img src="http://images.wikia.com/common/releases_200902.4/skins/monobook/blank.gif?js_test_disabled"/></noscript>
+<img src="http://images.wikia.com/common/releases_200902.4/skins/monobook/blank.gif?js_test_blah"/><noscript><img src="http://images.wikia.com/common/releases_200902.4/skins/monobook/blank.gif?js_test_blah_disabled"/></noscript>
 </div>
 			<?php
 				echo AdEngine::getInstance()->getPlaceHolderDiv('LEFT_NAVBOX_2', false);
