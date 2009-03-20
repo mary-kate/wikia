eb011b6a9643161934e3c17a6f81e4757d1ad032
bartek <bartek@d23c911d-4c54-0410-aefe-1982d53a7c61>
2009-03-20 11:37:37 +0000

    VideoEmbedTool: add some log info too

--- wikia/branches/WikiaVideo/maintenance/cleanupImages.php
+++ wikia/branches/WikiaVideo/maintenance/cleanupImages.php
@@ -43,12 +43,14 @@ class ImageCleanup extends TableCleanup {
 	function processPage( $row ) {
 		global $wgContLang;
 
+		$source = $row->img_name;
+
 		// do nothing for videos, regardless of anything else
 		if ( ( 'VIDEO' == $row->img_media_type ) && ( 'video' == $row->img_major_mime ) && ( 'swf' == $row->img_minor_mime ) ) {
+			$this->log( "omitting video row '$source'" );
 			return $this->progress( 0 );
 		}
 
-		$source = $row->img_name;
 		if( $source == '' ) {
 			// Ye olde empty rows. Just kill them.
 			$this->killRow( $source );
