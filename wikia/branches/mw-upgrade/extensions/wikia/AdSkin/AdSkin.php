72a7b4ec405a8be240b760dc3cbe123cfdecf079
emil <emil@d23c911d-4c54-0410-aefe-1982d53a7c61>
2009-03-22 20:04:50 +0000

    a branch for mw upgrade

---
+++ wikia/branches/mw-upgrade/extensions/wikia/AdSkin/AdSkin.php
@@ -0,0 +1,104 @@
+<?php
+/*
+ * Author: Christian Williams 
+ */
+
+$wgAdSkinVersion = 4;
+
+$wgHooks['GetHTMLAfterBody'][] = 'RenderAdSkin';
+$wgHooks['SpecialFooterAfterWikia'][] = 'RenderAdSkinJS';
+$wgHooks['MonacoAdLink'][] = 'RenderAdLink';
+
+function RenderAdSkin() {
+	global $wgAdSkin, $wgExtensionsPath, $wgUser, $wgWikiaLogo, $wgAdSkinVersion;
+
+	// Disable for logged in users
+	if (is_object($wgUser) && $wgUser->isLoggedIn() ){
+		return true;
+	}
+
+	// Disable if not the main page
+	if (!ArticleAdLogic::isMainPage()) {
+		return true;
+	}
+
+	if (isset($wgAdSkin)) {
+		switch ($wgAdSkin) {
+		case "wow_lich_king":
+			echo '<link rel="stylesheet" type="text/css" href="'. $wgExtensionsPath .'/wikia/AdSkin/css/wow_lich_king.css?'. $wgAdSkinVersion .'" />';
+			break;
+		case "wow_lich_king_warhammer":
+			echo '<link rel="stylesheet" type="text/css" href="'. $wgExtensionsPath .'/wikia/AdSkin/css/wow_lich_king_warhammer.css?'. $wgAdSkinVersion .'" />';
+			break;
+		case "dragonball_origins":
+			echo '<link rel="stylesheet" type="text/css" href="'. $wgExtensionsPath .'/wikia/AdSkin/css/dragonball_origins.css?'. $wgAdSkinVersion .'" />';
+			break;
+		case "dnd":
+			echo '<link rel="stylesheet" type="text/css" href="'. $wgExtensionsPath .'/wikia/AdSkin/css/dnd.css?'. $wgAdSkinVersion .'" />';
+			break;
+		case "underworld":
+			echo '<link rel="stylesheet" type="text/css" href="'. $wgExtensionsPath .'/wikia/AdSkin/css/underworld.css?'. $wgAdSkinVersion .'" />';
+			echo '<style type="text/css">';
+			echo 'body.mainpage #wikia_header {
+				background: url('. $wgWikiaLogo .') 10px 2px no-repeat;
+			}';
+			echo '</style>';
+			break;
+		case "warhammer":
+			echo '<link rel="stylesheet" type="text/css" href="'. $wgExtensionsPath .'/wikia/AdSkin/css/warhammer.css?'. $wgAdSkinVersion .'" />';
+			echo '<style type="text/css">';
+			echo 'body.mainpage #wikia_header {
+				background: #252525 url('. $wgWikiaLogo .') 10px 2px no-repeat;
+			}';
+			echo '</style>';
+			break;
+		case "superpages":
+			/*
+			echo '<div style="position: absolute;">';
+			echo '<script language="JavaScript" type="text/javascript">
+			document.write(\'<script language="JavaScript" src="http://ad.doubleclick.net/adj/npm.wikia/superpages;tile=1;sz=1x1;click=;ord=?" type="text/javascript"></scr\' + \'ipt>\');
+			</script>
+			<noscript>
+			<a href="http://ad.doubleclick.net/jump/npm.wikia/superpages;tile=1;sz=1x1;click=;ord=?" target="_blank"><img src="http://ad.doubleclick.net/ad/npm.wikia/superpages;tile=1;sz=1x1;ord=?" width="1" height=1" border="0" alt=""></a></noscript>';
+			echo '</div>';
+			*/
+			echo '<link rel="stylesheet" type="text/css" href="'. $wgExtensionsPath .'/wikia/AdSkin/css/superpages.css?'. $wgAdSkinVersion .'" />';
+			break;
+		}	
+	}
+
+	return true;
+}
+
+function RenderAdSkinJS() {
+	global $wgUser, $wgExtensionsPath, $wgAdSkin, $wgAdSkinVersion;
+	
+	if (is_object($wgUser) && $wgUser->isLoggedIn() ){
+		return true;
+	}
+
+	if (isset($wgAdSkin)) {
+		switch ($wgAdSkin) {
+		case "warhammer":
+			echo '<script type="text/javascript" src="'. $wgExtensionsPath .'/wikia/AdSkin/js/warhammer.js?'. $wgAdSkinVersion .'"></script>';
+			echo '<A HREF="http://ad.doubleclick.net/jump/N2790.Wikia/B3436947.10;sz=1x1;ord='. time() .'?"><IMG SRC="http://ad.doubleclick.net/ad/N2790.Wikia/B3436947.10;sz=1x1;ord='. time() .'?" BORDER=0 WIDTH=1 HEIGHT=1 ALT="Click Here"></A>'; 
+			break;
+		}
+	}
+	
+	return true;
+}
+
+function RenderAdLink() {
+	global $wgAdSkin, $wgAdSkinVersion, $wgExtensionsPath;
+
+	if (isset($wgAdSkin)) {
+		switch ($wgAdSkin) {
+		case "warhammer_link":
+			echo '<link rel="stylesheet" type="text/css" href="'. $wgExtensionsPath .'/wikia/AdSkin/css/warhammer_link.css?'. $wgAdSkinVersion .'" />';
+			echo '<a href="http://www.warhammeronline.com/trial?WHK33J-U7C8Q-HWPEJ-9QAIJ-4Y8JK" id="warhammer_link">Download the Free 10-Day Trial</a>';
+			break;
+		}
+	}
+	return true;
+}
---
+++ wikia/branches/mw-upgrade/extensions/wikia/AnalyticsEngine/AnalyticsEngine.php
@@ -0,0 +1,42 @@
+<?php
+$wgExtensionCredits['other'][] = array(
+	'name' => 'AnalyticsEngine',
+	'author' => 'Nick Sullivan'
+);
+
+interface iAnalyticsProvider {
+	public function getSetupHtml();
+	public function trackEvent($eventName, $eventDetails=array());
+}
+
+require_once dirname(__FILE__) . '/AnalyticsProviderQuantServe.php';
+require_once dirname(__FILE__) . '/AnalyticsProviderGA_Urchin.php';
+
+class AnalyticsEngine {
+
+	const EVENT_PAGEVIEW = 'page_view';
+
+	private $providers = array('GAT', 'GA_Urchin', 'QuantServe', 'MessageQueue');
+
+	static public function track($provider, $event, $eventDetails=array()){
+		global $wgDevelEnvironment; 
+		
+		if ( !empty($wgDevelEnvironment) ) {
+			return '<!-- DevelEnvironment -->';
+		}
+		
+		switch ($provider){
+	//	  case 'GAT': $AP = new AnalyticsProviderGAT(); break;
+		  case 'GA_Urchin': $AP = new AnalyticsProviderGA_Urchin(); break;
+		  case 'QuantServe': $AP = new AnalyticsProviderQuantServe(); break;
+	//	  case 'MessageQueue': $AP = new AnalyticsProviderMessageQueue(); break;
+		  default: return '<!-- Invalid provider for AnalyticsEngine::getTrackCode -->';
+		}
+
+		$out = "<!-- Start for $provider, $event -->\n";
+		$out .= $AP->getSetupHtml();
+		$out .= $AP->trackEvent($event, $eventDetails) . "\n";
+		return $out;
+	}
+
+}
