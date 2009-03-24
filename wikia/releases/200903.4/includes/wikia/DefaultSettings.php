a10b789ebda9cd869098d0975b92976e6835c356
eloy <eloy@d23c911d-4c54-0410-aefe-1982d53a7c61>
2009-03-24 11:05:19 +0000

    styleversionjs++

--- wikia/releases/200903.4/includes/wikia/DefaultSettings.php
+++ wikia/releases/200903.4/includes/wikia/DefaultSettings.php
@@ -27,7 +27,7 @@ if( !defined( 'MEDIAWIKI' ) ) {
  * Use revision number
  */
 $wgStyleVersion         = '5563';
-$wgMergeStyleVersionJS  = '5563';
+$wgMergeStyleVersionJS  = '7151';
 $wgMergeStyleVersionCSS = '5563';
 
 /**
