db80a7519a7530ff30341862de39056dcb539eb0
emil <emil@d23c911d-4c54-0410-aefe-1982d53a7c61>
2009-03-31 10:28:03 +0000

    the code has been moved to trunk

--- wikia/branches/mw-upgrade/extensions/wikia/EditResearch/EditResearch.i18n.php
+++
@@ -1,7 +0,0 @@
-<?php
-$messages = array();
-$messages['en'] = array(
-	'search_wikipedia' => 'Search Wikipedia',
-	'previous' => 'previous',
-	'research_no_results' => 'No pages found.  Please search again.'
-);
--- wikia/branches/mw-upgrade/extensions/wikia/NewEditPage/NewEditPage.i18n.php
+++
@@ -1,5 +0,0 @@
-<?php
-$messages = array();
-$messages['en'] = array(
-	'new-edit-page-preview-notice' => 'Preview mode: no changes saved yet! Scroll down to continue editing.',
-);
