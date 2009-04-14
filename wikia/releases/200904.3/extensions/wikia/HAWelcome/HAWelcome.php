df082e557d8b2bee1b8af9cf72f903cb6f67333c
eloy <eloy@d23c911d-4c54-0410-aefe-1982d53a7c61>
2009-04-14 14:11:21 +0000

    * merge https://svn.wikia-code.com/wikia/trunk/extensions/wikia/HAWelcome -r7539:HEAD

--- wikia/releases/200904.3/extensions/wikia/HAWelcome/HAWelcome.php
+++ wikia/releases/200904.3/extensions/wikia/HAWelcome/HAWelcome.php
@@ -274,13 +274,15 @@ class HAWelcomeJob extends Job {
 						 * remove bots from admins
 						 */
 						$admins = array( "rev_user" => array_unique( array_diff( $admins, $bots ) ) );
-
 						$row = $dbr->selectRow(
 							array( "revision" ),
 							array( "rev_user", "rev_user_text"),
-							$dbr->makeList( $admins, LIST_OR ),
+							array(
+								$dbr->makeList( $admins, LIST_OR ),
+								"rev_timestamp > " . $dbr->addQuotes(  $dbr->timestamp( time() - 5184000 ) ) // 60 days ago (24*60*60*60)
+							),
 							__METHOD__,
-							array( "ORDER BY" => "rev_id DESC")
+							array( "ORDER BY" => "rev_timestamp DESC")
 						);
 						Wikia::log( __METHOD__, "query", $dbr->lastQuery() );
 						if( $row->rev_user ) {
