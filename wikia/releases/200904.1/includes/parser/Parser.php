d95d25600ffad28930637e2508309a7f53fe022c
emil <emil@d23c911d-4c54-0410-aefe-1982d53a7c61>
2009-04-14 08:25:54 +0000

    respect read-only mode

--- wikia/releases/200904.1/includes/parser/Parser.php
+++ wikia/releases/200904.1/includes/parser/Parser.php
@@ -3369,11 +3369,13 @@ class Parser
 		if (!$text)
 			return wfMsg('scarytranscludefailed', $url);
 
-		$dbw = wfGetDB(DB_MASTER);
-		$dbw->replace('transcache', array('tc_url'), array(
-			'tc_url' => $url,
-			'tc_time' => time(),
-			'tc_contents' => $text));
+		if( !wfReadOnly() ) {
+			$dbw = wfGetDB(DB_MASTER);
+			$dbw->replace('transcache', array('tc_url'), array(
+				'tc_url' => $url,
+				'tc_time' => time(),
+				'tc_contents' => $text));
+		}
 		return $text;
 	}
 
